a2197404234e9435b99f4cd078b38b95bb2c69dc
nagits <aleksey.nagits@gmail.com>
2015-04-10 13:40:32 +0500

    Add route list-form-page

---
+++ app/routes/list-form-page.js
@@ -0,0 +1,33 @@
+import Ember from 'ember';
+import AuthenticatedRouteMixin from 'simple-auth/mixins/authenticated-route-mixin';
+import PaginatedRouteMixin from 'prototype-ember-cli-application/mixins/paginated-route';
+import SortableRouteMixin from 'prototype-ember-cli-application/mixins/sortable-route';
+
+export default Ember.Route.extend(AuthenticatedRouteMixin, PaginatedRouteMixin, SortableRouteMixin, {
+    // 'employee'
+    modelTypeKey: Ember.required(),
+
+    view: Ember.required(),
+
+    model: function(params, transition) {
+        var page = parseInt(params.page, 10),
+            perPage = this.modelFor('application').get('perPage');
+
+        Ember.assert('page must be greater than zero.', page > 0);
+        Ember.assert('per_page must be greater than zero.', perPage > 0);
+
+        var self = this,
+            store = this.store,
+            typeKey = this.get('modelTypeKey'),
+            adapter = store.adapterFor(store.modelFor(typeKey));
+
+        var sorting = self.deserializeSortingParam(params['sort']);
+        var pageQuery = adapter.getPaginationQuery(page, perPage, sorting, store.serializerFor(typeKey));
+
+        return store.findManyByView(this.get('modelTypeKey'), this.get('view'), pageQuery)
+            .then(function(records) {
+                self.includeSorting(records, sorting);
+                return self.includePagination(records, page, perPage);
+            });
+    }
+});
---
+++ tests/unit/routes/list-form-page-test.js
@@ -0,0 +1,14 @@
+import {
+  moduleFor,
+  test
+} from 'ember-qunit';
+
+moduleFor('route:list-form-page', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  var route = this.subject();
+  assert.ok(route);
+});

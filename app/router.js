83da46fb184925260a0f3db7452021725acd46a2
nagits <aleksey.nagits@gmail.com>
2015-04-10 16:52:17 +0500

    Update Router

--- app/router.js
+++ app/router.js
@@ -6,4 +6,23 @@ var Router = Ember.Router.extend({
 });
 
 export default Router.map(function() {
+    // /login
+    this.route('login');
+
+    // /employees
+    this.resource('employees', function() {
+        //// /employees/2 - render into outlet in employees template
+        //this.resource('employee', {path: ':id'});
+
+        //// /employees/new
+        //this.route('new');
+
+        this.route('page', { path: 'page/:page' });
+    });
+
+    // /employees/2 - render into outlet in application template
+    this.resource('employee', {path: 'employees/:id'});
+
+    // /employees/new
+    this.route('employee.new', { path: 'employees/new' });
 });

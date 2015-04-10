3b6f08469490e6f497b3e11cd12046e5c144154c
nagits <aleksey.nagits@gmail.com>
2015-04-10 16:28:59 +0500

    Add route employee/new

---
+++ app/routes/employee/new.js
@@ -0,0 +1,7 @@
+import EditFormNewRoute from 'prototype-ember-cli-application/routes/edit-form-new';
+import Model from 'prototype-ember-cli-application/models/employee';
+
+export default EditFormNewRoute.extend({
+    view: Model.Views.EmployeeE,
+    modelTypeKey: 'employee'
+});

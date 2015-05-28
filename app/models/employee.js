d95eca6440d7266c4b065c65d73bfaaf0166331e
nagits <aleksey.nagits@gmail.com>
2015-05-28 12:13:46 +0500

    Commented out tmp code for hasMany testing
    Remove them from models/employee when it will be covered with tests

--- app/models/employee.js
+++ app/models/employee.js
@@ -1,4 +1,5 @@
-import Ember from 'ember';
+// Remove commented out lines, when they will be covered with tests.
+/*import Ember from 'ember';*/
 import DS from 'ember-data';
 import ModelProjectionsCollection from '../objects/model-projections-collection';
 import ModelProjection from '../objects/model-projection';
@@ -8,11 +9,11 @@ var Model = ProjectedModel.extend({
   firstName: DS.attr('string'),
   lastName: DS.attr('string'),
   birthDate: DS.attr('date'),
-  reportsTo: DS.belongsTo('employee', { inverse: null, async: true }),
-  tmpChildren: DS.hasMany('employee', { inverse: null, async: true })
+  reportsTo: DS.belongsTo('employee', { inverse: null, async: true })/*,
+  tmpChildren: DS.hasMany('employee', { inverse: null, async: true })*/
 });
 
-Ember.$.mockjax({
+/*Ember.$.mockjax({
   url: "*Employees(3)",
   responseText: {
     "@odata.context": "http://northwindodata.azurewebsites.net/odata/$metadata#Employees(EmployeeID,FirstName,LastName,BirthDate,ReportsTo)/$entity",
@@ -23,28 +24,28 @@ Ember.$.mockjax({
     "ReportsTo": 2,
     "TmpChildren": [4,5]
   }
-});
+});*/
 
 Model.reopenClass({
   Projections: ModelProjectionsCollection.create({
     EmployeeE: ModelProjection.create({
       type: 'employee',
       name: 'EmployeeE',
-      properties: ['firstName', 'lastName', 'birthDate', 'reportsTo', 'tmpChildren'],
+      properties: ['firstName', 'lastName', 'birthDate', 'reportsTo'/*, 'tmpChildren'*/],
       masters: ModelProjectionsCollection.create({
         reportsTo: ModelProjection.create({
           type: 'employee',
           name: 'EmployeeE.masters.reportsTo',
           properties: ['firstName']
         })
-      }),
+      })/*,
       details: ModelProjectionsCollection.create({
         tmpChildren: ModelProjection.create({
           type: 'employee',
           name: 'EmployeeE.details.tmpChildren',
           properties: ['lastName']
         })
-      })
+      })*/
     }),
     EmployeeL: ModelProjection.create({
       type: 'employee',

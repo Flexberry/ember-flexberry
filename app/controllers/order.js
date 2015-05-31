9806bdf02a942fe18d507759947d2c8a62c1748e
erhead <erhead@yandex.ru>
2015-05-31 11:44:10 +0500

    Add edit form controller

--- app/controllers/order.js
+++ app/controllers/order.js
@@ -1,56 +1,13 @@
-import Ember from 'ember';
-import EmberValidations from 'ember-validations';
-import ErrorableControllerMixin from 'prototype-ember-cli-application/mixins/errorable-controller';
+import EditFormController from 'prototype-ember-cli-application/controllers/edit-form';
 
-// TODO: Ember.ObjectController is deprecated, please use Ember.Controller.
-// But validations don't work using Ember.Controller.
-export default Ember.ObjectController.extend(EmberValidations.Mixin, ErrorableControllerMixin, {
-  actions: {
-    save: function() {
-      this.send('dismissErrorMessages');
-      if (this.get('numTotalErrors')) {
-        this.send('addErrorMessage', 'В модели присутствуют ошибки.');
-        return;
-      }
+export default EditFormController.extend({
+  // Route to redirect to when closing.
+  parentRoute: "orders",
 
-      var model = this.get('model');
-      if (model.get('isDirty')) {
-        model.save().then(function() {
-          alert('Saved');
-        }, function(ajaxError) {
-          alert('Save failed');
-          if (ajaxError){
-            var jsonErrors = Ember.$.parseJSON(ajaxError.responseText);
-            if (jsonErrors && jsonErrors.error && jsonErrors.error.message){
-              this.send('addErrorMessage', jsonErrors.error.message);
-            }
-          }
-        }.bind(this));
-      } else {
-        alert('There are no changes');
-      }
-    },
-
-    close: function() {
-      // TODO: нужно учитывать пэйджинг.
-      // Без сервера не обойтись, наверное. Нужно определять, на какую страницу редиректить.
-      // Либо редиректить на что-то типа /orders/page/whichContains/{object id}, а контроллер/роут там далее разрулит, куда дальше послать редирект.
-      this.transitionToRoute('orders');
-    }
-  },
-
-  // Validators.
+  // Validation rules.
   validations: {
     orderDate: {
       presence: true
     }
-  },
-
-  // Common number of validation errors.
-  numTotalErrors: function () {
-    return this.get('validators').mapBy('errors.length').reduce(
-      function (beforeSum, addValue) {
-        return beforeSum + addValue;
-      }, 0);
-  }.property('validators.@each.errors.length')
+  }
 });

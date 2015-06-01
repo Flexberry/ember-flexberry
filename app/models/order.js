b6fe57f8ce2eeb00d1237c164458d6e7258dd5bd
nagits <aleksey.nagits@gmail.com>
2015-06-01 19:43:52 +0500

    Define order projections using helper

--- app/models/order.js
+++ app/models/order.js
@@ -1,6 +1,4 @@
 import DS from 'ember-data';
-import ModelProjectionsCollection from '../objects/model-projections-collection';
-import ModelProjection from '../objects/model-projection';
 import ProjectedModel from './projected-model';
 
 var Model = ProjectedModel.extend({
@@ -8,29 +6,7 @@ var Model = ProjectedModel.extend({
   employeeID: DS.belongsTo('employee', { inverse: null, async: true })
 });
 
-// TODO: why not Model.Projections = ...?
-Model.reopenClass({
-  // TODO: rename Projections to lowercase.
-  Projections: ModelProjectionsCollection.create({
-    OrderE: ModelProjection.create({
-      type: 'order',
-      name: 'OrderE',
-      properties: ['orderDate', 'employeeID'],
-      masters: ModelProjectionsCollection.create({
-        employeeID: ModelProjection.create({
-          // TODO: rename type to ownerType or something else.
-          type: 'order',
-          name: 'OrderE.masters.employeeID',
-          properties: ['firstName']
-        })
-      })
-    }),
-    OrderL: ModelProjection.create({
-      type: 'order',
-      name: 'OrderL',
-      properties: ['orderDate']
-    })
-  })
-});
+Model.defineProjection('order', 'OrderE', ['orderDate', 'employeeID.firstName']);
+Model.defineProjection('order', 'OrderL', ['orderDate']);
 
 export default Model;

061ffbfe6697c5d1f329c7af2f3be91eeee262f7
nagits <aleksey.nagits@gmail.com>
2015-06-03 16:51:01 +0500

    Add some attributes to order model and edit form

--- app/models/order.js
+++ app/models/order.js
@@ -2,11 +2,24 @@ import DS from 'ember-data';
 import ProjectedModel from './projected-model';
 
 var Model = ProjectedModel.extend({
+  shipName: DS.attr('string'),
+  shipCountry: DS.attr('string'),
   orderDate: DS.attr('date'),
   employeeID: DS.belongsTo('employee', { inverse: null, async: true })
 });
 
-Model.defineProjection('order', 'OrderE', ['orderDate', 'employeeID.firstName']);
-Model.defineProjection('order', 'OrderL', ['orderDate']);
+Model.defineProjection('order', 'OrderE', [
+  'shipName',
+  'shipCountry',
+  'orderDate',
+  'employeeID.firstName',
+  'employeeID.lastName'
+]);
+
+Model.defineProjection('order', 'OrderL', [
+  'shipName',
+  'shipCountry',
+  'orderDate'
+]);
 
 export default Model;

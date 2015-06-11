2c2c0f8876e7297e2edf3610c9e814f383e72789
ankulyapin <an.kulyapin@gmail.com>
2015-06-11 11:15:41 +0500

    Remove back auth initialize function to make test work

--- app/initializers/authentication.js
+++ app/initializers/authentication.js
@@ -1,11 +1,13 @@
 import CustomAuthenticator from '../authenticators/custom';
 import CustomAuthorizer from '../authorizers/custom';
 
+export function initialize(container) {
+  container.register('authenticator:custom', CustomAuthenticator);
+  container.register('authorizer:custom', CustomAuthorizer);
+}
+
 export default {
   name: 'authentication',
   before: 'simple-auth',
-  initialize: function (container) {
-    container.register('authorizer:custom', CustomAuthorizer);
-    container.register('authenticator:custom', CustomAuthenticator);
-  }
+  initialize: initialize
 };

799a37acb8568feb8223146f25c7dcab2e50c6c6
ankulyapin <an.kulyapin@gmail.com>
2015-06-22 11:54:12 +0500

    Add modal-dialog component

--- app/routes/application.js
+++ app/routes/application.js
@@ -9,6 +9,24 @@ export default Ember.Route.extend(ApplicationRouteMixin, {
   actions: {
     invalidateSession: function() {
       this.get('session').invalidate();
+    },
+
+    // action to show modal-dialog by name
+    showModalDialog: function (modalDialogName, data) {
+      var params = Ember.$.extend({
+        into: 'application',
+        outlet: 'modal'
+      }, data);
+
+      this.render(modalDialogName, params);
+    },
+
+    // action to remove modal outlet on modal-dialog close
+    removeModalDialog: function() {
+      this.disconnectOutlet({
+        outlet: 'modal',
+        parentView: 'application'
+      });
     }
   }
 });

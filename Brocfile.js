c56174a4105cb567d735ac1955159c70e341c65c
nagits <aleksey.nagits@gmail.com>
2015-06-30 18:14:20 +0500

    Install broccoli-jscs with empty jscs rules

--- Brocfile.js
+++ Brocfile.js
@@ -2,7 +2,12 @@
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-var app = new EmberApp();
+var app = new EmberApp({
+  jscsOptions: {
+    enabled: true,
+    esnext: true
+  }
+});
 
 // Use `app.import` to add additional libraries to the generated
 // output files.

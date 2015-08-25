77dd6a60e3f5d49542ae69805369be597b05594c
nagits <aleksey.nagits@gmail.com>
2015-08-25 12:56:39 +0500

    Revert upgrade-to-jquery-compat-3.0.0

--- config/environment.js
+++ config/environment.js
@@ -20,10 +20,7 @@ module.exports = function(environment) {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
         // e.g. 'with-controller': true
-      },
-
-      // Disable runtime jQuery-version assertions.
-      FORCE_JQUERY: true
+      }
     },
     APP: {
       // Here you can pass flags/options to your application instance

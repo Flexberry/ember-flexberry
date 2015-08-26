1899962cfe38b040f6f567e7b2cb795c1687d9ed
hvostya <tosotova@ics.perm.ru>
2015-08-26 09:38:27 +0500

    Add logic to refresh button

--- app/components/olv-toolbar.js
+++ app/components/olv-toolbar.js
@@ -2,9 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   modelName: null,
+  modelController:null,
   createNewButton: false,
   refreshButton: false,
   createNewPath: function() {
     return this.get('modelName') + '.new';
-  }.property('modelName')
+  }.property('modelName'),
+
+  actions: {
+    clickRefreshButton: function() {
+      this.get('modelController').send('refreshList');
+    }
+  }
 });

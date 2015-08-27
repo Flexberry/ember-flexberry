8eb118c23cbe1bccddbdbf0e7625c74c721ada40
nagits <aleksey.nagits@gmail.com>
2015-08-27 14:46:00 +0500

    Install datatables package

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -25,7 +25,7 @@ module.exports = function(defaults) {
   if (app.env !== 'production') {
     app.import('bower_components/jquery-mockjax/jquery.mockjax.js');
   }
-  
+
   app.import({
     development: 'bower_components/moment/min/moment-with-locales.js',
     production:  'bower_components/moment/min/moment-with-locales.min.js'
@@ -40,6 +40,32 @@ module.exports = function(defaults) {
     development: 'bower_components/semantic-ui-daterangepicker/daterangepicker.css',
     production:  'bower_components/semantic-ui-daterangepicker/daterangepicker.min.css'
   });
-  
+
+  app.import({
+    development: 'bower_components/datatables/media/js/jquery.dataTables.js',
+    production:  'bower_components/datatables/media/js/jquery.dataTables.min.js'
+  });
+
+  app.import({
+    development: 'bower_components/datatables/media/css/jquery.dataTables.css',
+    production:  'bower_components/datatables/media/css/jquery.dataTables.min.css'
+  });
+
+  app.import('bower_components/datatables/media/images/sort_asc_disabled.png', {
+    destDir: 'images'
+  });
+  app.import('bower_components/datatables/media/images/sort_asc.png', {
+    destDir: 'images'
+  });
+  app.import('bower_components/datatables/media/images/sort_both.png', {
+    destDir: 'images'
+  });
+  app.import('bower_components/datatables/media/images/sort_desc_disabled.png', {
+    destDir: 'images'
+  });
+  app.import('bower_components/datatables/media/images/sort_desc.png', {
+    destDir: 'images'
+  });
+
   return app.toTree();
 };

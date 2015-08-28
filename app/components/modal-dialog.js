e1eb05cba47c5769f5d7325159b26d3b08164d26
hvostya <tosotova@ics.perm.ru>
2015-08-28 15:30:17 +0500

    Fixes of indentations

--- app/components/modal-dialog.js
+++ app/components/modal-dialog.js
@@ -3,15 +3,15 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   didInsertElement: function() {
       var _this = this;
-	  this.$('.ui.modal').modal("setting", {
-        onApprove: function () {
-            _this.sendAction('ok');
+      this.$('.ui.modal').modal('setting', {
+        onApprove: function() {
+          _this.sendAction('ok');
         },
-		onDeny: function () {
-            _this.sendAction('close');
+        onDeny: function() {
+          _this.sendAction('close');
         },
-		onHidden: function () {
-            _this.sendAction('close');
+        onHidden: function() {
+          _this.sendAction('close');
         }
       }).modal('show');
     }

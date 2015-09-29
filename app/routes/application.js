e687090923ec80fa039b93f0b625bef0f4686ea7
nagits <aleksey.nagits@gmail.com>
2015-09-29 15:18:54 +0500

    Remove context model, use pre-generated sitemap

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,10 +2,6 @@ import Ember from 'ember';
 import ApplicationRouteMixin from 'simple-auth/mixins/application-route-mixin';
 
 export default Ember.Route.extend(ApplicationRouteMixin, {
-  model: function() {
-    return this.store.find('context', 0);
-  },
-
   actions: {
     invalidateSession: function() {
       this.get('session').invalidate();
--- app/serializers/context.js
+++
@@ -1,8 +0,0 @@
-import ApplicationSerializer from '../serializers/application';
-
-export default ApplicationSerializer.extend({
-  // Раньше можно было определить primaryKey как функцию, и задать один алгоритм для всех сериализаторов.
-  // Щас либо по отдельности для каждого задавать это свойство, либо переопределять функции,
-  // в которых задействован primaryKey (нужно иметь ввиду, что Ember Data в бете).
-  primaryKey: 'ContextID'
-});

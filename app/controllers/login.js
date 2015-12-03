8dca63d0f898192332e379c7d62d08cdb26fc2e7
nagits <aleksey.nagits@gmail.com>
2015-12-03 17:58:47 +0500

    Refactor login controller

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,17 +1,5 @@
-import Ember from 'ember';
-import LoginControllerMixin from 'simple-auth/mixins/login-controller-mixin';
+import BaseLoginController from './base-login';
 
-export default Ember.Controller.extend(LoginControllerMixin, {
-  content: {},
-  authenticator: 'authenticator:custom',
-  actions: {
-    authenticate: function() {
-      var _this = this;
-      var credentials = this.getProperties('identification', 'password');
-      this.get('session').authenticate('authenticator:custom', credentials)
-        .then(null, function(data) {
-          _this.set('errorMessage', data.error_description);
-        });
-    }
-  }
+export default BaseLoginController.extend({
+  authenticator: 'authenticator:custom'
 });

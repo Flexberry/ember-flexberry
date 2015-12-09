904ec5fd379bf47f66794af36ffcd5ecbf5d3ec5
nagits <aleksey.nagits@gmail.com>
2015-12-09 19:21:18 +0500

    Fix bower packages installation for addon

--- blueprints/ember-flexberry/index.js
+++ blueprints/ember-flexberry/index.js
@@ -1,8 +1,10 @@
 /* globals module */
 module.exports = {
   afterInstall: function () {
-    this.addBowerPackageToProject('git://github.com/BreadMaker/semantic-ui-daterangepicker.git#5d46ed2e6e5a0bf398bb6a5df82e06036dfc46be');
-    this.addBowerPackageToProject('datatables');
+    return this.addBowerPackagesToProject([
+      { name: 'semantic-ui-daterangepicker', target: '5d46ed2e6e5a0bf398bb6a5df82e06036dfc46be' },
+      { name: 'datatables', target: '~1.10.8' }
+    ]);
   },
 
   normalizeEntityName: function () {}

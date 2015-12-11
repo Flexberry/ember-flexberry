18d52a6a7a6a6aa6f9b1d69c284d3c43c7a9bd5e
Evgeniy Khaberev <ehaberev@yandex.ru>
2015-12-11 12:34:30 +0500

    Fix datetime-picker component
    Fixed value setting by apply button when date/time is not changed

--- app/components/datetime-picker.js
+++ app/components/datetime-picker.js
@@ -35,7 +35,7 @@ export default Ember.Component.extend({
     }
 
     var val = this.get('value');
-    var startDate = new Date();
+    var startDate = moment(new Date());
     if (val !== undefined && moment(val).isValid()) {
       startDate = moment(val);
       this.$('input').val(startDate.format(this.dateTimeFormat));
@@ -56,12 +56,23 @@ export default Ember.Component.extend({
         format: this.dateTimeFormat
       },
       function(start, end, label) {
-        var dateToSet = start.isValid() ? start.toDate() : _this.invalidDate;
-        _this.set('value', dateToSet);
+        _this.setValue(start);
       }
       );
+      this.$('input').on('apply.daterangepicker', function(ev, picker) {
+        var currentValue = _this.get('value');
+        var pickerStartDateString = moment(picker.startDate.toDate()).format(_this.dateTimeFormat);
+        if (!currentValue || !moment(moment(currentValue).format(_this.dateTimeFormat), _this.dateTimeFormat).isSame(moment(pickerStartDateString, _this.dateTimeFormat))) {
+          _this.setValue(picker.startDate);
+        }
+      });
     }
   }.on('didInsertElement'),
+
+  setValue: function(dateFromPicker) {
+    var dateToSet = dateFromPicker.isValid() ? dateFromPicker.toDate() : this.invalidDate;
+    this.set('value', dateToSet);
+  },
 
   // Set proper start date when value changed outside of component.
   _valueChanged: function() {

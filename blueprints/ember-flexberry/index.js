84ae1aaed7d82d10470c6259aa588a75804db93d
nagits <aleksey.nagits@gmail.com>
2015-12-11 14:58:24 +0500

    Fix addon build and installation

--- blueprints/ember-flexberry/index.js
+++ blueprints/ember-flexberry/index.js
@@ -1,10 +1,18 @@
 /* globals module */
 module.exports = {
   afterInstall: function () {
+    var _this = this;
     return this.addBowerPackagesToProject([
       { name: 'semantic-ui-daterangepicker', target: '5d46ed2e6e5a0bf398bb6a5df82e06036dfc46be' },
       { name: 'datatables', target: '~1.10.8' }
-    ]);
+    ]).then(function() {
+      return _this.addAddonsToProject({
+        packages: [
+          { name: 'ember-cli-simple-auth', target: '0.8.0' },
+          { name: 'semantic-ui-ember', target: '0.9.0' }
+        ]
+      });
+    });
   },
 
   normalizeEntityName: function () {}

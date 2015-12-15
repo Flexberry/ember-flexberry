6364747909e9f76abdf3107b9c193fc44dfe48f0
nagits <aleksey.nagits@gmail.com>
2015-12-15 16:18:35 +0500

    Update ember-cli-release to 0.2.8

--- config/release.js
+++ config/release.js
@@ -1,8 +1,27 @@
 /* jshint node: true */
+
+// var RSVP = require('rsvp');
 var path = require('path');
 var fs = require('fs');
 
+// For details on each option run `ember help release`
 module.exports = {
+  // local: true,
+  // remote: 'some_remote',
+  // annotation: "Release %@",
+  // message: "Bumped version to %@",
+  // manifest: [ 'package.json', 'bower.json', 'someconfig.json' ],
+  // strategy: 'date',
+  // format: 'YYYY-MM-DD',
+  // timezone: 'America/Los_Angeles',
+  //
+  // beforeCommit: function(project, versions) {
+  //   return new RSVP.Promise(function(resolve, reject) {
+  //     // Do custom things here...
+  //   });
+  // }
+
+  // TODO: rewrite using promises and async fs.
   beforeCommit: function(project, versions) {
     var filePath = path.join(project.root, 'vendor/ember-flexberry/register-version.js');
     if (fs.existsSync(filePath)) {

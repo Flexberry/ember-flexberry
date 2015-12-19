53647e7049c31921b306395e270924c4a222ce8d
nagits <aleksey.nagits@gmail.com>
2015-12-19 17:59:07 +0500

    Add task "Update docs" in Travis build

---
+++ scripts/update-docs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Exit with nonzero exit code if anything fails.
+set -e
+
+npm install -g yuidocjs
+git clone --recursive https://github.com/Flexberry/Documentation.git docs
+cd docs
+
+npm install
+git submodule foreach git pull origin master
+yuidoc
+
+git config user.name "Flexberry-man"
+git config user.email "mail@flexberry.net"
+
+git add --all
+git commit -m "Update docs" -m "By Travis, Repo ${TRAVIS_REPO_SLUG},\nBuild #${TRAVIS_BUILD_NUMBER}, Commit ${TRAVIS_COMMIT}."
+
+# Redirect any output to /dev/null to hide any sensitive credential data that might otherwise be exposed.
+git push --force --quiet "https://${GH_TOKEN}@github.com/Flexberry/Documentation.git" > /dev/null 2>&1
+
+echo "Done."

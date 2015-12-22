5a656d0b4134b583726ea682a71c6cd02ca87246
hvostya <tosotova@ics.perm.ru>
2015-12-22 15:58:24 +0500

    Fix extra colon

--- addon/components/flexberry-base.js
+++ addon/components/flexberry-base.js
@@ -3,5 +3,5 @@ import Ember from 'ember';
 // Base component for Flexberry Ember UI Components.
 export default Ember.Component.extend({
   // Flag to make control readonly.
-  readonly: undefined,
+  readonly: undefined
 });

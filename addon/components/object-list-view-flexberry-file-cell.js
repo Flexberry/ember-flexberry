c42bd519f1da4a7872f50af3e213a98d88b5be56
Serega Matin <seregamatin@gmail.com>
2015-12-23 13:04:30 +0500

    Add olv-flexberry-file-cell

---
+++ addon/components/object-list-view-flexberry-file-cell.js
@@ -0,0 +1,9 @@
+import OlvInputCellComponent from 'ember-flexberry/components/object-list-view-input-cell';
+
+export default OlvInputCellComponent.extend({
+  actions: {
+    fileChange: function(options) {
+      this.get('record').set(this.get('column.propName'), options.value);
+    }
+  }
+});

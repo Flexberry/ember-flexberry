7adc1e15da0b0e0c11b383758322e2ed3412486a
Evgeniy Khaberev <ehaberev@yandex.ru>
2015-12-28 21:20:59 +0500

    Modify the way of two way binding
    in object-list-view-input-cell component. Add binding input value with
    model on key press.

--- addon/components/object-list-view-input-cell.js
+++ addon/components/object-list-view-input-cell.js
@@ -9,7 +9,18 @@ export default BaseComponent.extend({
   didInsertElement: function() {
     var _this = this;
     this.$('input').change(function() {
-      _this.record.set(_this.column.propName, _this.$(this).val());
+      _this._setValue();
     });
+    this.$('input').keyup(function() {
+      _this._setValue();
+    });
+  },
+
+  _setValue: function() {
+    var currentModelValue = this.record.get(this.column.propName);
+    var currentInputValue = this.$('input').val();
+    if (currentModelValue !== currentInputValue) {
+      this.record.set(this.column.propName, currentInputValue);
+    }
   }
 });

c919dc20612914ff11987a6a01e1a0aff02d7ce8
nagits <aleksey.nagits@gmail.com>
2016-01-21 17:11:16 +0500

    Remove datatables component imports

--- index.js
+++ index.js
@@ -44,32 +44,6 @@ module.exports = {
       production:  'bower_components/semantic-ui-daterangepicker/daterangepicker.min.css'
     });
 
-    app.import({
-      development: 'bower_components/datatables/media/js/jquery.dataTables.js',
-      production:  'bower_components/datatables/media/js/jquery.dataTables.min.js'
-    });
-
-    app.import({
-      development: 'bower_components/datatables/media/css/jquery.dataTables.css',
-      production:  'bower_components/datatables/media/css/jquery.dataTables.min.css'
-    });
-
-    app.import('bower_components/datatables/media/images/sort_asc_disabled.png', {
-      destDir: 'images'
-    });
-    app.import('bower_components/datatables/media/images/sort_asc.png', {
-      destDir: 'images'
-    });
-    app.import('bower_components/datatables/media/images/sort_both.png', {
-      destDir: 'images'
-    });
-    app.import('bower_components/datatables/media/images/sort_desc_disabled.png', {
-      destDir: 'images'
-    });
-    app.import('bower_components/datatables/media/images/sort_desc.png', {
-      destDir: 'images'
-    });
-
     // Custom script which fixes some jQuery 1.10.x+ AJAX bugs with code from newer jQuery.3.0.0-alpha1+compat version.
     app.import('vendor/jquery.3.0.0-alpha1+compat/jquery.ajaxreplacement.js');
   }

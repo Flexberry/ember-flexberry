9b28895556a03f05376e171ac135d74afe4f1949
nagits <aleksey.nagits@gmail.com>
2016-01-22 17:59:50 +0500

    Fix flexberry-groupedit-test

--- tests/integration/components/flexberry-groupedit-test.js
+++ tests/integration/components/flexberry-groupedit-test.js
@@ -1,27 +1,16 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import Employee from '../../../models/employee';
 
 moduleForComponent('flexberry-groupedit', 'Integration | Component | Flexberry groupedit', {
   integration: true
 });
 
 test('it renders', function(assert) {
-  assert.expect(2);
-
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{flexberry-groupedit}}`);
-
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#flexberry-groupedit}}
-      template block text
-    {{/flexberry-groupedit}}
-  `);
-
-  //Component does not support template block usage.
-  assert.equal(this.$().text().trim(), '');
+  this.set('proj', Employee.projections.get('EmployeeE'));
+  this.render(hbs`{{flexberry-groupedit modelProjection=proj componentName='my-group-edit'}}`);
+  assert.ok(true);
 });

86a4e32f932bfba91470fd1e7760fff7c1b516b7
Serega Matin <seregamatin@gmail.com>
2016-02-15 14:29:15 +0500

    Remove space

--- tests/dummy/app/controllers/group-edit-test.js
+++ tests/dummy/app/controllers/group-edit-test.js
@@ -3,7 +3,7 @@ import EditFormController from 'ember-flexberry/controllers/edit-form';
 export default EditFormController.extend({
   title: 'Group edit test',
 
-  getCellComponent: function (attr, bindingPath) {
+  getCellComponent: function(attr, bindingPath) {
     return 'object-list-view-input-cell';
   }
 });

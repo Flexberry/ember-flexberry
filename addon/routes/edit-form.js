611db3b8dca65e049ae9e9c39adb8de2e9172231
nagits <aleksey.nagits@gmail.com>
2016-02-16 13:53:39 +0500

    Rework hasMany relationships saving

--- addon/routes/edit-form.js
+++ addon/routes/edit-form.js
@@ -51,23 +51,22 @@ export default ProjectedModelFormRoute.extend(FlexberryGroupeditRouteMixin, {
     this._super.apply(this, arguments);
 
     controller.send('dismissErrorMessages');
-    var model = controller.get('model');
-    this._rollbackDetails(model);
+
+    controller.rollbackHasManyRelationships();
+    let model = controller.get('model');
     if (model && model.get('hasDirtyAttributes')) {
       model.rollbackAttributes();
     }
   },
 
   setupController: function(controller, model) {
-    // Call _super for default behavior.
-    this._super(controller, model);
+    this._super(...arguments);
 
     // Define 'modelProjection' for controller instance.
     let modelClass = model.constructor;
     let modelProjName = this.get('modelProjection');
     let proj = modelClass.projections.get(modelProjName);
     controller.set('modelProjection', proj);
-    controller.set('modelProjectionName', modelProjName);
   },
 
   actions: {
@@ -81,35 +80,5 @@ export default ProjectedModelFormRoute.extend(FlexberryGroupeditRouteMixin, {
       this._super(transition);
       this.controller.send('routeWillTransition');
     }
-  },
-
-  _rollbackDetails: function(model) {
-    var modelClass = model.constructor;
-    var modelProjName = this.get('modelProjection');
-    var projection = modelClass.projections.get(modelProjName);
-    var attributes = projection.attributes;
-    for (var attrName in attributes) {
-      if (!attributes.hasOwnProperty(attrName)) {
-        continue;
-      }
-
-      var attr = attributes[attrName];
-      if (attr.kind === 'hasMany') {
-        var detailModels = model.get(attrName);
-        for (var i = 0; i < detailModels.get('length'); i++) {
-          if (detailModels.objectAt(i).get('hasDirtyAttributes')) {
-            detailModels.objectAt(i).rollbackAttributes();
-          }
-        }
-      }
-    }
-
-    var _this = this;
-    this.get('deletedRecords').forEach(function(deletedRecord) {
-      _this.store.findRecord(deletedRecord.model, deletedRecord.id, { reload: false }).then(function(record) {
-        record.rollbackAttributes();
-      });
-    });
-    this.get('deletedRecords').clear();
   }
 });
--- tests/dummy/app/serializers/order.js
+++ tests/dummy/app/serializers/order.js
@@ -6,8 +6,5 @@ export default ApplicationSerializer.extend(DS.EmbeddedRecordsMixin, {
     employee: { serialize: 'records', deserialize: 'records' }
   },
 
-  // Раньше можно было определить primaryKey как функцию, и задать один алгоритм для всех сериализаторов.
-  // Щас либо по отдельности для каждого задавать это свойство, либо переопределять функции,
-  // в которых задействован primaryKey (нужно иметь ввиду, что Ember Data в бете).
   primaryKey: 'OrderID'
 });

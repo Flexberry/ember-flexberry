7c649bde394825ca8472f8a7d52010c8fe146667
nagits <aleksey.nagits@gmail.com>
2016-02-24 16:21:53 +0500

    Remove deprecated todo

--- tests/dummy/app/serializers/employee.js
+++ tests/dummy/app/serializers/employee.js
@@ -1,7 +1,6 @@
 import DS from 'ember-data';
 import ApplicationSerializer from '../serializers/application';
 
-// TODO: ODataSerializer.extend
 export default ApplicationSerializer.extend(DS.EmbeddedRecordsMixin, {
   attrs: {
     employee1: { serialize: 'records', deserialize: 'records' },

f8cb40b446a5b5001102c6d29375ec602ef0fc41
kuzkok <kuzkok@gmail.com>
2016-02-25 12:23:23 +0500

    Render errors messages in validation summary

--- tests/integration/components/flexberry-validationsummary-test.js
+++ tests/integration/components/flexberry-validationsummary-test.js
@@ -1,5 +1,6 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import Ember from 'ember';
 
 moduleForComponent('flexberry-validationsummary', 'Integration | Component | flexberry validationsummary', {
   integration: true
@@ -9,16 +10,9 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });"
 
-  this.render(hbs`{{flexberry-validationsummary}}`);
+  this.set('errors', new Ember.Object());
+
+  this.render(hbs`{{flexberry-validationsummary errors=errors}}`);
 
   assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:"
-  this.render(hbs`
-    {{#flexberry-validationsummary}}
-      template block text
-    {{/flexberry-validationsummary}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
 });

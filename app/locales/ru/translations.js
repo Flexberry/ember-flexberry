1ed874c66150bc9dfb70ebe3da6691041c6511ef
erhead <erhead@yandex.ru>
2016-02-27 15:46:17 +0500

    Add russian locale

---
+++ app/locales/ru/translations.js
@@ -0,0 +1,64 @@
+export default {
+  'edit-form': {
+    'saved-message': 'Форма сохранена.',
+    'save-failed-message': 'Ошибка сохранения.',
+    'delete-failed-message': 'Ошибка удаления.'
+  },
+  
+  'flexberry-datepicker': {
+    // Months and days of week names are taken from moment.js.
+    'apply-button-text': 'Выбрать',
+    'cancel-button-text': 'Отмена'
+  },
+  
+  'flexberry-file': {
+    'placeholder': '(нет файла)',
+    
+    'add-btn-text': 'Добавить файл',
+    'remove-btn-text': 'Удалить файл',
+    'upload-btn-text': 'Отправить файл',
+    'download-btn-text': 'Загрузить файл',
+    
+    'error-dialog-title': 'Ошибка компонента выбора файла',
+    'error-dialog-content': 'Произошла ошибка в компоненте выбора файла',
+    'error-dialog-ok-btn-text': 'OK',
+    
+    'add-file-error-title': 'Ошибка добавления файла',
+    'file-too-big-message': 'Размер файла должен быть не больше {{maxSize}} байт. Выбранный файл {{fileName}} имеет размер {{actualSize}} байт.',
+    
+    'upload-file-error-title': 'Ошибка отправки файла',
+    'upload-file-error-message': 'Отправка файла {{fileName}} не удалась. {{errorText}}',
+    
+    'download-file-error-title': 'Ошибка загрузки файла',
+    'download-file-error-message': 'Загрузка файла {{fileName}} не удалась. {{errorText}}'
+  },
+  
+  'flexberry-lookup': {
+    'placeholder': '(нет значения)',
+    'choose-button-text': 'Выбрать',
+    'remove-button-text': 'Очистить'
+  },
+  
+  'login': {
+    'login-failed-message': 'Вход не удался',
+    'login-field-label': 'Логин',
+    'login-field-placeholder': 'Введите логин',
+    'password-field-label': 'Пароль',
+    'password-field-placeholder': 'Введите пароль',
+    'login-button-text': 'Войти'
+  },
+  
+  'modal-dialog' : {
+    'ok-button-text': 'OK',
+    'close-button-text': 'Закрыть'
+  },
+  
+  'object-list-view': {
+    'no-data-message': 'Нет данных'
+  },
+  
+  'olv-toolbar': {
+    'add-button-text': 'Добавить',
+    'refresh-button-text': 'Обновить'
+  }
+};

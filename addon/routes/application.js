9e7af0a0de78d6bef9a400d94c98b83559422c08
erhead <erhead@yandex.ru>
2016-02-28 00:08:49 +0500

    Add moment.js locale initialization

--- addon/routes/application.js
+++ addon/routes/application.js
@@ -1,8 +1,11 @@
 import Ember from 'ember';
 import ModalApplicationRouteMixin from '../mixins/modal-application-route';
+import Moment from 'moment';
 
 export default Ember.Route.extend(ModalApplicationRouteMixin, {
   beforeModel: function() {
-    this.set('i18n.locale', navigator.language || navigator.userLanguage || 'en');
+    var language = navigator.language || navigator.userLanguage || 'en';
+    this.set('i18n.locale', language);
+    Moment.lang(language);
   }
 });

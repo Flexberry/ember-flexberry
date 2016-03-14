68e9a937bb60b98598d4d9c24c52015af9510709
hvostya <tosotova@ics.perm.ru>
2016-03-14 14:25:58 +0500

    Fix occasional data merge with loaded one

--- addon/routes/edit-form.js
+++ addon/routes/edit-form.js
@@ -40,11 +40,19 @@ export default ProjectedModelFormRoute.extend(FlexberryGroupeditRouteMixin, {
     let modelName = this.get('modelName');
     let modelProjName = this.get('modelProjection');
 
+    // Get data from service in order to decide if it is necessary to reload data or not.
+    // If already visited detail's route is observed or it is come back to agregators's route,
+    // it is not necessary (otherwise data merge with loaded data can occur occasionally).
+    let flexberryDetailInteractionService = this.get('flexberryDetailInteractionService');
+    let modelCurrentNotSaved = flexberryDetailInteractionService.get('modelCurrentNotSaved');
+    let modelSelectedDetail = flexberryDetailInteractionService.get('modelSelectedDetail');
+    let needReload = !(modelCurrentNotSaved || (modelSelectedDetail && modelSelectedDetail.get('hasDirtyAttributes')));
+
+    // TODO: now 'findRecord' at ember-flexberry-projection not support 'reload: false' flag.
+    let findRecordParameters = needReload ? { reload: needReload, projection: modelProjName } : undefined;
+
     // :id param defined in router.js
-    return this.store.findRecord(modelName, params.id, {
-      reload: true,
-      projection: modelProjName
-    });
+    return this.store.findRecord(modelName, params.id, findRecordParameters);
   },
 
   resetController: function(controller, isExisting, transition) {

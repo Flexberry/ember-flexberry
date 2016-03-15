18b123d0354edc9ad0d3bace0d7e35bf2815aa53
Serega Matin <seregamatin@gmail.com>
2016-03-16 02:29:17 +0500

    Fix moment defaults initialization

--- addon/components/object-list-view-cell.js
+++ addon/components/object-list-view-cell.js
@@ -2,6 +2,7 @@
  * @module ember-flexberry
  */
 
+import Ember from 'ember';
 import FlexberryBaseComponent from './flexberry-base-component';
 
 /**
@@ -26,5 +27,27 @@ export default FlexberryBaseComponent.extend({
    * @type String
    * @default undefined
    */
-  value: undefined
+  value: undefined,
+
+  /**
+   * Formatted displaying value.
+   *
+   * @property formattedValue
+   * @type String
+   * @readonly
+   */
+  formattedValue: Ember.computed('value', function() {
+    var value = this.get('value');
+    var valueType = Ember.typeOf(value);
+
+    switch (valueType) {
+      case 'date':
+
+        // Convert date to string.
+        // Locale is current 'locale' from i18n, format is current 'moment.defaultFormat' from config/environment).
+        return this.get('moment').moment(value).format();
+      default:
+        return value;
+    }
+  })
 });

28f5ae1bc293a9b25f3483ec155f0b4eb4dc7d6e
kuzkok <kuzkok@gmail.com>
2016-03-18 13:13:23 +0100

    No fire sendAction on choose and delete if component in readonly mode

--- addon/components/flexberry-lookup.js
+++ addon/components/flexberry-lookup.js
@@ -255,9 +255,17 @@ var FlexberryLookup = FlexberryBaseComponent.extend({
 
   actions: {
     choose: function(chooseData) {
+      if (this.get('readonly')) {
+        return;
+      }
+
       this.sendAction('choose', chooseData);
     },
     remove: function(removeData) {
+      if (this.get('readonly')) {
+        return;
+      }
+
       this.sendAction('remove', removeData);
     }
   }

66359697413f6b930f2049915b146501d3a3fcbe
kuzkok <kuzkok@gmail.com>
2016-03-25 20:46:28 +0100

    Change base enum transform logic, remove unused utility

--- addon/transforms/flexberry-enum.js
+++ addon/transforms/flexberry-enum.js
@@ -3,6 +3,7 @@
  */
 
 import DS from 'ember-data';
+import { inverseEnum, enumCaptions } from '../utils/enum-functions';
 
 /**
  * Base transform class that implements an enumeration type.
@@ -16,22 +17,105 @@ let FlexberryEnum = DS.Transform.extend({
 
   /**
    * Object that contains enum values and corresponding captions.
-   * Should be defined in app/enums/.
-   * Should be overridden in inherited classes.
+   * Must be overridden in inherited classes.
    *
-   * @property transformMap
-   * @type TransformMap
+   * Should be defined in app/enums/.
+   *
+   * ```js
+   * // app/enums/order-status.js
+   * import { createEnum } from 'ember-flexberry/utils/enum-functions';
+   *
+   * export default createEnum({
+   *   Paid: 'Paid',
+   *   InProcess: 'In process',
+   *   Sended: 'Sended',
+   *   Arrived: 'Arrived',
+   *   NotArrived: 'Not arrived',
+   *   Unknown: 'Unknown'
+   * });
+   *
+   * // app/transforms/order-status
+   * import FlexberryEnum from 'ember-flexberry/transforms/flexberry-enum';
+   * import OrderStatus from '../enums/order-status';
+   *
+   * export default FlexberryEnum.extend({
+   *   enum: OrderStatus
+   * });
+   * ```
+   *
+   * @property enum
+   * @type {Object}
    * @default undefined
    * @public
    */
-  transformMap: undefined,
+  enum: undefined,
 
+  /**
+   * Object with inversed enum, value from enum property will be is property here.
+   *
+   * @property inverse
+   * @type {Object}
+   * @default undefined
+   * @public
+   * @readonly
+   */
+  inverse: undefined,
+
+  /**
+   * Array that contains all values of enum properties.
+   *
+   * @property captions
+   * @type {Array}
+   * @default undefined
+   * @public
+   * @readonly
+   */
+  captions: undefined,
+
+  /**
+   * Returns deserialized enumeration field.
+   *
+   * @method deserialize
+   * @public
+   *
+   * @param {String|Number} serialized Serialized enumeration field.
+   * @return {String} Deserialized enumeration field.
+   * Returns `null` or `undefined` if `serialized` has one of these values.
+   */
   deserialize(serialized) {
-    return this.get('transformMap').getCaption(serialized);
+    if (serialized === null || serialized === undefined) {
+      return serialized;
+    }
+
+    return this.get('enum')[serialized];
   },
 
+  /**
+   * Returns serialized enumeration field.
+   *
+   * @method serialize
+   * @public
+   *
+   * @param {String} deserialized Deserialized enumeration field.
+   * @return {String|Number} Serialized enumeration field.
+   * Returns `null` or `undefined` if `deserialized` has one of these values.
+   */
   serialize(deserialized) {
-    return this.get('transformMap').getValue(deserialized);
+    if (deserialized === null || deserialized === undefined) {
+      return deserialized;
+    }
+
+    return this.get('inverse')[deserialized];
+  },
+
+  init() {
+    let enumDictionary = this.get('enum');
+    if (!enumDictionary) {
+      throw new Error('Enum property is undefined');
+    }
+
+    this.set('inverse', inverseEnum(enumDictionary));
+    this.set('captions', enumCaptions(enumDictionary));
   }
 });
 
--- tests/unit/transforms/flexberry-enum-test.js
+++ tests/unit/transforms/flexberry-enum-test.js
@@ -1,12 +1,51 @@
 import { moduleFor, test } from 'ember-qunit';
+import { createEnum } from 'ember-flexberry/utils/enum-functions';
 
 moduleFor('transform:flexberry-enum', 'Unit | Transform | flexberry enum', {
-  // Specify the other units that are required for this test.
-  // needs: ['serializer:foo']
 });
 
-// Replace this with your real tests.
-test('it exists', function(assert) {
-  let transform = this.subject();
-  assert.ok(transform);
+let stringEnum = createEnum({
+  enumValue: 'Value for string enum property'
+});
+
+let numberEnum = createEnum({
+  32: 'Value for number enum property'
+});
+
+test('it should throw exception if no enum property set', function(assert) {
+  assert.throws(() => {
+    this.subject();
+  });
+});
+
+test('it should deserialize enum value for string enums', function(assert) {
+  let transform = this.subject({ enum: stringEnum });
+
+  let deserialized = transform.deserialize('enumValue');
+
+  assert.equal(deserialized, 'Value for string enum property');
+});
+
+test('it should serialize enum property for string enums', function(assert) {
+  let transform = this.subject({ enum: stringEnum });
+
+  let serialized = transform.serialize('Value for string enum property');
+
+  assert.equal(serialized, 'enumValue');
+});
+
+test('it should deserialize enum value for number enums', function(assert) {
+  let transform = this.subject({ enum: numberEnum });
+
+  let deserialized = transform.deserialize(32);
+
+  assert.equal(deserialized, 'Value for number enum property');
+});
+
+test('it should serialize enum property for number enums', function(assert) {
+  let transform = this.subject({ enum: numberEnum });
+
+  let serialized = transform.serialize('Value for number enum property');
+
+  assert.equal(serialized, 32);
 });

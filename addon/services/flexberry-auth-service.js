9920b2ab825872d366f6ce01c320a8b317a41ab2
hvostya <tosotova@ics.perm.ru>
2016-04-12 18:20:05 +0500

    Add service for auth

---
+++ addon/services/flexberry-auth-service.js
@@ -0,0 +1,61 @@
+/**
+ * @module ember-flexberry
+ */
+
+import Ember from 'ember';
+
+/**
+ * Service to work with authentication and authorization.
+ * It is a wrapper for real used service which is set on applied part.
+
+ Example:
+ ```js
+ // app/initializers/auth-initializer.js
+ import Ember from 'ember';
+ import FlexberryAuthService from 'ember-flexberry/services/flexberry-auth-service';
+
+ export function initialize(application) {
+   FlexberryAuthService.reopen({
+     currentAuthService: Ember.inject.service('flexberry-ember-simple-auth-service')
+   });
+ }
+
+ export default {
+   name: 'auth-initializer',
+   initialize
+ };
+ ```
+ *
+ * @class FlexberryAuthService
+ * @extends Ember.Service
+ * @public
+ */
+export default Ember.Service.extend({
+  /**
+   * Service to call auth methods.
+   * If this service is not defined, stub is executed.
+   *
+   * @property currentAuthService
+   * @type Service
+   * @default undefined
+   */
+  currentAuthService: undefined,
+
+  /**
+   * It updates custom xhr before send in order to add necessary auth information.
+   *
+   * @method authCustomRequest
+   * @param {Object} [options] Request parameters.
+   * @param {Object} options.xhr Xhr to send.
+   * @param {Object} options.element Current element which is going to request.
+   * @return {Object} Updated method parameters.
+   */
+  authCustomRequest: function(options) {
+    let currentAuthService = this.get('currentAuthService');
+    if (!currentAuthService) {
+      return options;
+    }
+
+    return currentAuthService.authCustomRequest(options);
+  }
+});
---
+++ app/services/flexberry-auth-service.js
@@ -0,0 +1,2 @@
+import FlexberryAuthService from 'ember-flexberry/services/flexberry-auth-service';
+export default FlexberryAuthService;

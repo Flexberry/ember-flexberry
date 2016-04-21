dc5c57011f91e5dda5ffe0a0d072f9e5cdd04789
kaf <kaf@nevod.ru>
2016-04-21 18:03:51 +0500

    Indent tune

--- addon/routes/i-i-s-caseberry-logging-objects-application-log-e.js
+++ addon/routes/i-i-s-caseberry-logging-objects-application-log-e.js
@@ -1,6 +1,6 @@
 import EditFormRoute from 'ember-flexberry/routes/edit-form';
 
-export default EditFormRoute.extend( {
+export default EditFormRoute.extend({
   modelProjection: 'ApplicationLogE',
   modelName: 'i-i-s-caseberry-logging-objects-application-log'
-}); 
+});
--- addon/serializers/i-i-s-caseberry-logging-objects-application-log.js
+++ addon/serializers/i-i-s-caseberry-logging-objects-application-log.js
@@ -2,10 +2,10 @@ import ApplicationSerializer from './application';
 
 // TODO: ODataSerializer.extend
 export default ApplicationSerializer.extend({
-attrs: {
+  attrs: {
 
 },
-/**
+  /**
   * Field name where object identifier is kept.
   */
   primaryKey: '__PrimaryKey'

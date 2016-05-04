671720e87d8a097fe47bbbe9ac4500decc8e3454
kaf <kaf@nevod.ru>
2016-05-05 00:54:15 +0500

    Add order and priority values to models

--- addon/mixins/olv-toolbar-mixin.js
+++ addon/mixins/olv-toolbar-mixin.js
@@ -8,22 +8,31 @@ export default Ember.Mixin.create({
       var colsConfigSettings={
         controllerName: "lookup-dialog", template: "lookup-dialog", contentTemplate: "colsconfig-dialog-content",
       };
-      let model=[];
+      let orderList=['ordernot','orderasc','orderdesc'];
+      let record,model=[];
       let projectionAttributes=this.modelProjection.attributes;
+      let order,priority=0,n=0;
       for (let prop in projectionAttributes) {
         let colName=projectionAttributes[prop].caption;
         if (projectionAttributes[prop].kind=='belongsTo') {
           colName=colName  + '@' + projectionAttributes[prop].modelName;
         }
-        model[model.length]={Name:colName}
+        order=(n%3);
+        record={name:colName};
+        record[orderList[order]]=true;
+        if (order!=0) {
+          priority+=1;
+          record['priority']=priority;
+        }
+        model[n]=record;
+        n+=1;
       }
+      alert(JSON.stringify(model));
       let controller = this.get('colsconfigController');
       var loadingParams = {
         view: 'application',
         outlet: 'modal'
       };
-//       this.send('showModalDialog', "lookup-dialog",{controller: controller},loadingParams);
-//       loadingParams.outlet='modal-content';
       this.send('showModalDialog', "colsconfig-dialog-content",
                 {controller: controller,model:model},
                 loadingParams);

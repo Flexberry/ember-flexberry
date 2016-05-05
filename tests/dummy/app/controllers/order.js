42b6df27e789de0a9ed8fba69cc43f7cdc2c8bb0
Eugene Kolegov <ekolegov@outlook.com>
2016-05-05 19:34:21 +0500

    Fix dummy app

--- tests/dummy/app/controllers/order.js
+++ tests/dummy/app/controllers/order.js
@@ -2,5 +2,14 @@ import DetailEditFormController from './detail-edit-form';
 
 export default DetailEditFormController.extend({
   // Caption of this particular edit form.
-  title: 'Order'
+  title: 'Order',
+
+  /**
+   * Route name for transition on flexberry-objectlistview
+   *
+   * @property parentRoute
+   * @type String
+   * @default 'orders'
+   */
+  parentRoute: 'orders',
 });

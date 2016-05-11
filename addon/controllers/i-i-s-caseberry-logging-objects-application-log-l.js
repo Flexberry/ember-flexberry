7c2b5c012ec82c871b03a234f9717c92275eb683
kaf <kaf@nevod.ru>
2016-05-11 19:50:48 +0500

    Change to short reexport syntax, add ember-cli-htmlbars to pachages.json

--- addon/controllers/i-i-s-caseberry-logging-objects-application-log-l.js
+++ addon/controllers/i-i-s-caseberry-logging-objects-application-log-l.js
@@ -1,7 +1,4 @@
 /**
  * @module ember-flexberry
  */
-import ListFormController from 'ember-flexberry/controllers/list-form';
-
-var IISCaseberryLoggingObjectsApplicationLogLController = ListFormController;
-export default IISCaseberryLoggingObjectsApplicationLogLController;
+export { default } from 'ember-flexberry/controllers/list-form';

b980bd8b49be19ab220120c689f041f701e2185c
Alexander Kosinsky <akosinsky@ics.perm.ru>
2016-05-12 12:18:00 +0500

    Fix serializer inheritance: task 104745

--- blueprints/ember-flexberry/index.js
+++ blueprints/ember-flexberry/index.js
@@ -5,15 +5,10 @@ module.exports = {
     return this.addBowerPackagesToProject([
       { name: 'semantic-ui-daterangepicker', target: '5d46ed2e6e5a0bf398bb6a5df82e06036dfc46be' },
       { name: 'blueimp-file-upload', target: '9.11.2' },
-      { name: 'jquery-file-download', target: '1.4.4' },
-      { name: 'devicejs', target: '0.2.7' }
+      { name: 'jquery-file-download', target: '1.4.4' }
     ]).then(function() {
-      return _this.addAddonsToProject({
-        packages: [
-          { name: 'semantic-ui-ember', target: '0.9.3' },
-          { name: 'ember-moment', target: '6.0.0' }
-        ]
-      });
+      // TODO: Since ember-cli 1.13.10 use addAddonsToProject.
+      return _this.addAddonToProject({ name: 'semantic-ui-ember', target: '0.9.3' });
     });
   },
 

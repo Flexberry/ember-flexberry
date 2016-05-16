15c894e5b04d074eac7ceeec26024d3114f8cb5e
Alexander Elyseev <alexander.elyseev@gmail.com>
2016-05-16 10:47:06 +0500

    Fix TravisCI build for test generator

--- scripts/test-generator.sh
+++ scripts/test-generator.sh
@@ -8,14 +8,18 @@ set -e
 # Create temp directory for new ember app.
 ADDON_DIR="$PWD"
 META_DIR="$PWD/vendor/flexberry/dummy-metadata"
-TMP_DIR=`mktemp -d`
-pushd "$TMPDIR"
+TMP_DIR='/tmp/embertest'
+mkdir -p "$TMP_DIR"
+rm -rf "$TMP_DIR/*"
+pushd "$TMP_DIR"
 
 # Initialize new ember app and install addon from the build.
 # EmberCLI asks whether it needs to overwrite existing files,
 # so we need to remove them for non-interactive build.
 ember init
-rm .editorconfig
+cp app/index.html .
+rm -r app/*
+mv index.html app
 ember install "${ADDON_DIR}"
 
 # Generate components using Dummy metamodel and test them.

99569e2f7586156168c8f741b9047f18287f8629
Ember Flexberry <ember@macbook.local>
2016-05-18 21:26:33 +0500

    Rename ember-flexberry-projections to ember-flexberry-data in dummy models

--- tests/dummy/app/models/ember-flexberry-dummy-suggestion.js
+++ tests/dummy/app/models/ember-flexberry-dummy-suggestion.js
@@ -1,6 +1,6 @@
 import DS from 'ember-data';
 import BaseModel from 'ember-flexberry/models/base';
-import Proj from 'ember-flexberry-projections';
+import Proj from 'ember-flexberry-data';
 
 var Model = BaseModel.extend({
   address: DS.attr('string'),

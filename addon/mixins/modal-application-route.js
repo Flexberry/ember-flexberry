76bc10f48c2ac3a3ea3b0f9d84860dc1f90acf3e
Eugene Kolegov <ekolegov@outlook.com>
2016-05-26 19:47:21 +0500

    Update ModalApplicationRoute

--- addon/mixins/modal-application-route.js
+++ addon/mixins/modal-application-route.js
@@ -1,9 +1,78 @@
+/**
+ * @module ember-flexberry
+ */
+
 import Ember from 'ember';
 
+/**
+ * Mixin contains actions for open and close modal window.
+ *
+ * Sample usage:
+ * ```javascript
+ * // app/routes/application.js
+ * import Ember from 'ember';
+ * import ModalApplicationRouteMixin from 'ember-flexberry/mixins/modal-application-route';
+ *
+ * export default Ember.Route.extend(ModalApplicationRouteMixin, {
+ * ...
+ * });
+ * ```
+ *
+ * ```javascript
+ * // app/components/your-component.js
+ * ...
+ * actions: {
+ *   ...
+ *   showMessage() {
+ *     this.sendAction('showMessage', 'message');
+ *   },
+ *   hideMessage() {
+ *     this.sendAction('hideMessage');
+ *   },
+ *   ...
+ * }
+ * ...
+ * ```
+ *
+ * ```handlebars
+ * <!-- app/templates/components/your-component.hbs -->
+ * ...
+ * <button {{action 'showMessage'}}>Show message</button>|<button {{action 'hideMessage'}}>Hide message</button>
+ * ...
+ * ```
+ *
+ * ```handlebars
+ * <!-- app/templates/application.hbs -->
+ * ...
+ * {{your-component showMessage='showModalDialog' hideMessage='removeModalDialog'}}
+ * ...
+ * {{outlet 'modal'}}
+ * ...
+ * ```
+ *
+ * ```handlebars
+ * <!-- app/templates/message.hbs -->
+ * <h1>Your message here.</h1>
+ * ```
+ *
+ * @class ModalApplicationRoute
+ * @extend [Ember.Mixin](http://emberjs.com/api/classes/Ember.Mixin.html)
+ */
 export default Ember.Mixin.create({
   actions: {
-    // Action to show modal-dialog by name.
-    showModalDialog: function(modalDialogName, data, modalParams) {
+    /**
+     * Action to show modal-dialog by name.
+     *
+     * @method actions.showModalDialog
+     * @param {String} modalDialogName Template name modal window.
+     * @param {Object} [data] Data for transfer to modal window.
+     * @param {Ember.Controller} data.controller [Controller](http://emberjs.com/api/classes/Ember.Controller.html).
+     * @param {DS.Model} data.model [Model](http://emberjs.com/api/data/classes/DS.Model.html).
+     * @param {Object} [modalParams] Object with parameters for modal window.
+     * @param {String} [modalParams.outlet] Outlet name.
+     * @param {String} [modalParams.view] Template name with outlet.
+     */
+    showModalDialog(modalDialogName, data, modalParams) {
       modalParams = this._getModalParams(modalParams);
       var params = Ember.$.extend({
         into: modalParams.view,
@@ -13,8 +82,15 @@ export default Ember.Mixin.create({
       this.render(modalDialogName, params);
     },
 
-    // Action to remove modal outlet on modal-dialog close.
-    removeModalDialog: function(modalParams) {
+    /**
+     * Action to remove modal outlet on modal-dialog close.
+     *
+     * @method actions.removeModalDialog
+     * @param {Object} [modalParams] Object with parameters for modal window.
+     * @param {String} [modalParams.outlet] Outlet name.
+     * @param {String} [modalParams.view] Template name with outlet.
+     */
+    removeModalDialog(modalParams) {
       modalParams = this._getModalParams(modalParams);
       this.disconnectOutlet({
         outlet: modalParams.outlet,
@@ -23,12 +99,21 @@ export default Ember.Mixin.create({
     }
   },
 
-  _getModalParams: function(modalParams) {
+  /**
+   * Validate modalParams object.
+   *
+   * @method _getModalParams
+   * @param {Object} [modalParams] Object with parameters for modal window.
+   * @param {String} [modalParams.outlet] Outlet name.
+   * @param {String} [modalParams.view] Template name with outlet.
+   * @private
+   */
+  _getModalParams(modalParams) {
     modalParams = modalParams || {};
 
-    var params = {};
+    let params = {};
     params.outlet = modalParams.outlet || 'modal';
     params.view = modalParams.view || 'application';
     return params;
-  }
+  },
 });

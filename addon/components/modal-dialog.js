167659fe742f3a0535f48a3d6a783b82ad0352ad
Eugene Kolegov <ekolegov@outlook.com>
2016-05-26 19:46:09 +0500

    Update ModalDialog

--- addon/components/modal-dialog.js
+++ addon/components/modal-dialog.js
@@ -5,41 +5,28 @@
 import Ember from 'ember';
 
 /**
- * ModalDialog component for SemanticUI.
+ * ModalDialog component for Semantic UI.
+ *
+ * Sample usage:
+ * ```handlebars
+ * {{#modal-dialog
+ *   title='Title'
+ *   sizeClass='large'
+ *   close='removeModalDialog'
+ *   created='createdModalDialog'
+ *   useOkButton=false
+ *   useCloseButton=false
+ * }}
+ *   {{outlet 'modal-content'}}
+ * {{/modal-dialog}}
+ * ```
  *
  * @class ModalDialog
- * @extends Ember.Component
+ * @extends [Ember.Component](http://emberjs.com/api/classes/Ember.Component.html)
  */
 export default Ember.Component.extend({
   /**
-   * Flag indicates whether to show apply button or not.
-   *
-   * @property useOkButton
-   * @type Boolean
-   * @default true
-   */
-  useOkButton: true,
-
-  /**
-   * Flag indicates whether to show close button or not.
-   *
-   * @property useCloseButton
-   * @type Boolean
-   * @default true
-   */
-  useCloseButton: true,
-
-  /**
-   * Flag indicates whether an image content is viewed at modal dialog or not.
-   *
-   * @property viewImageContent
-   * @type Boolean
-   * @default false
-   */
-  viewImageContent: false,
-
-  /**
-   * Size of Semantic-UI modal.
+   * Size of Semantic UI modal.
    * Possible variants: 'small', 'large', 'fullscreen'.
    *
    * @property sizeClass
@@ -49,21 +36,45 @@ export default Ember.Component.extend({
   sizeClass: 'small',
 
   /**
-   * Flag: indicates buttons toolbar visibility,
-   * `true` if at least one of buttons is visible.
+   * Flag: indicates whether an image content is viewed at modal dialog or not.
+   *
+   * @property viewImageContent
+   * @type Boolean
+   * @default false
+   */
+  viewImageContent: false,
+
+  /**
+   * Flag: indicates whether to show apply button or not.
+   *
+   * @property useOkButton
+   * @type Boolean
+   * @default true
+   */
+  useOkButton: true,
+
+  /**
+   * Flag: indicates whether to show close button or not.
+   *
+   * @property useCloseButton
+   * @type Boolean
+   * @default true
+   */
+  useCloseButton: true,
+
+  /**
+   * Flag: indicates toolbar visibility, `true` if at least one of buttons is visible.
    *
    * @property toolbarVisible
    * @type Boolean
-   * @default true
-   * @readonly
+   * @readOnly
    */
   toolbarVisible: Ember.computed('useOkButton', 'useCloseButton', function () {
     return this.get('useOkButton') || this.get('useCloseButton');
   }),
 
   /**
-   * Semantic-UI Modal settings,
-   * more info at http://semantic-ui.com/modules/modal.html#settings
+   * Semantic UI Modal settings, [more info here](http://semantic-ui.com/modules/modal.html#settings).
    *
    * @property settings
    * @type Object
@@ -71,7 +82,10 @@ export default Ember.Component.extend({
    */
   settings: {},
 
-  didInsertElement: function () {
+  /**
+   * Initializes DOM-related component's logic.
+   */
+  didInsertElement() {
     let _this = this;
     let modalSettings = Ember.$.extend({
         observeChanges: true,
@@ -97,5 +111,5 @@ export default Ember.Component.extend({
       _this.get('settings'));
 
     this.$('.ui.modal').modal(modalSettings).modal('show');
-  }
+  },
 });

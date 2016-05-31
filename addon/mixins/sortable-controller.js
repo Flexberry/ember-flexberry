2b356d936c8f6692afdc07afb287b2c100f7b638
Eugene Kolegov <ekolegov@outlook.com>
2016-05-31 14:13:33 +0500

    Update SortableController

--- addon/mixins/sortable-controller.js
+++ addon/mixins/sortable-controller.js
@@ -1,22 +1,77 @@
+/**
+  @module ember-flexberry
+ */
+
 import Ember from 'ember';
 
+/**
+  Mixin for controller, that sorting on the list form.
+
+  Example:
+  ```javascript
+  // app/controllers/employees.js
+  import Ember from 'ember';
+  import SortableController from 'ember-flexberry/mixins/sortable-controller'
+  export default Ember.Controller.extend(SortableController, {
+  });
+  ```
+
+  ```javascript
+  // app/routes/employees.js
+  import Ember from 'ember';
+  import SortableRoute from 'ember-flexberry/mixins/sortable-route'
+  export default Ember.Route.extend(SortableRoute, {
+  });
+  ```
+
+  ```handlebars
+  <!-- app/templates/employees.hbs -->
+  ...
+  {{flexberry-objectlistview
+    ...
+    orderable=true
+    sortByColumn=(action 'sortByColumn')
+    addColumnToSorting=(action 'addColumnToSorting')
+    ...
+  }}
+  ...
+  ```
+
+  @class SortableController
+  @uses <a href="http://emberjs.com/api/classes/Ember.Mixin.html">Ember.Mixin</a>
+ */
 export default Ember.Mixin.create({
-  queryParams: ['sort'],
+  /**
+    Default value for sorting.
+
+    @property sortDefaultValue
+    @type String
+   */
   sortDefaultValue: undefined,
+
+  /**
+    String with sorting parameters.
+
+    @property sort
+    @type String
+   */
   sort: Ember.computed.oneWay('sortDefaultValue'),
 
-  getNextSortDirection: function(currentDirection) {
-    return currentDirection === 'asc' ? 'desc' : 'none';
-  },
+  /**
+    Object with sorting parameters for model.
 
+    @property computedSorting
+    @type Object
+    @readOnly
+   */
   computedSorting: Ember.computed('model.sorting', function() {
-    var sorting = this.get('model.sorting');
-    var result = {};
+    let sorting = this.get('model.sorting');
+    let result = {};
 
     if (sorting) {
-      for (var i = 0; i < sorting.length; i++) {
-        var propName = sorting[i].propName;
-        var sortDef = {
+      for (let i = 0; i < sorting.length; i++) {
+        let propName = sorting[i].propName;
+        let sortDef = {
           sortAscending: sorting[i].direction === 'asc' ? true : false,
           sortNumber: i + 1
         };
@@ -28,28 +83,31 @@ export default Ember.Mixin.create({
   }),
 
   /**
-   * Производит операцию, обратную методу deserializeSortingParam.
-   *
-   * @param {Object} sorting Объект с параметрами сортировки, который нужно преобразовать в строку.
-   * @returns {String} Результирующая строка.
+    Defines which query parameters the controller accepts. [More info.](http://emberjs.com/api/classes/Ember.Controller.html#property_queryParams).
+
+    @property queryParams
+    @type Array
+    @default ['sort']
    */
-  serializeSortingParam: function(sorting) {
-    return sorting.map(function(element) {
-      return (element.direction === 'asc' ? '+' : '-') + element.propName;
-    }).join('') || this.get('sortDefaultValue');
-  },
+  queryParams: ['sort'],
 
   actions: {
-    sortByColumn: function(column) {
-      var propName = column.propName;
-      var oldSorting = this.get('model.sorting');
-      var newSorting = [];
-      var sortDirection;
+    /**
+      Sorting list by column.
+
+      @method actions.sortByColumn
+      @param {Object} column Column for sorting.
+     */
+    sortByColumn(column) {
+      let propName = column.propName;
+      let oldSorting = this.get('model.sorting');
+      let newSorting = [];
+      let sortDirection;
       if (oldSorting) {
         sortDirection = 'asc';
-        for (var i = 0; i < oldSorting.length; i++) {
+        for (let i = 0; i < oldSorting.length; i++) {
           if (oldSorting[i].propName === propName) {
-            sortDirection = this.getNextSortDirection(oldSorting[i].direction);
+            sortDirection = this._getNextSortDirection(oldSorting[i].direction);
             break;
           }
         }
@@ -61,19 +119,25 @@ export default Ember.Mixin.create({
         newSorting.push({ propName: propName, direction: sortDirection });
       }
 
-      let sortQueryParam = this.serializeSortingParam(newSorting);
+      let sortQueryParam = this._serializeSortingParam(newSorting);
       this.set('sort', sortQueryParam);
     },
 
-    addColumnToSorting: function(column) {
-      var propName = column.propName;
-      var oldSorting = this.get('model.sorting');
-      var newSorting = [];
-      var changed = false;
+    /**
+      Add column into end list sorting.
 
-      for (var i = 0; i < oldSorting.length; i++) {
+      @method actions.addColumnToSorting
+      @param {Object} column Column for sorting.
+     */
+    addColumnToSorting(column) {
+      let propName = column.propName;
+      let oldSorting = this.get('model.sorting');
+      let newSorting = [];
+      let changed = false;
+
+      for (let i = 0; i < oldSorting.length; i++) {
         if (oldSorting[i].propName === propName) {
-          var newDirection = this.getNextSortDirection(oldSorting[i].direction);
+          let newDirection = this._getNextSortDirection(oldSorting[i].direction);
           if (newDirection !== 'none') {
             newSorting.push({ propName: propName, direction: newDirection });
           }
@@ -88,8 +152,34 @@ export default Ember.Mixin.create({
         newSorting.push({ propName: propName, direction: 'asc' });
       }
 
-      let sortQueryParam = this.serializeSortingParam(newSorting);
+      let sortQueryParam = this._serializeSortingParam(newSorting);
       this.set('sort', sortQueryParam);
-    }
-  }
+    },
+  },
+
+  /**
+    Convert object with sorting parameters into string.
+
+    @method _serializeSortingParam
+    @param {Object} sorting Object with sorting parameters.
+    @returns {String} String with sorting parameters.
+    @private
+   */
+  _serializeSortingParam(sorting) {
+    return sorting.map((element) => {
+      return (element.direction === 'asc' ? '+' : '-') + element.propName;
+    }).join('') || this.get('sortDefaultValue');
+  },
+
+  /**
+    Get next sorting direction.
+
+    @method _getNextSortDirection
+    @param {String} currentDirection Current sorting direction.
+    @return {String} Sorting direction.
+    @private
+   */
+  _getNextSortDirection(currentDirection) {
+    return currentDirection === 'asc' ? 'desc' : 'none';
+  },
 });

ecc1f74caa0aa1ef25f8f562d42afd9a0966575d
Eugene Kolegov <ekolegov@outlook.com>
2016-06-08 13:28:02 +0500

    Update controllers and routes

--- addon/routes/i-i-s-caseberry-logging-objects-application-log-e.js
+++ addon/routes/i-i-s-caseberry-logging-objects-application-log-e.js
@@ -1,9 +1,31 @@
 /**
- * @module ember-flexberry
+  @module ember-flexberry
  */
+
 import EditFormRoute from 'ember-flexberry/routes/edit-form';
 
+/**
+  Route for support edit form {{#crossLink "IISCaseberryLoggingObjectsApplicationLog"}}IISCaseberryLoggingObjectsApplicationLog{{/crossLink}} record.
+
+  @class IISCaseberryLoggingObjectsApplicationLogERoute
+  @extends EditFormRoute
+ */
 export default EditFormRoute.extend({
+  /**
+    Projection name.
+
+    @property modelProjection
+    @type String
+    @default 'ApplicationLogE'
+   */
   modelProjection: 'ApplicationLogE',
-  modelName: 'i-i-s-caseberry-logging-objects-application-log'
+
+  /**
+    Model name.
+
+    @property modelName
+    @type String
+    @default 'i-i-s-caseberry-logging-objects-application-log'
+   */
+  modelName: 'i-i-s-caseberry-logging-objects-application-log',
 });
--- addon/routes/i-i-s-caseberry-logging-objects-application-log-l.js
+++ addon/routes/i-i-s-caseberry-logging-objects-application-log-l.js
@@ -1,9 +1,31 @@
 /**
- * @module ember-flexberry
+  @module ember-flexberry
  */
+
 import ListFormRoute from 'ember-flexberry/routes/list-form';
 
+/**
+  Route for support list form {{#crossLink "IISCaseberryLoggingObjectsApplicationLog"}}IISCaseberryLoggingObjectsApplicationLog{{/crossLink}} record.
+
+  @class IISCaseberryLoggingObjectsApplicationLogLRoute
+  @extends ListFormRoute
+ */
 export default ListFormRoute.extend({
+  /**
+    Projection name.
+
+    @property modelProjection
+    @type String
+    @default 'ApplicationLogL'
+   */
   modelProjection: 'ApplicationLogL',
-  modelName: 'i-i-s-caseberry-logging-objects-application-log'
+
+  /**
+    Model name.
+
+    @property modelName
+    @type String
+    @default 'i-i-s-caseberry-logging-objects-application-log'
+   */
+  modelName: 'i-i-s-caseberry-logging-objects-application-log',
 });

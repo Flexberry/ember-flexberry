d4086becff86f95cafe29e37a2f54caaf272f760
hvostya <tosotova@ics.perm.ru>
2016-06-09 13:07:46 +0500

    Fix unnecessary reference

--- addon/routes/list-form.js
+++ addon/routes/list-form.js
@@ -8,7 +8,6 @@ import PaginatedRouteMixin from '../mixins/paginated-route';
 import LimitedRouteMixin from '../mixins/limited-route';
 import FlexberryObjectlistviewRouteMixin from '../mixins/flexberry-objectlistview-route';
 import ProjectedModelFormRoute from '../routes/projected-model-form';
-import QueryBuilder from 'ember-flexberry-data/query/builder';
 import ReloadListMixin from '../mixins/reload-list-mixin';
 
 /**

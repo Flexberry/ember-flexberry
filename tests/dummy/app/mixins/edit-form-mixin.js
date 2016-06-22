2882030e202d37c5d3b0a7f7cc2db4c5775273f2
Nasutya <nastia@bratchikovy.ru>
2016-06-22 11:19:25 +0500

    Add comment.

--- tests/dummy/app/mixins/edit-form-mixin.js
+++ tests/dummy/app/mixins/edit-form-mixin.js
@@ -2,7 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Mixin.create({
   /**
-  Base edit form route for dummy application.
+  * Сleaning of the changes on edit form.
+  *
+  * @method resetController
   **/
   resetController: function(controller, isExisting, transition) {
     this._super.apply(this, arguments);

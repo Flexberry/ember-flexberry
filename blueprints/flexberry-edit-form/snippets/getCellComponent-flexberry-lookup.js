cb606cf1d469c62ec65b45710ea51b980484743c
akosinsky <akosinsky@ics.perm.ru>
2016-06-29 13:08:48 +0500

    Feature customization lookups in flexberry-groupedit
    TFS #108280

---
+++ blueprints/flexberry-edit-form/snippets/getCellComponent-flexberry-lookup.js
@@ -0,0 +1,14 @@
+﻿          case '<%= master %>':
+            return {
+              componentName: 'flexberry-lookup',
+              componentProperties: {
+                choose: 'showLookupDialog',
+                chooseText: '...',
+                remove: 'removeLookupValue',
+                displayAttributeName: '<%= displayAttributeName %>',
+                required: <%= required %>,
+                relationName: '<%= master %>',
+                projection: '<%= projection %>',
+                autocomplete: true
+              }
+            };
---
+++ blueprints/flexberry-edit-form/snippets/getCellComponent-function.js
@@ -0,0 +1,8 @@
+﻿function(attr, bindingPath) {
+    if (attr.kind === 'belongsTo') {
+      switch (bindingPath) {
+<%= bodySwitchBindingPath %>
+      }
+    }
+    return this._super(...arguments);
+  }

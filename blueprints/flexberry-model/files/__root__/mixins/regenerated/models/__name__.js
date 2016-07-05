5ed5db38f25c420935630a92aa82deb955dacd22
Alexander Kosinsky <akosinsky@ics.perm.ru>
2016-07-05 17:35:49 +0500

    Fix travis error 2

--- blueprints/flexberry-model/files/__root__/mixins/regenerated/models/__name__.js
+++ blueprints/flexberry-model/files/__root__/mixins/regenerated/models/__name__.js
@@ -2,12 +2,12 @@ import Ember from 'ember';
 import DS from 'ember-data';
 <%if(projections) {%>import Proj from 'ember-flexberry-data';<%}%>
 let MixinModel = Ember.Mixin.create({
-  init: function (){
+  init: function () {
     this._super(...arguments);
     this.defineProjections();
   },
 <%= model %>,
-  defineProjections: function(){
+  defineProjections: function() {
 <%= projections %>
   }
 });

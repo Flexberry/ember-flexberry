482e87ff21171a76c44609cb6e7c26b3b4e828ae
Alexander Kosinsky <akosinsky@ics.perm.ru>
2016-07-08 17:10:38 +0500

    Feature generation edit-form-mixin.js for routes
    TFS #108453

--- blueprints/flexberry-edit-form/files/__root__/routes/__name__.js
+++ blueprints/flexberry-edit-form/files/__root__/routes/__name__.js
@@ -1,6 +1,7 @@
 import EditFormRoute from 'ember-flexberry/routes/edit-form';
+import EditFormMixin from '../../mixins/edit-form-mixin';
 
-export default EditFormRoute.extend({
+export default EditFormRoute.extend(EditFormMixin, {
   modelProjection: '<%=modelProjection%>',
   modelName: '<%=modelName%>'
 });
--- blueprints/flexberry-edit-form/files/__root__/routes/__name__/new.js
+++ blueprints/flexberry-edit-form/files/__root__/routes/__name__/new.js
@@ -1,6 +1,7 @@
 import EditFormNewRoute from 'ember-flexberry/routes/edit-form-new';
+import EditFormMixin from '../../mixins/edit-form-mixin';
 
-export default EditFormNewRoute.extend({
+export default EditFormNewRoute.extend(EditFormMixin, {
   modelProjection: '<%=modelProjection%>',
   modelName: '<%=modelName%>',
   templateName: '<%=entityName%>',

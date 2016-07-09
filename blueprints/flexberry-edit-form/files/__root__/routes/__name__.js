e4a22bb122517872ef1cb5559f007827f09605de
Alexander Kosinsky <akosinsky@ics.perm.ru>
2016-07-09 07:40:29 +0500

    Fix travis error

--- blueprints/flexberry-edit-form/files/__root__/routes/__name__.js
+++ blueprints/flexberry-edit-form/files/__root__/routes/__name__.js
@@ -1,5 +1,5 @@
 import EditFormRoute from 'ember-flexberry/routes/edit-form';
-import EditFormMixin from '../../mixins/edit-form-mixin';
+import EditFormMixin from '../mixins/edit-form-mixin';
 
 export default EditFormRoute.extend(EditFormMixin, {
   modelProjection: '<%=modelProjection%>',

e2b98221d0eedd516cebd43d128b9d7766704924
Alexander Kosinsky <akosinsky@ics.perm.ru>
2016-07-11 21:19:34 +0500

    Fix travis error 2

--- blueprints/flexberry-model-init/files/__root__/models/__name__.js
+++ blueprints/flexberry-model-init/files/__root__/models/__name__.js
@@ -1,6 +1,7 @@
 import { MixinModel as <%= className %>Mixin<%if (projections) {%>, defineProjections<%}%> } from '../mixins/regenerated/models/<%= name %>';
 import <%if(parentModelName) {%><%= parentClassName %>Model from './<%= parentModelName %>';<%}else{%>__BaseModel from './base';<%}%>
 let Model = <%if(parentModelName) {%><%= parentClassName %>Model.extend<%}else{%>__BaseModel.extend<%}%>(<%= className %>Mixin, {
+
 });
 
 <%if(projections) {%>defineProjections(Model);<%}%>
--- blueprints/flexberry-model/files/__root__/mixins/regenerated/models/__name__.js
+++ blueprints/flexberry-model/files/__root__/mixins/regenerated/models/__name__.js
@@ -3,6 +3,7 @@ import DS from 'ember-data';
 <%if(projections) {%>import Proj from 'ember-flexberry-data';<%}%>
 export let MixinModel = Ember.Mixin.create({
 <%= model %>,
+
 });
 
 <%if(projections) {%>export let defineProjections = function(model) {<%}%>

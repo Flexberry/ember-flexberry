0d9dd3cff644a0763ea03f7fae30e97ee2b490d9
yzaripova <zaripovaya@gmail.com>
2016-07-12 12:41:53 +0500

    Fix example lookup in modal
    tfs #111069

--- tests/dummy/app/controllers/components-examples/flexberry-lookup/lookup-in-modal.js
+++ tests/dummy/app/controllers/components-examples/flexberry-lookup/lookup-in-modal.js
@@ -4,25 +4,30 @@ import EditFormController from 'ember-flexberry/controllers/edit-form';
 export default EditFormController.extend({
 
   actions: {
-    modalwindow: function() {
-      var repeatWindow = Ember.$('#repeat-window').modal({ closable: false });
+    modalwindow() {
+      let repeatWindow = Ember.$('#repeat-window').modal({
+        closable: false,
+        autofocus: false,
+      });
       this.set('repeatWindow', repeatWindow);
       this.get('repeatWindow').modal('refresh').modal('show');
     },
 
-    modalwindowdouble: function() {
-      var repeatWindow = Ember.$('#repeat-window-double').modal({ closable: false });
+    modalwindowdouble() {
+      let repeatWindow = Ember.$('#repeat-window-double').modal({
+        closable: false,
+        autofocus: false,
+      });
       this.set('repeatWindowdouble', repeatWindow);
       this.get('repeatWindowdouble').modal('refresh').modal('show');
     },
 
-    logout: function() {
+    logout() {
       this.get('repeatWindow').modal('hide');
     },
 
-    logoutdouble: function() {
+    logoutdouble() {
       this.get('repeatWindowdouble').modal('hide');
     }
-
   }
 });

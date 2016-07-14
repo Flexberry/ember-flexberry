7d3363cf09dfc5c25af6056abfe1ba4642fb9de4
erhead <erhead@yandex.ru>
2016-07-14 11:10:19 +0500

    Add custom boolean captions to object-list-view

--- addon/components/object-list-view-cell.js
+++ addon/components/object-list-view-cell.js
@@ -44,12 +44,15 @@ export default FlexberryBaseComponent.extend({
 
     switch (valueType) {
       case 'date':
-
         // Convert date to string.
         // Locale is current 'locale' from i18n, format is current 'moment.defaultFormat' from config/environment).
         let momentValue = this.get('moment').moment(value);
         let dateFormat = this.get('dateFormat');
         return dateFormat ? momentValue.format(dateFormat) : momentValue.format();
+      case 'boolean':
+        return value 
+          ? this.get('i18n').t('components.object-list-view-cell.boolean-true-caption')
+          : this.get('i18n').t('components.object-list-view-cell.boolean-false-caption');
       default:
         return value;
     }

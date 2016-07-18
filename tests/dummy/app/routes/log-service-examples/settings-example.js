a4f94b691150dd85bbf011aa513d58c26d0485b4
kaf <kaf@nevod.ru>
2016-07-18 17:48:13 +0500

    Add developerUserSettings

--- tests/dummy/app/routes/log-service-examples/settings-example.js
+++ tests/dummy/app/routes/log-service-examples/settings-example.js
@@ -10,6 +10,219 @@ export default ApllicationLogListFormRoute.extend({
   */
   modelProjection: 'ApplicationLogL',
 
+ /**
+  developerUserSettings.
+  {
+  <componentName>: {
+    <settingName>: {
+        colsOrder: [ { propName :<colName>, hide: true|false }, ... ],
+        sorting: [{ propName: <colName>, direction: 'asc'|'desc' }, ... ],
+        colsWidths: [ <colName>:<colWidth>, ... ],
+      },
+      ...
+    },
+    ...
+  }
+  For default userSetting use empty name ('').
+  <componentName> may contain any of properties: colsOrder, sorting, colsWidth or being empty.
+
+  @property developerUserSettings
+  @type Object
+  @default {}
+  */
+  developerUserSettings: { IISLoggingObjectListView: `
+    {
+      "DEFAULT": {
+        "colsOrder": [
+        {
+          "propName": "timestamp",
+          "hide": false
+        },
+        {
+          "propName": "category"
+        },
+        {
+          "propName": "machineName"
+        },
+        {
+          "propName": "appDomainName"
+        },
+        {
+          "propName": "processId"
+        },
+        {
+          "propName": "message"
+        },
+        {
+          "propName": "processName",
+          "hide": true
+        },
+        {
+          "propName": "formattedMessage",
+          "hide": true
+        },
+        {
+          "propName": "eventId",
+          "hide": true
+        },
+        {
+          "propName": "priority",
+          "hide": true
+        },
+        {
+          "propName": "severity",
+          "hide": true
+        },
+        {
+          "propName": "title",
+          "hide": true
+        },
+        {
+          "propName": "threadName",
+          "hide": true
+        },
+        {
+          "propName": "win32ThreadId",
+          "hide": true
+        }
+        ],
+        "sorting": [
+        {
+          "propName": "timestamp",
+          "direction": "desc",
+          "sortPriority": 1
+        }
+        ]
+      },
+      "Message": {
+        "colsOrder": [
+        {
+          "propName": "timestamp",
+          "hide": false
+        },
+        {
+          "propName": "category"
+        },
+        {
+          "propName": "machineName"
+        },
+        {
+          "propName": "appDomainName"
+        },
+        {
+          "propName": "processId"
+        },
+        {
+          "propName": "message"
+        },
+        {
+          "propName": "processName",
+          "hide": true
+        },
+        {
+          "propName": "formattedMessage",
+          "hide": true
+        },
+        {
+          "propName": "eventId",
+          "hide": true
+        },
+        {
+          "propName": "priority",
+          "hide": true
+        },
+        {
+          "propName": "severity",
+          "hide": true
+        },
+        {
+          "propName": "title",
+          "hide": true
+        },
+        {
+          "propName": "threadName",
+          "hide": true
+        },
+        {
+          "propName": "win32ThreadId",
+          "hide": true
+        }
+        ],
+        "sorting": [
+        {
+          "propName": "timestamp",
+          "direction": "desc",
+          "sortPriority": 1
+        }
+        ]
+      },
+      "FormattedMessage": {
+        "colsOrder": [
+        {
+          "propName": "timestamp",
+          "hide": false
+        },
+        {
+          "propName": "category"
+        },
+        {
+          "propName": "machineName"
+        },
+        {
+          "propName": "appDomainName"
+        },
+        {
+          "propName": "processId"
+        },
+        {
+          "propName": "formattedMessage",
+          "hide": false
+        },
+        {
+          "propName": "message",
+          "hide": true
+        },
+        {
+          "propName": "processName",
+          "hide": true
+        },
+        {
+          "propName": "eventId",
+          "hide": true
+        },
+        {
+          "propName": "priority",
+          "hide": true
+        },
+        {
+          "propName": "severity",
+          "hide": true
+        },
+        {
+          "propName": "title",
+          "hide": true
+        },
+        {
+          "propName": "threadName",
+          "hide": true
+        },
+        {
+          "propName": "win32ThreadId",
+          "hide": true
+        }
+        ],
+        "sorting": [
+        {
+          "propName": "timestamp",
+          "direction": "desc",
+          "sortPriority": 1
+        }
+        ]
+      }
+    }
+    `
+  },
+
   /**
     Name of model to be used as list's records types.
 

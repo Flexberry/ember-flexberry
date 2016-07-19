644ef447f738122195d3b45551714ffe85d7284f
Eugene Kolegov <ekolegov@outlook.com>
2016-07-19 12:53:47 +0500

    Docs object-list-view-row

--- addon/components/object-list-view-row.js
+++ addon/components/object-list-view-row.js
@@ -6,23 +6,49 @@ import Ember from 'ember';
 import FlexberryBaseComponent from './flexberry-base-component';
 
 /**
+  Component for displayed one record in {{#crossLink "ObjectListViewComponent"}}{{/crossLink}}.
+
   @class ObjectListViewRowComponent
   @extends FlexberryBaseComponent
 */
 export default FlexberryBaseComponent.extend({
   /**
+    Flag used to display embedded records.
+
+    @property _expanded
+    @type Boolean
+    @default false
+    @private
   */
   _expanded: false,
 
   /**
+    Store the string for building hierarchical indent.
+
+    @property _hierarchicalIndent
+    @type String
+    @default '&nbsp;'
+    @private
   */
   _hierarchicalIndent: '&nbsp;',
 
   /**
+    Level nesting by default.
+
+    @property _level
+    @type Number
+    @default 0
+    @private
   */
   _level: 0,
 
   /**
+    Level nesting current record.
+
+    @property _currentLevel
+    @type Number
+    @default 0
+    @private
   */
   _currentLevel: Ember.computed({
     get() {
@@ -34,10 +60,23 @@ export default FlexberryBaseComponent.extend({
   }),
 
   /**
+    Store nested records.
+
+    @property _records
+    @type Ember.NativeArray
+    @default Empty
+    @private
   */
   _records: Ember.computed(() => Ember.A()),
 
   /**
+    Current record.
+    - `key` - Ember GUID for record.
+    - `data` - Instance of DS.Model.
+    - `config` - Object with config for record.
+
+    @property record
+    @type Object
   */
   record: Ember.computed(() => ({
     key: undefined,
@@ -46,6 +85,11 @@ export default FlexberryBaseComponent.extend({
   })),
 
   /**
+    Store nested records.
+
+    @property records
+    @type Ember.NativeArray
+    @default Empty
   */
   records: Ember.computed({
     get() {
@@ -75,12 +119,22 @@ export default FlexberryBaseComponent.extend({
   }),
 
   /**
+    Flag indicate whether there nested records.
+
+    @property hasRecords
+    @type Boolean
+    @default false
   */
   hasRecords: Ember.computed('records.length', function() {
     return this.get('records.length');
   }),
 
   /**
+    Hierarchical indent based on the current level of nesting.
+
+    @property hierarchicalIndent
+    @type String
+    @default ''
   */
   hierarchicalIndent: Ember.computed({
     get() {
@@ -103,23 +157,39 @@ export default FlexberryBaseComponent.extend({
   }),
 
   /**
+    Tag name for the view's outer element. [More info](http://emberjs.com/api/classes/Ember.Component.html#property_tagName).
+
+    @property tagName
+    @type String
+    @default ''
   */
   tagName: '',
 
   actions: {
     /**
+      Show/hide embedded records.
+
+      @method actions.expand
     */
     expand() {
       this.toggleProperty('_expanded');
     },
 
     /**
+      Redirect action from FlexberryLookupComponent in the controller.
+
+      @method actions.showLookupDialog
+      @param {Object} chooseData
     */
     showLookupDialog(chooseData) {
       this.get('currentController').send('showLookupDialog', chooseData);
     },
 
     /**
+      Redirect action from FlexberryLookupComponent in the controller.
+
+      @method actions.removeLookupValue
+      @param {Object} removeData
     */
     removeLookupValue(removeData) {
       this.get('currentController').send('removeLookupValue', removeData);
@@ -127,6 +197,9 @@ export default FlexberryBaseComponent.extend({
   },
 
   /**
+    Setup a view, but do not finish waking it up. [More info](http://emberjs.com/api/classes/Ember.Component.html#method_init).
+
+    @method init
   */
   init() {
     this._super(...arguments);

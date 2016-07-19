15b2317b2aedd975ebe13777f4119a363fcb4e7c
Eugene Kolegov <ekolegov@outlook.com>
2016-07-19 12:55:31 +0500

    Docs flexberry-objectlistview-hierarchical-route

--- addon/mixins/flexberry-objectlistview-hierarchical-route.js
+++ addon/mixins/flexberry-objectlistview-hierarchical-route.js
@@ -6,11 +6,20 @@ import Ember from 'ember';
 import QueryBuilder from 'ember-flexberry-data/query/builder';
 
 /**
+  Mixin for [Ember.Route](http://emberjs.com/api/classes/Ember.Route.html) to support hierarchical mode into {{#crossLink "FlexberryObjectlistviewComponent"}}{{/crossLink}}.
+
   @class FlexberryObjectlistviewHierarchicalRouteMixin
+  @uses <a href="http://emberjs.com/api/classes/Ember.Mixin.html">Ember.Mixin</a>
 */
 export default Ember.Mixin.create({
   actions: {
     /**
+      Set in `property` for `target` promise that load nested records.
+
+      @method actions.loadRecordsById
+      @param {String} id Record ID.
+      @param {ObjectListViewRowComponent} Instance of {{#crossLink "ObjectListViewRowComponent"}}{{/crossLink}}.
+      @param {String} property Property name into {{#crossLink "ObjectListViewRowComponent"}}{{/crossLink}}.
     */
     loadRecordsById(id, target, property) {
       let hierarchicalAttribute = this.controllerFor(this.routeName).get('hierarchicalAttribute');

8ec20a5ffe7d1f2261ab873377e62a3339542517
Eugene Kolegov <ekolegov@outlook.com>
2016-07-28 20:35:32 +0500

    Fix flexberry-list-form blueprint generate developerUserSettings

--- blueprints/flexberry-list-form/files/__root__/routes/__name__.js
+++ blueprints/flexberry-list-form/files/__root__/routes/__name__.js
@@ -6,16 +6,41 @@ export default ListFormRoute.extend({
 
     @property modelProjection
     @type String
-    @default '<%= modelProjection %>'
-   */
-  modelProjection: '<%= modelProjection %>',
+    @default '<%=modelProjection%>'
+  */
+  modelProjection: '<%=modelProjection%>',
 
   /**
     Name of model to be used as list's records types.
 
     @property modelName
     @type String
-    @default '<%= modelName %>'
-   */
-  modelName: '<%= modelName %>'
+    @default '<%=modelName%>'
+  */
+  modelName: '<%=modelName%>',
+
+  /**
+    Defined user settings developer.
+    For default userSetting use empty name ('').
+    Property `<componentName>` may contain any of properties: `colsOrder`, `sorting`, `colsWidth` or being empty.
+
+    ```javascript
+    {
+      <componentName>: {
+        <settingName>: {
+          colsOrder: [ { propName :<colName>, hide: true|false }, ... ],
+          sorting: [{ propName: <colName>, direction: "asc"|"desc" }, ... ],
+          colsWidths: [ <colName>:<colWidth>, ... ],
+        },
+        ...
+      },
+      ...
+    }
+    ```
+
+    @property developerUserSettings
+    @type Object
+    @default {}
+  */
+  developerUserSettings: { <%= formName %>: {} },
 });

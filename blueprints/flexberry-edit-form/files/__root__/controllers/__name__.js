bf4cc2a375e1e27c11ff4cf070f710fb27de8737
Alexander Kosinsky <akosinsky@ics.perm.ru>
2016-07-29 13:03:21 +0500

    Fix edit-form title

--- blueprints/flexberry-edit-form/files/__root__/controllers/__name__.js
+++ blueprints/flexberry-edit-form/files/__root__/controllers/__name__.js
@@ -2,7 +2,7 @@ import EditFormController from 'ember-flexberry/controllers/edit-form';
 
 export default EditFormController.extend({
   // Caption of this particular edit form.
-  title: '<%= caption %>',<%if(parentRoute){%>
+  <%if(parentRoute){%>
   parentRoute: '<%= parentRoute %>',<%}if (functionGetCellComponent) {%>
   getCellComponent: <%= functionGetCellComponent %>,<%}%>
 });

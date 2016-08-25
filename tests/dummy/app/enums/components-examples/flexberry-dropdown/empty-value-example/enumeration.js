580d563984c02ffc816437c67e41a548b6ef336a
Sokolov Andrey <s.andrey-2011@yandex.ru>
2016-08-25 15:30:34 +0500

    Add test dropdown with empty value

---
+++ tests/dummy/app/enums/components-examples/flexberry-dropdown/empty-value-example/enumeration.js
@@ -0,0 +1,8 @@
+import { createEnum } from 'ember-flexberry-data/utils/enum-functions';
+
+export default createEnum({
+  Value1: '',
+  Value2: 'Enum value №1',
+  Value3: 'Enum value №2',
+  Value4: 'Enum value №3',
+});

f7d2679bf5d4d69ceaf29a6773921ae1d9a87cd2
Bratchikov Igor <bratchikov@ics.perm.ru>
2016-09-01 15:44:50 +0500

    Add explicit type definition to flexberry-field Default type is still 'text'. Also flexberry-textbox include same changes.

--- addon/components/flexberry-field.js
+++ addon/components/flexberry-field.js
@@ -53,6 +53,15 @@ export default FlexberryBaseComponent.extend({
   classNames: ['flexberry-field', 'ui', 'field'],
 
   /**
+    Type of html input.
+
+    @property type
+    @type String
+    @default 'text'
+   */
+  type: 'text',
+
+  /**
     Path to component's settings in application configuration (JSON from ./config/environment.js).
 
     @property appConfigSettingsPath

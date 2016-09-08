2717beda255cb4f474ef141397cadf1592d2246a
yzaripova <zaripovaya@gmail.com>
2016-09-08 16:45:20 +0500

    Rollback blueprints for base model
    tfs #116013

--- blueprints/flexberry-model-init/files/__root__/models/__name__.js
+++ blueprints/flexberry-model-init/files/__root__/models/__name__.js
@@ -1,5 +1,5 @@
 import { Model as <%= className %>Mixin<%if (projections) {%>, defineProjections<%}%> } from '../mixins/regenerated/models/<%= name %>';
-import <%if(parentModelName) {%><%= parentClassName %>Model from './<%= parentModelName %>';<%}else{%>__Projection from 'ember-flexberry-data';<%}%>
+import <%if(parentModelName) {%><%= parentClassName %>Model from './<%= parentModelName %>';<%}else{%>{ __Projection } from 'ember-flexberry-data';<%}%>
 let Model = <%if(parentModelName) {%><%= parentClassName %>Model.extend<%}else{%>__Projection.Model.extend<%}%>(<%= className %>Mixin, {
 
 });

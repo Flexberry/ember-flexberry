627f093f793a5e18c3e15b3e623922dc254641d2
DubrovinPavel <dubrovip@yandex.ru>
2016-09-09 14:25:40 +0500

    Add validations inheritance generation in models

--- blueprints/flexberry-model-init/files/__root__/models/__name__.js
+++ blueprints/flexberry-model-init/files/__root__/models/__name__.js
@@ -1,7 +1,7 @@
 import { Model as <%= className %>Mixin<%if (projections) {%>, defineProjections<%}%> } from '../mixins/regenerated/models/<%= name %>';
 import <%if(parentModelName) {%><%= parentClassName %>Model from './<%= parentModelName %>';<%}else{%>{ Projection } from 'ember-flexberry-data';<%}%>
 let Model = <%if(parentModelName) {%><%= parentClassName %>Model.extend<%}else{%>Projection.Model.extend<%}%>(<%= className %>Mixin, {
-
+<%= initFunction %>
 });
 <%if(projections) {%>defineProjections(Model);<%}%>
 export default Model;

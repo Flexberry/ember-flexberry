c08d28ba95c8b166c4a59beab74b698d47090e84
Eugene Kolegov <ekolegov@outlook.com>
2016-09-09 12:53:05 +0500

    Feature addon add lock initializers

---
+++ app/instance-initializers/lock.js
@@ -0,0 +1,1 @@
+export { default, initialize } from 'ember-flexberry/instance-initializers/lock';
---
+++ tests/unit/instance-initializers/lock-test.js
@@ -0,0 +1,26 @@
+import Ember from 'ember';
+import { initialize } from 'dummy/instance-initializers/lock';
+import { module, test } from 'qunit';
+import destroyApp from '../../helpers/destroy-app';
+
+module('Unit | Instance Initializer | lock', {
+  beforeEach() {
+    Ember.run(() => {
+      this.application = Ember.Application.create();
+      this.appInstance = this.application.buildInstance();
+    });
+  },
+
+  afterEach() {
+    Ember.run(this.appInstance, 'destroy');
+    destroyApp(this.application);
+  },
+});
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  initialize(this.appInstance);
+
+  // you would normally confirm the results of the initializer here
+  assert.ok(true);
+});

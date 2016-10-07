18dced74a39158a4e1fcdf13e980c9ed55300236
Bratchikov Igor <bratchikov@ics.perm.ru>
2016-10-07 15:41:03 +0500

    Add form-load-time-tracker service

---
+++ tests/unit/services/form-load-time-tracker-test.js
@@ -0,0 +1,12 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('service:form-load-time-tracker', 'Unit | Service | form load time tracker', {
+  // Specify the other units that are required for this test.
+  // needs: ['service:foo']
+});
+
+// Replace this with your real tests.
+test('it exists', function(assert) {
+  let service = this.subject();
+  assert.ok(service);
+});

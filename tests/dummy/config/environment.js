5434efcdac821e0c3e8198cc172636e73a3bda1d
Eugene Kolegov <ekolegov@outlook.com>
2016-10-09 21:39:34 +0500

    :arrow_up: Update ember to 2.7.0 version

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -5,7 +5,7 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'dummy',
     environment: environment,
-    baseURL: '/',
+    rootURL: '/',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
@@ -116,7 +116,6 @@ module.exports = function(environment) {
 
   if (environment === 'test') {
     // Testem prefers this...
-    ENV.baseURL = '/';
     ENV.locationType = 'none';
 
     // keep test console output quieter

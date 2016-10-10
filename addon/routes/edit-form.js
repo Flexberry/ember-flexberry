dbdd3bead65d503202a6252f68acd77cb52944f6
Eugene Kolegov <ekolegov@outlook.com>
2016-10-10 10:45:44 +0500

    Fix edit-form route need reload condition and projection

--- addon/routes/edit-form.js
+++ addon/routes/edit-form.js
@@ -68,10 +68,10 @@ export default ProjectedModelFormRoute.extend(FlexberryGroupeditRouteMixin, {
     let flexberryDetailInteractionService = this.get('flexberryDetailInteractionService');
     let modelCurrentNotSaved = flexberryDetailInteractionService.get('modelCurrentNotSaved');
     let modelSelectedDetail = flexberryDetailInteractionService.get('modelSelectedDetail');
-    let needReload = !(modelCurrentNotSaved || (modelSelectedDetail && modelSelectedDetail.get('hasDirtyAttributes')));
+    let needReload = !!(modelCurrentNotSaved || (modelSelectedDetail && modelSelectedDetail.get('hasDirtyAttributes')));
 
     // TODO: now 'findRecord' at ember-flexberry-projection not support 'reload: false' flag.
-    let findRecordParameters = needReload ? { reload: needReload, projection: modelProjName } : undefined;
+    let findRecordParameters = { reload: needReload, projection: modelProjName };
 
     // :id param defined in router.js
     return this.store.findRecord(modelName, params.id, findRecordParameters);

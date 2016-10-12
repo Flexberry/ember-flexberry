27394709970997f91d7bc5aa32368ee709b805f2
Serega Matin <seregamatin@gmail.com>
2016-10-12 22:34:51 +0500

    Add flexberry-textbox 'readonly' mode test

--- tests/integration/components/flexberry-textbox-test.js
+++ tests/integration/components/flexberry-textbox-test.js
@@ -19,178 +19,213 @@ moduleForComponent('flexberry-textbox', 'Integration | Component | flexberry-tex
     Ember.Component.reopen({
       i18n: Ember.inject.service('i18n')
     });
+
+    // Set 'ru' as initial locale.
+    this.set('i18n.locale', 'ru');
   }
 });
 
 test('it renders properly', function(assert) {
-    assert.expect(10);
+  assert.expect(10);
 
-    // Set default locale as 'ru' & render component.
-    this.set('i18n.locale', 'ru');
-    this.render(hbs`{{flexberry-textbox
-      class=class
-    }}`);
+  // Render component.
+  this.render(hbs`{{flexberry-textbox
+    class=class
+  }}`);
 
-    // Retrieve component.
-    let $component = this.$().children();
-    let $textboxInput = $component.children('input');
+  // Retrieve component.
+  let $component = this.$().children();
+  let $textboxInput = $component.children('input');
 
-    // Check wrapper <div>.
-    assert.strictEqual($component.prop('tagName'), 'DIV', 'Component\'s wrapper is a <div>');
-    assert.strictEqual($component.hasClass('flexberry-textbox'), true, 'Component\'s wrapper has \' flexberry-textbox\' css-class');
-    assert.strictEqual($component.hasClass('ui'), true, 'Component\'s wrapper has \'ui\' css-class');
-    assert.strictEqual($component.hasClass('input'), true, 'Component\'s wrapper has \'input\' css-class');
+  // Check wrapper <div>.
+  assert.strictEqual($component.prop('tagName'), 'DIV', 'Component\'s wrapper is a <div>');
+  assert.strictEqual($component.hasClass('flexberry-textbox'), true, 'Component\'s wrapper has \' flexberry-textbox\' css-class');
+  assert.strictEqual($component.hasClass('ui'), true, 'Component\'s wrapper has \'ui\' css-class');
+  assert.strictEqual($component.hasClass('input'), true, 'Component\'s wrapper has \'input\' css-class');
 
-    // Check <input>.
-    assert.strictEqual($textboxInput.length === 1, true, 'Component has inner <input>');
-    assert.strictEqual($textboxInput.attr('type'), 'text', 'Component\'s inner <input> is of text type');
+  // Check <input>.
+  assert.strictEqual($textboxInput.length === 1, true, 'Component has inner <input>');
+  assert.strictEqual($textboxInput.attr('type'), 'text', 'Component\'s inner <input> is of text type');
 
-    // Check wrapper's additional CSS-classes.
-    let additioanlCssClasses = 'additional-css-class-name and-another-one';
-    this.set('class', additioanlCssClasses);
-    Ember.A(additioanlCssClasses.split(' ')).forEach((cssClassName, index) => {
-      assert.strictEqual(
-      $component.hasClass(cssClassName),
-      true,
-      'Component\'s wrapper has additional css class \'' + cssClassName + '\'');
-    });
+  // Check wrapper's additional CSS-classes.
+  let additioanlCssClasses = 'additional-css-class-name and-another-one';
+  this.set('class', additioanlCssClasses);
+  Ember.A(additioanlCssClasses.split(' ')).forEach((cssClassName, index) => {
+    assert.strictEqual(
+    $component.hasClass(cssClassName),
+    true,
+    'Component\'s wrapper has additional css class \'' + cssClassName + '\'');
+  });
 
-    // Clean up wrapper's additional CSS-classes.
-    this.set('class', '');
-    Ember.A(additioanlCssClasses.split(' ')).forEach((cssClassName, index) => {
-      assert.strictEqual(
-      $component.hasClass(cssClassName),
-      false,
-      'Component\'s wrapper hasn\'t additional css class \'' + cssClassName + '\'');
-    });
+  // Clean up wrapper's additional CSS-classes.
+  this.set('class', '');
+  Ember.A(additioanlCssClasses.split(' ')).forEach((cssClassName, index) => {
+    assert.strictEqual(
+    $component.hasClass(cssClassName),
+    false,
+    'Component\'s wrapper hasn\'t additional css class \'' + cssClassName + '\'');
+  });
+});
+
+test('readonly mode works properly', function(assert) {
+  assert.expect(3);
+
+  // Render component.
+  this.render(hbs`{{flexberry-textbox
+    class=class
+    readonly=readonly
+  }}`);
+
+  // Retrieve component.
+  let $component = this.$().children();
+  let $textboxInput = $component.children('input');
+
+  // Check that <input>'s readonly attribute doesn't exist yet.
+  assert.strictEqual(
+    Ember.$.trim($textboxInput.attr('readonly')),
+    '',
+    'Component\'s inner <input> hasn\'t readonly attribute');
+
+  // Activate readonly mode & check that <input>'s readonly attribute exists now & has value equals to 'readonly'. 
+  this.set('readonly', true);
+  assert.strictEqual(
+    Ember.$.trim($textboxInput.attr('readonly')),
+    'readonly',
+    'Component\'s inner <input> has readonly attribute with value equals to \'readonly\'');
+
+  // Deactivate readonly mode & check that <input>'s readonly attribute doesn't exist now. 
+  this.set('readonly', false);
+  assert.strictEqual(
+    Ember.$.trim($textboxInput.attr('readonly')),
+    '',
+    'Component\'s inner <input> hasn\'t readonly attribute');
 });
 
 test('it renders i18n-ed placeholder', function(assert) {
-    assert.expect(2);
+  assert.expect(2);
 
-    // Set default locale as 'ru' & render component.
-    this.set('i18n.locale', 'ru');
-    this.render(hbs`{{flexberry-textbox}}`);
+  // Render component.
+  this.render(hbs`{{flexberry-textbox}}`);
 
-    // Retrieve component.
-    let $component = this.$().children();
-    let $textboxInput = $component.children('input');
+  // Retrieve component.
+  let $component = this.$().children();
+  let $textboxInput = $component.children('input');
 
-    // Check <input>'s placeholder.
-    assert.strictEqual(
-      Ember.$.trim($textboxInput.attr('placeholder')),
-      Ember.get(I18nRuLocale, 'components.flexberry-textbox.placeholder'),
-      'Component\'s inner <input>\'s placeholder is equals to it\'s default value from i18n locales/ru/translations');
+  // Check <input>'s placeholder.
+  assert.strictEqual(
+    Ember.$.trim($textboxInput.attr('placeholder')),
+    Ember.get(I18nRuLocale, 'components.flexberry-textbox.placeholder'),
+    'Component\'s inner <input>\'s placeholder is equals to it\'s default value from i18n locales/ru/translations');
 
-    // Change current locale to 'en' & check <input>'s placeholder again.
-    this.set('i18n.locale', 'en');
-    assert.strictEqual(
-      Ember.$.trim($textboxInput.attr('placeholder')),
-      Ember.get(I18nEnLocale, 'components.flexberry-textbox.placeholder'),
-      'Component\'s inner <input>\'s placeholder is equals to it\'s value from i18n locales/en/translations');
+  // Change current locale to 'en' & check <input>'s placeholder again.
+  this.set('i18n.locale', 'en');
+  assert.strictEqual(
+    Ember.$.trim($textboxInput.attr('placeholder')),
+    Ember.get(I18nEnLocale, 'components.flexberry-textbox.placeholder'),
+    'Component\'s inner <input>\'s placeholder is equals to it\'s value from i18n locales/en/translations');
 });
 
 test('it renders manually defined placeholder', function(assert) {
-    assert.expect(2);
+  assert.expect(2);
 
-    // Set <input>'s placeholder' & render component.
-    let placeholder = 'Input is empty, please type some text';
-    this.set('placeholder', placeholder);
-    this.render(hbs`{{flexberry-textbox
-      placeholder=placeholder
-    }}`);
+  // Set <input>'s placeholder' & render component.
+  let placeholder = 'Input is empty, please type some text';
+  this.set('placeholder', placeholder);
+  this.render(hbs`{{flexberry-textbox
+    placeholder=placeholder
+  }}`);
 
-    // Retrieve component.
-    let $component = this.$().children();
-    let $textboxInput = $component.children('input');
+  // Retrieve component.
+  let $component = this.$().children();
+  let $textboxInput = $component.children('input');
 
-    // Check <input>'s placeholder.
-    assert.strictEqual(
-      Ember.$.trim($textboxInput.attr('placeholder')),
-      placeholder,
-      'Component\'s inner <input>\'s placeholder is equals to manually defined value \'' + placeholder + '\'');
+  // Check <input>'s placeholder.
+  assert.strictEqual(
+    Ember.$.trim($textboxInput.attr('placeholder')),
+    placeholder,
+    'Component\'s inner <input>\'s placeholder is equals to manually defined value \'' + placeholder + '\'');
 
-    // Change placeholder's value & check <input>'s placeholder again.
-    placeholder = 'Input has no value';
-    this.set('placeholder', placeholder);
-    assert.strictEqual(
-      Ember.$.trim($textboxInput.attr('placeholder')),
-      placeholder,
-      'Component\'s inner <input>\'s placeholder is equals to manually updated value \'' + placeholder + '\'');
+  // Change placeholder's value & check <input>'s placeholder again.
+  placeholder = 'Input has no value';
+  this.set('placeholder', placeholder);
+  assert.strictEqual(
+    Ember.$.trim($textboxInput.attr('placeholder')),
+    placeholder,
+    'Component\'s inner <input>\'s placeholder is equals to manually updated value \'' + placeholder + '\'');
 });
 
-test('Changes in inner <input> causes changes in property binded to \'value\'', function(assert) {
-    assert.expect(4);
+test('changes in inner <input> causes changes in property binded to \'value\'', function(assert) {
+  assert.expect(4);
 
-    // Set <input>'s value' & render component.
-    this.set('value', null);
-    this.render(hbs`{{flexberry-textbox
-      value=value
-    }}`);
+  // Set <input>'s value' & render component.
+  this.set('value', null);
+  this.render(hbs`{{flexberry-textbox
+    value=value
+  }}`);
 
-    // Retrieve component.
-    let $component = this.$().children();
-    let $textboxInput = $component.children('input');
+  // Retrieve component.
+  let $component = this.$().children();
+  let $textboxInput = $component.children('input');
 
-    // Check <input>'s value & binded value for initial emptyness.
-    assert.strictEqual(
-      Ember.$.trim($textboxInput.val()),
-      '',
-      'Component\'s inner <input>\'s value is equals to \'\'');
-    assert.strictEqual(
-      this.get('value'),
-      null,
-      'Component\'s property binded to \'value\' is equals to null');
+  // Check <input>'s value & binded value for initial emptyness.
+  assert.strictEqual(
+    Ember.$.trim($textboxInput.val()),
+    '',
+    'Component\'s inner <input>\'s value is equals to \'\'');
+  assert.strictEqual(
+    this.get('value'),
+    null,
+    'Component\'s property binded to \'value\' is equals to null');
 
-    // Change <input>'s value (imitate situation when user typed something into component's <input>)
-    // & check them again ('chenge' event is needed to force bindings work).
-    let newValue = 'Some text typed into textboxes inner <input>';
-    $textboxInput.val(newValue);
-    $textboxInput.change();
+  // Change <input>'s value (imitate situation when user typed something into component's <input>)
+  // & check them again ('change' event is needed to force bindings work).
+  let newValue = 'Some text typed into textboxes inner <input>';
+  $textboxInput.val(newValue);
+  $textboxInput.change();
 
-    assert.strictEqual(
-      Ember.$.trim($textboxInput.val()),
-      newValue,
-      'Component\'s inner <input>\'s value is equals to \'' + newValue + '\'');
-    assert.strictEqual(
-      this.get('value'),
-      newValue,
-      'Component\'s property binded to \'value\' is equals to \'' + newValue + '\'');
+  assert.strictEqual(
+    Ember.$.trim($textboxInput.val()),
+    newValue,
+    'Component\'s inner <input>\'s value is equals to \'' + newValue + '\'');
+  assert.strictEqual(
+    this.get('value'),
+    newValue,
+    'Component\'s property binded to \'value\' is equals to \'' + newValue + '\'');
 });
 
-test('Changes in property binded to \'value\' causes changes in inner <input>', function(assert) {
-    assert.expect(4);
+test('changes in property binded to \'value\' causes changes in inner <input>', function(assert) {
+  assert.expect(4);
 
-    // Set <input>'s value' & render component.
-    this.set('value', null);
-    this.render(hbs`{{flexberry-textbox
-      value=value
-    }}`);
+  // Set <input>'s value' & render component.
+  this.set('value', null);
+  this.render(hbs`{{flexberry-textbox
+    value=value
+  }}`);
 
-    // Retrieve component.
-    let $component = this.$().children();
-    let $textboxInput = $component.children('input');
+  // Retrieve component.
+  let $component = this.$().children();
+  let $textboxInput = $component.children('input');
 
-    // Check <input>'s value & binded value for initial emptyness.
-    assert.strictEqual(
-      Ember.$.trim($textboxInput.val()),
-      '',
-      'Component\'s inner <input>\'s value is equals to \'\'');
-    assert.strictEqual(
-      this.get('value'),
-      null,
-      'Component\'s property binded to \'value\' is equals to null');
+  // Check <input>'s value & binded value for initial emptyness.
+  assert.strictEqual(
+    Ember.$.trim($textboxInput.val()),
+    '',
+    'Component\'s inner <input>\'s value is equals to \'\'');
+  assert.strictEqual(
+    this.get('value'),
+    null,
+    'Component\'s property binded to \'value\' is equals to null');
 
-    // Change property binded to 'value' & check them again.
-    let newValue = 'Some text typed into textboxes inner <input>';
-    this.set('value', newValue);
+  // Change property binded to 'value' & check them again.
+  let newValue = 'Some text typed into textboxes inner <input>';
+  this.set('value', newValue);
 
-    assert.strictEqual(
-      Ember.$.trim($textboxInput.val()),
-      newValue,
-      'Component\'s inner <input>\'s value is equals to \'' + newValue + '\'');
-    assert.strictEqual(
-      this.get('value'),
-      newValue,
-      'Component\'s property binded to \'value\' is equals to \'' + newValue + '\'');
+  assert.strictEqual(
+    Ember.$.trim($textboxInput.val()),
+    newValue,
+    'Component\'s inner <input>\'s value is equals to \'' + newValue + '\'');
+  assert.strictEqual(
+    this.get('value'),
+    newValue,
+    'Component\'s property binded to \'value\' is equals to \'' + newValue + '\'');
 });

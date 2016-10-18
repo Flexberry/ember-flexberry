e3e6f23faca690d4d79098b3be7affc57d7ddc9c
Nasutya <nastia@bratchikovy.ru>
2016-10-18 18:29:03 +0500

    Add test for flexberry-textarea.

---
+++ tests/integration/components/flexberry-textarea-test.js
@@ -0,0 +1,26 @@
+import Ember from 'ember';
+
+import I18nService from 'ember-i18n/services/i18n';
+import I18nRuLocale from 'ember-flexberry/locales/ru/translations';
+import I18nEnLocale from 'ember-flexberry/locales/en/translations';
+
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('flexberry-textarea', 'Integration | Component | flexberry-textarea', {
+  integration: true,
+
+  beforeEach: function () {
+    this.register('locale:ru/translations', I18nRuLocale);
+    this.register('locale:en/translations', I18nEnLocale);
+    this.register('service:i18n', I18nService);
+
+    this.inject.service('i18n', { as: 'i18n' });
+    Ember.Component.reopen({
+      i18n: Ember.inject.service('i18n')
+    });
+
+    // Set 'ru' as initial locale.
+    this.set('i18n.locale', 'ru');
+  }
+});

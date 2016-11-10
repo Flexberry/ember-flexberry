5fca5c9d6f066a3eff0bd5164ac238572d950fef
PMasalkin <pashamasalkin@yandex.ru>
2016-11-10 14:28:08 +0500

    Add the directory for acceptance tests.

---
+++ tests/acceptance/flexberry-lookup-test.js
@@ -0,0 +1,21 @@
+import { test } from 'qunit';
+import moduleForAcceptance from '../../tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | flexberry lookup');
+
+/*test('visiting flexberry-lookup', function(assert) {
+  visit('/flexberry-lookup');
+
+  andThen(function() {
+    assert.equal(currentURL(), '/flexberry-lookup');
+  });
+});*/
+
+test('visiting flexberry-lookup', function(assert) {
+  visit('/acceptance-tests/flexberry-lookup/settings-example');
+
+  andThen(function() {
+    assert.equal(currentURL(), '/acceptance-tests/flexberry-lookup/settings-example');
+  });
+});
+

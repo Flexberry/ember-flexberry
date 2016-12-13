1d2ef1e481e7a717ae1b826dcd576d2f55f254f1
Serega Matin <seregamatin@gmail.com>
2016-12-13 19:32:04 +0500

    Fix JSCS of generated model files

--- blueprints/flexberry-model-init/files/__root__/models/__name__.js
+++ blueprints/flexberry-model-init/files/__root__/models/__name__.js
@@ -1,10 +1,14 @@
 import { Model as <%= className %>Mixin<%if (projections) {%>, defineProjections<%}%> } from '../mixins/regenerated/models/<%= name %>';
 import <%if(parentModelName) {%><%= parentClassName %>Model from './<%= parentModelName %>';<%}else{%>{ Projection } from 'ember-flexberry-data';<%}%>
+
 let Model = <%if(parentModelName) {%><%= parentClassName %>Model.extend<%}else{%>Projection.Model.extend<%}%>(<%= className %>Mixin, {
 
 });
+
 <%if(parentModelName) {%>Model.reopenClass({
   _parentModelName: '<%= parentModelName %>'
 });<%}%>
+
 <%if(projections) {%>defineProjections(Model);<%}%>
+
 export default Model;

840e7006b2124c69afb45f5808dd51ff932b7705
Evgeniy Khaberev <ehaberev@yandex.ru>
2016-12-14 11:34:15 +0500

    Fix generation of parent models definition

--- blueprints/flexberry-model/files/__root__/mixins/regenerated/models/__name__.js
+++ blueprints/flexberry-model/files/__root__/mixins/regenerated/models/__name__.js
@@ -3,5 +3,10 @@ import DS from 'ember-data';
 <%if(projections) {%>import { Projection } from 'ember-flexberry-data';<%}%>
 export let Model = Ember.Mixin.create({
 <%= model %>
-});
-<%if(projections) {%>export let defineProjections = function (model) {<%}%><%= projections %><%if(projections) {%>};<%}%>
+});<%if(parentModelName) {%>
+export let defineBaseModel = function (modelClass) {
+  modelClass.reopenClass({
+    _parentModelName: '<%= parentModelName %>'
+  });
+};<%}%><%if(projections) {%>
+export let defineProjections = function (modelClass) {<%}%><%= projections %><%if(projections) {%>};<%}%>

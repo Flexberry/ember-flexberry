c73591bc531c58079599071ba804abdb8777db39
Nasutya <nastia@bratchikovy.ru>
2016-12-28 16:32:37 +0500

    Fix test.

--- tests/unit/services/log-test.js
+++ tests/unit/services/log-test.js
@@ -1,10 +1,8 @@
 import Ember from 'ember';
 import { module, test } from 'qunit';
-import hbs from 'htmlbars-inline-precompile';
+import DS from 'ember-data';
 import startApp from '../../helpers/start-app';
 import destroyApp from '../../helpers/destroy-app';
-import ApplicationLogModel from 'ember-flexberry/models/i-i-s-caseberry-logging-objects-application-log';
-import UserSettingsService from 'ember-flexberry/services/user-settings';
 
 let app;
 
@@ -20,37 +18,35 @@ module('Unit | Service | log', {
 test('error works properly', function(assert) {
   assert.expect(1);
 
-  let done = assert.async();
+  // Stub save method of i-i-s-caseberry-logging-objects-application-log base model.
+  let originalSaveMethod = DS.Model.prototype.save;
 
-  let store = app.__container__.lookup('service:store');
+  let savedLogRecord;
+  DS.Model.prototype.save = function() {
+    savedLogRecord = this;
+    return new Ember.RSVP.Promise((resolve, reject) => {
+      resolve();
+    });
+  };
+
+  // Get log-service instance & enable errors logging.
   let logService = app.__container__.lookup('service:log');
-
-  // Stub save method.
-  let originalSave = ApplicationLogModel.save;
-
-  let savedLogErrorRecord;
-
-  ApplicationLogModel.reopen({
-    save() {
-      let _this = this;
-      savedLogErrorRecord = _this;
-      return new Ember.RSVP.Promise((resolve, reject) => {
-        resolve();
-      });
-    }
-  });
-
   logService.enabled = true;
   logService.logErrors = true;
 
+  // Call to Ember.Logger.error & check results.
   Ember.run(() => {
-    Ember.Logger.error('The system generated an error').then(() => {
-      ApplicationLogModel.reopen({
-        save() {
-          return originalSave();
-        }
-      });
-      assert.strictEqual(savedLogErrorRecord.get('massage'), 'The system generated an error');
-    }).finally(done);
+    let errorMessage = 'The system generated an error';
+    let done = assert.async();
+
+    Ember.Logger.error(errorMessage).then(() => {
+      assert.strictEqual(Ember.$.trim(savedLogRecord.get('message')), errorMessage);
+    }).finally(() => {
+
+      // Restore save method of i-i-s-caseberry-logging-objects-application-log base model.
+      DS.Model.prototype.save = originalSaveMethod;
+
+      done();
+    });
   });
 });

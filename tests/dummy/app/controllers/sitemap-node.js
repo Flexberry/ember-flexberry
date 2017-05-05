250a317b93b8be5b1a25e455629bbd8177d307c1
Stero1d <5047508@gmail.com>
2017-05-05 12:04:42 +0500

    Working out crimean theme

--- tests/dummy/app/controllers/sitemap-node.js
+++ tests/dummy/app/controllers/sitemap-node.js
@@ -5,27 +5,17 @@ export default Ember.Controller.extend({
     hideSidebar: function() {
       Ember.$('.ui.sidebar').sidebar('hide');
     },
-    subMenuEl() { 
-      let $this = $('.subMenu:first',$(event.target));
-      let $this2 = $(event.target).parent().find('.subMenu:first');
+    subMenuEl(event){ 
+      let $this =  $(event.currentTarget).parent().find('.subMenu:first');
       if ($this.hasClass('hidden-menu')) {
-         $this.removeClass('hidden-menu');
-          $('.item-minus:first', $(event.target)).removeClass('hidden-menu');
-          $('.item-plus:first', $(event.target)).addClass('hidden-menu');
+        $this.removeClass('hidden-menu');
+        $(event.target).parent().find('.item-minus:first').removeClass('hidden-menu');
+        $(event.target).parent().find('.item-plus:first').addClass('hidden-menu');
       } else {
-         $this.addClass('hidden-menu');
-          $('.item-minus:first', $(event.target)).addClass('hidden-menu');
-          $('.item-plus:first', $(event.target)).removeClass('hidden-menu');
-              }
-      if (($this2.hasClass('hidden-menu')) && $this.length == 0) {
-          $this2.removeClass('hidden-menu');
-            $(event.target).parent().find('.item-minus:first').removeClass('hidden-menu');
-            $(event.target).parent().find('.item-plus:first').addClass('hidden-menu');
-      }  else {
-          $this2.addClass('hidden-menu');
-           $(event.target).parent().find('.item-minus:first').addClass('hidden-menu');
-           $(event.target).parent().find('.item-plus:first').removeClass('hidden-menu');
-              }
+        $this.addClass('hidden-menu');
+        $(event.target).parent().find('.item-minus:first').addClass('hidden-menu');
+        $(event.target).parent().find('.item-plus:first').removeClass('hidden-menu');
+      }
     },
   }
 });

af27b42c8756734934666e140f726a8d4de748b8
Nasutya <nastia@bratchikovy.ru>
2017-05-30 11:21:59 +0500

    Fix test by comments

--- tests/acceptance/components/readonly-test/edit-form-readonly-test.js
+++ tests/acceptance/components/readonly-test/edit-form-readonly-test.js
@@ -1,5 +1,30 @@
 import Ember from 'ember';
-import {executeTest} from './readonly-base-test';
+import { module, test } from 'qunit';
+import startApp from '../../../helpers/start-app';
+
+export function executeTest(testName, callback) {
+  let app;
+  let store;
+
+  module('Acceptance | readonly-test | ' + testName, {
+    beforeEach() {
+
+      // Start application.
+      app = startApp();
+
+      // Enable acceptance test mode in application controller (to hide unnecessary markup from application.hbs).
+      let applicationController = app.__container__.lookup('controller:application');
+      applicationController.set('isInAcceptanceTestMode', true);
+      store = app.__container__.lookup('service:store');
+    },
+
+    afterEach() {
+      Ember.run(app, 'destroy');
+    }
+  });
+
+  test(testName, (assert) => callback(store, assert, app));
+}
 
 executeTest('test readonly correctly', (store, assert, app) => {
   assert.expect(14);

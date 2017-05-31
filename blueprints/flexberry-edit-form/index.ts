13d39b8d8ba15ace71a2095064fac7e977d6ce7d
DubrovinPavel <dubrovip@yandex.ru>
2017-05-31 17:14:21 +0500

    Fix master fields caption generation on edit-forms

--- blueprints/flexberry-edit-form/index.ts
+++ blueprints/flexberry-edit-form/index.ts
@@ -207,6 +207,7 @@ class EditFormBlueprint {
         belongsToAttr.name = lodash.concat(currentPath, belongsToAttr.name).join(".");
         belongsToAttr.readonly="true";
         belongsToAttr.type=attr.type;
+        this.locales.setupEditFormAttribute(belongsToAttr);
         this._tmpSnippetsResult.push({ index: belongsToAttr.index, snippetResult: lodash.template(snippet)(belongsToAttr) });
       }
       this.fillBelongsToAttrs(belongsTo.belongsTo, currentPath);

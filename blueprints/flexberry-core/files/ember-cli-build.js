eac3ab40f79d5700751499a43394b18dd48eb259
Evgeniy Khaberev <ehaberev@yandex.ru>
2017-06-08 00:34:35 +0500

    Small fixes for themes generation

--- blueprints/flexberry-core/files/ember-cli-build.js
+++ blueprints/flexberry-core/files/ember-cli-build.js
@@ -4,7 +4,6 @@ var Ember<%=projectTypeNameCamel%> = require('ember-cli/lib/broccoli/ember-<%=pr
 
 module.exports = function(defaults) {
   var app = new Ember<%=projectTypeNameCamel%>(defaults, {
-    // Add options here
     jscsOptions: {
       enabled: true,
       esnext: true,
@@ -24,19 +23,6 @@ module.exports = function(defaults) {
       }
     }
   });
-
-  // Use `app.import` to add additional libraries to the generated
-  // output files.
-  //
-  // If you need to use different assets in different
-  // environments, specify an object as the first parameter. That
-  // object's keys should be the environment name and the values
-  // should be the asset to use in that environment.
-  //
-  // If the library that you are including contains AMD or ES6
-  // modules that you would like to import into your application
-  // please specify an object with the list of modules as keys
-  // along with the exports of each module as its value.
 
   app.import('vendor/font-icon.css');
   app.import('vendor/fonts/icons.eot', { destDir: 'assets/fonts' });

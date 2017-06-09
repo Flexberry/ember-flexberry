942b32dee574f1ecf91220d5b1651031abef9593
Stero1d <5047508@gmail.com>
2017-06-09 18:16:28 +0500

    Fixed fonts icons

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -32,7 +32,6 @@ module.exports = function(defaults) {
   app.import('vendor/fonts/icons.woff', { destDir: 'assets/fonts' });
   app.import('vendor/fonts/icons.woff2', { destDir: 'assets/fonts' });
   app.import('vendor/fonts/crim.eot', { destDir: 'assets/fonts' });
-  app.import('vendor/fonts/crim.otf', { destDir: 'assets/fonts' });
   app.import('vendor/fonts/crim.svg', { destDir: 'assets/fonts' });
   app.import('vendor/fonts/crim.ttf', { destDir: 'assets/fonts' });
   app.import('vendor/fonts/crim.woff', { destDir: 'assets/fonts' });

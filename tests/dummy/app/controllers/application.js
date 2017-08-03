8beb19b57073cabb5003b1dffdce87b82ba05eb3
i-Living <living.59@yandex.ru>
2017-08-03 13:17:16 +0500

    Fix JSCS

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -32,9 +32,9 @@ export default Ember.Controller.extend({
       }
 
       if (Ember.$('.inverted.vertical.main.menu').hasClass('visible')) {
-        Ember.$('.full.height').css({'transition': 'width 0.45s ease-in-out 0s', 'width': '100%'});
+        Ember.$('.full.height').css({ 'transition': 'width 0.45s ease-in-out 0s', 'width': '100%' });
       } else {
-        Ember.$('.full.height').css({'transition': 'width 0.3s ease-in-out 0s', 'width': 'calc(100% - ' + sidebar.width() + 'px)'});
+        Ember.$('.full.height').css({ 'transition': 'width 0.3s ease-in-out 0s', 'width': 'calc(100% - ' + sidebar.width() + 'px)' });
       }
     },
 

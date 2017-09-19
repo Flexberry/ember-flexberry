1a0de499e7d55d36840a33c5c382afdbab1bc424
Eugene Kolegov <ekolegov@outlook.com>
2017-09-19 11:54:32 +0500

    Fix locales addon replace caption key in models localization

--- addon/locales/en/translations.js
+++ addon/locales/en/translations.js
@@ -216,46 +216,46 @@ export default {
       'projections': {
         'ApplicationLogL': {
           'processId': {
-            'caption': 'URL'
+            __caption__: 'URL'
           },
           'timestamp':{
-            'caption': 'Time'
+            __caption__: 'Time'
           },
           'category':{
-            'caption': 'Category'
+            __caption__: 'Category'
           },
           'eventId':{
-            'caption': 'Event ID'
+            __caption__: 'Event ID'
           },
           'priority':{
-            'caption': 'Priority'
+            __caption__: 'Priority'
           },
           'severity':{
-            'caption': 'Severity'
+            __caption__: 'Severity'
           },
           'title':{
-            'caption': 'Title'
+            __caption__: 'Title'
           },
           'machineName':{
-            'caption': 'Server'
+            __caption__: 'Server'
           },
           'appDomainName':{
-            'caption': 'Browser'
+            __caption__: 'Browser'
           },
           'processName':{
-            'caption': 'Process name'
+            __caption__: 'Process name'
           },
           'threadName':{
-            'caption': 'ThreadName'
+            __caption__: 'ThreadName'
           },
           'win32ThreadId':{
-            'caption': 'Win32ThreadId',
+            __caption__: 'Win32ThreadId',
           },
           'message':{
-            'caption': 'Message'
+            __caption__: 'Message'
           },
           'formattedMessage':{
-            'caption': 'Formatted message'
+            __caption__: 'Formatted message'
           }
         }
       }
@@ -263,9 +263,9 @@ export default {
     'new-platform-flexberry-services-lock': {
       'projections': {
         'LockL': {
-          'lockKey': { 'caption': 'Key locked object' },
-          'userName': { 'caption': 'User locked object' },
-          'lockDate': { 'caption': 'Date lock' },
+          'lockKey': { __caption__: 'Key locked object' },
+          'userName': { __caption__: 'User locked object' },
+          'lockDate': { __caption__: 'Date lock' },
         },
       },
     },

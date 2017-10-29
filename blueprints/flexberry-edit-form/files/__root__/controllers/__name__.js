567b15067ddfeb562b92fcd13a38aedbee1f0587
Eugene Kolegov <ekolegov@outlook.com>
2017-10-29 21:32:46 +0500

    Feature flexberry-edit-form blueprint update the generation of templates and controllers

--- blueprints/flexberry-edit-form/files/__root__/controllers/__name__.js
+++ blueprints/flexberry-edit-form/files/__root__/controllers/__name__.js
@@ -1,6 +1,7 @@
 import EditFormController from 'ember-flexberry/controllers/edit-form';
+import { <%= modelProjection %>Validation } from '../mixins/regenerated/models/<%= modelName %>';
 
-export default EditFormController.extend({<%if(parentRoute){%>
+export default EditFormController.extend(<%= modelProjection %>Validation, {<%if(parentRoute){%>
   parentRoute: '<%= parentRoute %>',<%}if (functionGetCellComponent) {%>
 
   getCellComponent<%= functionGetCellComponent %>,<%}%>

0b8bcffc3c4ab4ca49f6f72dfa6e716da56b2d96
PashaMasalkin <pashamasalkin@yandex.ru>
2017-11-14 11:58:31 +0500

    Add i18n

--- tests/integration/components/flexberry-simpledatetime-test.js
+++ tests/integration/components/flexberry-simpledatetime-test.js
@@ -2,8 +2,26 @@ import { moduleForComponent, test } from 'ember-qunit';
 import Ember from 'ember';
 import hbs from 'htmlbars-inline-precompile';
 
+import I18nService from 'ember-i18n/services/i18n';
+import I18nRuLocale from 'ember-flexberry/locales/ru/translations';
+import I18nEnLocale from 'ember-flexberry/locales/en/translations';
+
 moduleForComponent('flexberry-simpledatetime', 'Integration | Component | flexberry simpledatetime', {
-  integration: true
+  integration: true,
+
+  beforeEach: function () {
+    this.register('locale:ru/translations', I18nRuLocale);
+    this.register('locale:en/translations', I18nEnLocale);
+    this.register('service:i18n', I18nService);
+
+    this.inject.service('i18n', { as: 'i18n' });
+    Ember.Component.reopen({
+      i18n: Ember.inject.service('i18n')
+    });
+
+    // Set 'ru' as initial locale.
+    this.set('i18n.locale', 'ru');
+  }
 });
 
 test('it renders', function(assert) {
@@ -20,7 +38,6 @@ test('render with type before value', function(assert) {
   this.render(hbs`{{flexberry-simpledatetime
       type='date'
       value=value
-      placeholder=placeholder
     }}`);
 
   // Retrieve component.
@@ -45,7 +62,6 @@ test('render with type afther value', function(assert) {
   this.render(hbs`{{flexberry-simpledatetime
       value=value
       type='date'
-      placeholder=placeholder
     }}`);
 
   // Retrieve component.

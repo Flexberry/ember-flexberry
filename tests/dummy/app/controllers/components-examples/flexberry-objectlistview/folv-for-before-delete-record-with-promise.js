84827e78c8c2725a8ef6bd7bfc548dba3406cc6d
PashaMasalkin <pashamasalkin@yandex.ru>
2018-01-11 09:43:15 +0500

    Correct test forms

--- tests/dummy/app/controllers/components-examples/flexberry-objectlistview/folv-for-before-delete-record-with-promise.js
+++ tests/dummy/app/controllers/components-examples/flexberry-objectlistview/folv-for-before-delete-record-with-promise.js
@@ -2,16 +2,21 @@ import Ember from 'ember';
 import ListFormController from 'ember-flexberry/controllers/list-form';
 
 export default ListFormController.extend({
+
+  /**
+    Parameter for use in the test beforeDeleteRecord.
+
+    @property recordWasNotDelete
+    @type bool
+    @default false
+   */
+  recordWasNotDelete: false,
+
   actions: {
     beforeDeleteRecord(record, data) {
-      let promise = Ember.A();
-
       return new Ember.RSVP.Promise((resolve) => {
-        let timeout = 1000;
-        Ember.run.later((() => {
-          resolve();
-          Ember.RSVP.Promise.all(promise);
-        }), timeout);
+        this.set('recordWasNotDelete', !record.get('isDeleted'));
+        resolve();
       });
     }
   }
--- tests/dummy/app/controllers/components-examples/flexberry-objectlistview/folv-for-before-delete-record.js
+++ tests/dummy/app/controllers/components-examples/flexberry-objectlistview/folv-for-before-delete-record.js
@@ -1,8 +1,19 @@
 import ListFormController from 'ember-flexberry/controllers/list-form';
 
 export default ListFormController.extend({
+
+  /**
+    Parameter for use in the test beforeDeleteRecord.
+
+    @property recordWasNotDelete
+    @type bool
+    @default false
+   */
+  recordWasNotDelete: false,
+
   actions: {
     beforeDeleteRecord(record, data) {
+      this.set('recordWasNotDelete', !record.get('isDeleted'));
       return;
     }
   }

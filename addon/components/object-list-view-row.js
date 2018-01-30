2a0564ff3b9af6eac69360a1007ac6ec7a9c44ea
DubrovinPavel <dubrovip@yandex.ru>
2018-01-30 17:00:01 +0500

    Fix travis build

--- addon/components/object-list-view-row.js
+++ addon/components/object-list-view-row.js
@@ -268,7 +268,7 @@ export default FlexberryBaseComponent.extend({
 
       // If user clicked on hierarchy expand button on lookup form we should not process row clicking.
       let classOfHierarchyExpandButton = 'hierarchy-expand';
-      if (!Ember.$(e.srcElement).hasClass(classOfHierarchyExpandButton) && !Ember.$(e.srcElement).children().hasClass(classOfHierarchyExpandButton))
+      if (Ember.isNone(e) || !Ember.$(e.srcElement).hasClass(classOfHierarchyExpandButton))
       {
         this.sendAction('rowClick', record, params);
       }

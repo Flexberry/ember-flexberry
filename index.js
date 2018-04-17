f75b98ecde0983474fa693f8565855be8c6a2135
Evgeniy Khaberev <ehaberev@yandex.ru>
2018-04-17 12:56:17 +0500

    Update ember to 2.17 version

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports = {
   name: 'ember-flexberry',
 
   included: function(app) {
-    this._super.included.apply(this._super, arguments);
+    this._super(...arguments);
 
     app.import('vendor/polyfills.js', {
       prepend: true

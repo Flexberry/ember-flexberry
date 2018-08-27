e13ec6c1135a0051252115559ca53ca2c93d71f5
TeterinaSvetlana <40786685+TeterinaSvetlana@users.noreply.github.com>
2018-08-27 16:50:12 +0500

    Fix code style

--- tests/acceptance/components/flexberry-objectlistview/folv-column-width-customization-test.js
+++ tests/acceptance/components/flexberry-objectlistview/folv-column-width-customization-test.js
@@ -6,81 +6,81 @@ executeTest('column width custom', (store, assert, app) => {
 
   let path = 'i-i-s-caseberry-logging-objects-application-log-l';
   Ember.run(() => {
-        let done = assert.async();
-        visit(path);
-        andThen(function() {
-          assert.equal(currentPath(), path);
-          let $defaultConfig = Ember.$('.remove.circle.icon');
-          loadingList($defaultConfig[0], '.object-list-view-container', 'table.object-list-view tbody tr').then(($list) => {
-            assert.ok($list);
-            let $configButton = Ember.$('.ui.button');
-            let done1 = assert.async();
+    let done = assert.async();
+    visit(path);
+    andThen(function() {
+      assert.equal(currentPath(), path);
+      let $defaultConfig = Ember.$('.remove.circle.icon');
+      loadingList($defaultConfig[0], '.object-list-view-container', 'table.object-list-view tbody tr').then(($list) => {
+        assert.ok($list);
+        let $configButton = Ember.$('.ui.button');
+        let done1 = assert.async();
 
-            // Loading config window
-            loadingList($configButton[1], '.object-list-view-container', 'table.object-list-view tbody tr').then(($list) => {
+        // Loading config window
+        loadingList($configButton[1], '.object-list-view-container', 'table.object-list-view tbody tr').then(($list) => {
+          assert.ok($list);
+
+          // Retrieve component & it's inner <input>.
+          let $component = Ember.$('.flexberry-checkbox.ui.checkbox');
+          let $checkboxInput = $component.children('input');
+
+          assert.equal($($component).hasClass('checked'), false, 'Width config is OFF');
+
+          // In case, width config is OFF
+          assert.equal($($component).hasClass('checked'), false, 'checkbox is not checked');
+          $component.click();
+          setTimeout(function() {
+            assert.equal($($component).hasClass('checked'), true, 'Width config is ON');
+            assert.equal($($component).hasClass('disabled'), false, 'Width values is ON');
+            assert.equal($checkboxInput.prop('checked'), true, 'Component\'s inner checkbox <input> is checked after click');
+            let $columnsTable = Ember.$('.ui.unstackable.fixed.selectable.celled.table').children('tbody#colsConfigtableRows').children();
+
+            for (let i = 0; i < 14; i++) {
+
+              // If column is hidden, editing its width should be disabled
+              if ($columnsTable[i].cells[0].children[0].className === 'large hide icon') {
+                assert.equal($columnsTable[i].cells[5].className, 'disabled', 'Column is disabled');
+              }
+
+              Ember.$('input.columnWidth')[0].value = 50;
+            }
+
+            let done2 = assert.async();
+
+            // Apply settings
+            let $useBtn = Ember.$('.ui.button#columnConfigurtionButtonUse');
+            loadingList($useBtn[0], '.object-list-view-container', 'table.object-list-view tbody tr').then(($list) => {
               assert.ok($list);
-
-              // Retrieve component & it's inner <input>.
-              let $component = Ember.$('.flexberry-checkbox.ui.checkbox');
-              let $checkboxInput = $component.children('input');
-              
-              assert.equal($($component).hasClass('checked'), false, 'Width config is OFF');
-
-              // In case, width config is OFF
-              assert.equal($($component).hasClass('checked'), false, 'checkbox is not checked');
-              $component.click();
               setTimeout(function() {
-                assert.equal($($component).hasClass('checked'), true, 'Width config is ON');
-                assert.equal($($component).hasClass('disabled'), false, 'Width values is ON');
-                assert.equal($checkboxInput.prop('checked'), true, 'Component\'s inner checkbox <input> is checked after click');
-                let $columnsTable = Ember.$('.ui.unstackable.fixed.selectable.celled.table').children('tbody#colsConfigtableRows').children();
+                let done3 = assert.async();
 
-                for (let i = 0; i < 14; i++) {
-
-                  // If column is hidden, editing its width should be disabled
-                  if ($columnsTable[i].cells[0].children[0].className === 'large hide icon') {
-                    assert.equal($columnsTable[i].cells[5].className, 'disabled', 'Column is disabled');
-                  }
-
-                  Ember.$('input.columnWidth')[0].value = 50;
-                }
-
-                let done2 = assert.async();
-
-                // Apply settings
-                let $useBtn = Ember.$('.ui.button#columnConfigurtionButtonUse');
-                loadingList($useBtn[0], '.object-list-view-container', 'table.object-list-view tbody tr').then(($list) => {
+                //Open config window second time
+                loadingList($configButton[1], '.object-list-view-container', 'table.object-list-view tbody tr').then(($list) => {
                   assert.ok($list);
+
+                  let columns = Ember.$('.dt-head-left.me.class');
+                  assert.equal(columns[0].offsetWidth, 70, 'Width is right');
+
+                  // Retrieve component & it's inner <input>.
+                  let $component = Ember.$('.flexberry-checkbox.ui.checkbox');
+                  let $checkboxInput = $component.children('input');
+
+                  assert.equal($($component).hasClass('checked'), true, 'Width config is ON');
+
+                  // In case, width config is ON
+                  assert.equal($($component).hasClass('checked'), true, 'checkbox is checked');
+                  $component.click();
                   setTimeout(function() {
-                    let done3 = assert.async();
+                    assert.equal($($component).hasClass('checked'), false, 'Width config is OFF');
+                    assert.equal($checkboxInput.prop('checked'), false, 'Component\'s inner checkbox <input> is not checked after click');                               
 
-                    //Open config window second time
-                    loadingList($configButton[1], '.object-list-view-container', 'table.object-list-view tbody tr').then(($list) => {
-                    assert.ok($list);
-                
-                    let columns = Ember.$('.dt-head-left.me.class');
-                    assert.equal(columns[0].offsetWidth, 70, 'Width is right');
+                    // Apply settings
+                    let $useBtn = Ember.$('.ui.button#columnConfigurtionButtonUse');
+                    let done4 = assert.async();
+                    loadingList($useBtn[0], '.object-list-view-container', 'table.object-list-view tbody tr').then(($list) => {
+                      assert.ok($list);
 
-                    // Retrieve component & it's inner <input>.
-                    let $component = Ember.$('.flexberry-checkbox.ui.checkbox');
-                    let $checkboxInput = $component.children('input');
-                  
-                    assert.equal($($component).hasClass('checked'), true, 'Width config is ON');
-
-                    // In case, width config is ON
-                    assert.equal($($component).hasClass('checked'), true, 'checkbox is checked');
-                    $component.click();
-                    setTimeout(function() {
-                      assert.equal($($component).hasClass('checked'), false, 'Width config is OFF');
-                      assert.equal($checkboxInput.prop('checked'), false, 'Component\'s inner checkbox <input> is not checked after click');                               
-
-                      // Apply settings
-                      let $useBtn = Ember.$('.ui.button#columnConfigurtionButtonUse');
-                      let done4 = assert.async();
-                      loadingList($useBtn[0], '.object-list-view-container', 'table.object-list-view tbody tr').then(($list) => {
-                        assert.ok($list);
-
-                        setTimeout(function() {}, 17000);
+                      setTimeout(function() {}, 17000);
 
                       }).catch((reason) => {
                         throw new Error(reason);
@@ -103,7 +103,7 @@ executeTest('column width custom', (store, assert, app) => {
                 }).finally(() => {
                   done2();
                 });
-                  }, 500);
+                  }, 17000);
 
                   }).catch((reason) => {
                 throw new Error(reason);

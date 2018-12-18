83489c207fd1a59ed77a47dffb614a18b4bb2fa0
Nasutya <nastia@bratchikovy.ru>
2018-12-18 16:06:01 +0500

    Update test multiple transactions

--- tests/acceptance/components/flexberry-objectlistview/folv-multiple-transactions-test.js
+++ tests/acceptance/components/flexberry-objectlistview/folv-multiple-transactions-test.js
@@ -1,49 +1,28 @@
 import Ember from 'ember';
-import { executeTest, addDataForDestroy } from './execute-folv-test';
+import { executeTest } from './execute-folv-test';
 import generateUniqueId from 'ember-flexberry-data/utils/generate-unique-id';
 
 import { Query } from 'ember-flexberry-data';
 const { Builder } = Query;
 
 executeTest('multiple transactions', (store, assert, app) => {
-  assert.expect(2);
-  let path = 'components-examples/flexberry-objectlistview/before-delete-record/folv-for-before-delete-record-data-cancel';
+  assert.expect(1);
   let modelName = 'ember-flexberry-dummy-suggestion-type';
-  let howAddRec = 1;
   let uuid = '0' + generateUniqueId();
+  let id1 = '1';
+  let id2 = '2';
 
   // Add records for deliting.
   Ember.run(() => {
-    let newRecord = store.createRecord(modelName, { name: uuid });
-    let done1 = assert.async();
+    let newRecord1 = store.createRecord(modelName, { ID:id1, name: uuid });
+    newRecord1.save();
 
-    newRecord.save().then(() => {
-      addDataForDestroy(newRecord);
-      let builder = new Builder(store).from(modelName).count();
-      let done = assert.async();
-      store.query(modelName, builder.build()).then((result) => {
-        visit(path + '?perPage=' + result.meta.count);
-        andThen(() => {
-          assert.equal(currentPath(), path);
+    let newRecord2 = store.createRecord(modelName, { ID:id2, name: uuid, parent:id1 });
+    newRecord2.save();
 
-          let olvContainerClass = '.object-list-view-container';
-          let trTableClass = 'table.object-list-view tbody tr';
-
-          let $folvContainer = Ember.$(olvContainerClass);
-          let $rows = () => { return Ember.$(trTableClass, $folvContainer).toArray(); };
-
-          // Check that the records have been added.
-          let recordIsForDeleting = $rows().reduce((sum, element) => {
-            let nameRecord = Ember.$.trim(element.children[1].innerText);
-            let flag = nameRecord.indexOf(uuid) >= 0;
-            return sum + flag;
-          }, 0);
-
-          assert.equal(recordIsForDeleting > 0, true, howAddRec + ' record added');
-        });
-        done();
-      });
-      done1();
+    let builder = new Builder(store, modelName).where('name', Query.FilterOperator.Eq, uuid);
+    store.query(modelName, builder.build()).then((result) => {
+      assert.ok(result.meta.count, 'record \'' + uuid + 'ok');
     });
   });
 });

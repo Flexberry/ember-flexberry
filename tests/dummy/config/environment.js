74d109268bd933803c542de42e6bd14662ea4528
Eugene Kolegov <ekolegov@outlook.com>
2019-01-16 14:51:23 +0500

    chore: Update backend URL for dummy app

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -1,7 +1,7 @@
 /* jshint node: true */
 
 module.exports = function(environment) {
-  var backendUrl = 'https://flexberry-ember-dummy.azurewebsites.net';
+  var backendUrl = 'http://flexberry.northeurope.cloudapp.azure.com';
 
   if (environment === 'development-loc') {
     // Use `ember s -e development-loc` command for local backend usage.

45f43d594cc17ff9198439824c6716c2c4587313
Масалкин Павел <pashamasalkin@yandex.ru>
2019-01-28 15:31:05 +0500

    Revert "Revert "Update example for hierarchical detail""
    This reverts commit 32e9b64484adae4dcb5a7f06c7fd04d27cb8aafb.

---
+++ tests/dummy/app/controllers/ember-flexberry-dummy-parent-detail-list.js
@@ -0,0 +1,12 @@
+import ListFormController from 'ember-flexberry/controllers/list-form';
+
+export default ListFormController.extend({
+  /**
+    Name of related edit form route.
+
+    @property editFormRoute
+    @type String
+    @default ''
+   */
+  editFormRoute: ''
+});

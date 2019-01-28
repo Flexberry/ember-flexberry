cb487d17f765035a3ed27d0a26b6544bff627631
Масалкин Павел <pashamasalkin@yandex.ru>
2019-01-28 15:32:08 +0500

    Revert "Delite acceptance test for multiple transactions"
    This reverts commit 647a3e2b3c8e9ba8314c1eed024f5d9105edc712.

---
+++ tests/acceptance/components/flexberry-objectlistview/folv-multiple-transactions-test.js
@@ -0,0 +1,30 @@
+import Ember from 'ember';
+import { executeTest } from './execute-folv-test';
+import generateUniqueId from 'ember-flexberry-data/utils/generate-unique-id';
+
+import { Query } from 'ember-flexberry-data';
+const { Builder } = Query;
+
+executeTest('multiple transactions', (store, assert, app) => {
+  assert.expect(1);
+  let modelName = 'ember-flexberry-dummy-suggestion-type';
+  let uuid1 = '1' + generateUniqueId();
+  let uuid2 = '2' + generateUniqueId();
+
+  // Add records for deliting.
+  Ember.run(() => {
+    let newRecord1;
+    let newRecord2;
+
+    newRecord1 = store.createRecord(modelName, { name: uuid1, parent: newRecord2 });
+    newRecord2 = store.createRecord(modelName, { name: uuid2, parent: newRecord1 });
+
+    newRecord1.save();
+    newRecord2.save();
+
+    let builder = new Builder(store, modelName).where('name', Query.FilterOperator.Eq, uuid1);
+    store.query(modelName, builder.build()).then((result) => {
+      assert.ok(result.meta.count, 'record \'' + uuid1 + 'ok');
+    });
+  });
+});

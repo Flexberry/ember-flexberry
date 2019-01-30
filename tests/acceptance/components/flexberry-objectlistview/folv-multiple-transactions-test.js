d52ce0f586c91b699a967b91db88640081a3189e
Масалкин Павел <pashamasalkin@yandex.ru>
2019-01-30 14:38:51 +0500

    test: finalized test details

--- tests/acceptance/components/flexberry-objectlistview/folv-multiple-transactions-test.js
+++ tests/acceptance/components/flexberry-objectlistview/folv-multiple-transactions-test.js
@@ -6,42 +6,47 @@ import { Query } from 'ember-flexberry-data';
 const { Builder } = Query;
 
 executeTest('multiple transactions', (store, assert, app) => {
-  assert.expect(1);
+  assert.expect(2);
   let modelName = 'ember-flexberry-dummy-suggestion-type';
   let uuid1 = '1' + generateUniqueId();
   let uuid2 = '2' + generateUniqueId();
 
-  // Add records for deliting.
   Ember.run(() => {
     let done = assert.async()
+    let timeout = 10000;
     let newRecord1;
     let newRecord2;
 
-    newRecord1 = store.createRecord(modelName, { name: uuid1});
-    newRecord2 = store.createRecord(modelName, { name: uuid2});
+    // Create new records.
+    newRecord1 = store.createRecord(modelName, { name: uuid1, patent: newRecord2 });
+    newRecord2 = store.createRecord(modelName, { name: uuid2, patent: newRecord1 });
 
     newRecord1.save().then(() => {
       newRecord2.save().then(() => {
+
+        // Add record to destroy afther test.
         addDataForDestroy(newRecord2);
         addDataForDestroy(newRecord1);
-        let timeout = 10000;
+
+        // Add circular reference to records.
+        newRecord2.set('parent', newRecord2);
+        newRecord1.set('parent', newRecord1);
+
+        // Save their together.
+        newRecord1.save();
+        newRecord2.save();
+
         Ember.run.later((function() {
-          newRecord2.set('parent', newRecord2);
-          newRecord1.set('parent', newRecord1);
-
-          newRecord1.save();
-          newRecord2.save();
-
-          Ember.run.later((function() {
-            let builder = new Builder(store, modelName).where('name', Query.FilterOperator.Eq, uuid1);
+          // Check created records.
+          let builder = new Builder(store, modelName).where('name', Query.FilterOperator.Eq, uuid1);
+          store.query(modelName, builder.build()).then((result) => {
+            assert.ok(result.content.length, 'record \'' + uuid1 + 'ok');
+            let builder = new Builder(store, modelName).where('name', Query.FilterOperator.Eq, uuid2);
             store.query(modelName, builder.build()).then((result) => {
-              assert.ok(result.content.length, 'record \'' + uuid1 + 'ok');
-              newRecord2.set('parent', null);
-              newRecord2.save().then(() => {
-                done();
-              });
+              assert.ok(result.content.length, 'record \'' + uuid2 + 'ok');
+              done();
             });
-          }), timeout);
+          });
         }), timeout);
       });
     });

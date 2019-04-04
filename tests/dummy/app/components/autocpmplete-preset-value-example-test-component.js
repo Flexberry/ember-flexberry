21cdc43ce1273198814ccae15aff857473a80c37
Масалкин Павел <pashamasalkin@yandex.ru>
2019-04-04 10:37:04 +0500

    Correct example

--- tests/dummy/app/components/autocpmplete-preset-value-example-test-component.js
+++ tests/dummy/app/components/autocpmplete-preset-value-example-test-component.js
@@ -1,22 +1,6 @@
-/**
-  @module ember-flexberry-dummy
- */
-
 import Ember from 'ember';
+import layout from '../templates/components/autocpmplete-preset-value-example-test-component';
 
-/**
-  Number input component.
-
-  @class NumberInputComponent
-  @extends <a href="http://emberjs.com/api/classes/Ember.Component.html">Ember.Component</a>
- */
 export default Ember.Component.extend({
-    displayValue:undefined,
-    value:undefined,
-    /**
-    Initializes component.
-    */
-    init() {
-    this._super(...arguments);
-    }
+  layout
 });

76148d4de07bffc0c8dc201ed8012a9da31ca6c4
DubrovinPavel <dubrovip@yandex.ru>
2019-04-16 14:41:57 +0500

    Fix eslint

--- addon/components/flexberry-groupedit.js
+++ addon/components/flexberry-groupedit.js
@@ -586,7 +586,9 @@ export default FlexberryBaseComponent.extend({
     let order = attributesKeys.find((key) => {
       let attrubute = attributes[key];
       if (attrubute.kind === 'attr' && information.isOrdered(projectionName.modelName, key)) {
+        /* eslint-disable ember/no-side-effects */
         this.set('sorting', [{ direction: 'asc', propName: key }]);
+        /* eslint-enable ember/no-side-effects */
         return key;
       }
     });

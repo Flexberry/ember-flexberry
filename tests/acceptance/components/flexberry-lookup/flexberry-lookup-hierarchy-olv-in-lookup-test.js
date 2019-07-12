86c62fef0462747c6e1d54fae16745a177f9270c
YUDIN Maksim <myudin@ics.perm.ru>
2019-07-12 16:13:45 +0500

    replaced timeouts and clicks

--- tests/acceptance/components/flexberry-lookup/flexberry-lookup-hierarchy-olv-in-lookup-test.js
+++ tests/acceptance/components/flexberry-lookup/flexberry-lookup-hierarchy-olv-in-lookup-test.js
@@ -1,6 +1,11 @@
 import Ember from 'ember';
 import { executeTest } from './execute-flexberry-lookup-test';
 
+function elementCheck(name) {
+  let $elem = Ember.$(name);
+  if ($elem.length === 0) { return false; }else { return true; }
+}
+
 executeTest('flexberry-lookup hierarchy olv test', (store, assert, app) => {
   assert.expect(16);
   let path = 'components-examples/flexberry-lookup/hierarchy-olv-in-lookup-example';
@@ -8,123 +13,73 @@ executeTest('flexberry-lookup hierarchy olv test', (store, assert, app) => {
   andThen(() => {
     assert.equal(currentPath(), path);
 
-    function elementCheck(name) {
-      let $elem = Ember.$(name);
-      if ($elem.length === 0) { return false; }else { return true;}
-    }
-
     let $lookupChooseButtons = Ember.$('.ui-change');
-    $lookupChooseButtons[0].click();
 
     //First lookup
-    Ember.run(() => {
-      var done = assert.async();
-      setTimeout(function() {
-        assert.equal(elementCheck('button.hierarchical-button'), true, 'has hierarchical-button');
-        assert.equal(elementCheck('button.hierarchy-expand'), true, 'has hierarchy-expand');
-        assert.equal(elementCheck('div.nav-bar'), false, 'no nav-bar');
+    click($lookupChooseButtons[0]);
 
-        let $menuTable = Ember.$('.content table.object-list-view');
-        let $menuTableBody = $menuTable.children('tbody');
-        let $menuTableRowCount = $menuTableBody.children('tr').length;
+    andThen(() => {
+      assert.equal(elementCheck('button.hierarchical-button'), true, 'has hierarchical-button');
+      assert.equal(elementCheck('button.hierarchy-expand'), true, 'has hierarchy-expand');
+      assert.equal(elementCheck('div.nav-bar'), false, 'no nav-bar');
 
-        let $expandButton = Ember.$('button.hierarchy-expand');
-        $expandButton[0].click();
+      let $menuTable = Ember.$('.content table.object-list-view');
+      let $menuTableBody = $menuTable.children('tbody');
+      let $menuTableRowCount = $menuTableBody.children('tr').length;
 
+      let $expandButton = Ember.$('button.hierarchy-expand').eq(0);
+      click($expandButton);
+
+      andThen(() => {
         assert.equal($menuTableRowCount < $menuTableBody.children('tr').length, true, 'hierarchy expanded');
-
+  
         let $nextTableRow = $expandButton.parents('tr').next('tr');
         let $nextTableRowTd = $nextTableRow.children('td').next();
-
+  
         assert.equal($nextTableRowTd.css('padding-left'), '20px', 'expanded text has padding-left: 20px');
-
-        let $closeButton = Ember.$('i.close.icon');
-        $closeButton.click();
-
-        done();
-      }, 7000);
+  
+        click($lookupChooseButtons[1]);
+      });
     });
 
     //Second lookup
-    Ember.run(() => {
-      var done = assert.async();
-      setTimeout(function() {
-        $lookupChooseButtons[1].click();
+    andThen(() => {
+      assert.equal(elementCheck('button.hierarchical-button'), true, 'has hierarchical-button');
+      assert.equal(elementCheck('button.hierarchy-expand'), false, 'no hierarchy-expand');
+      assert.equal(elementCheck('div.nav-bar'), true, 'has nav-bar');
 
-        done();
-      }, 7500);
-    });
+      let $hierarchicalButton = Ember.$('button.hierarchical-button');
+      click($hierarchicalButton);
 
-    Ember.run(() => {
-      var done = assert.async();
-      setTimeout(function() {
-        assert.equal(elementCheck('button.hierarchical-button'), true, 'has hierarchical-button');
-        assert.equal(elementCheck('button.hierarchy-expand'), false, 'no hierarchy-expand');
-        assert.equal(elementCheck('div.nav-bar'), true, 'has nav-bar');
-
-        let $hierarchicalButton = Ember.$('button.hierarchical-button');
-        $hierarchicalButton.click();
-
-        done();
-      }, 8500);
-    });
-
-    Ember.run(() => {
-      var done = assert.async();
-      setTimeout(function() {
+      andThen(() => {
         assert.equal(elementCheck('button.hierarchy-expand'), true, 'has hierarchy-expand');
         assert.equal(elementCheck('div.nav-bar'), false, 'no nav-bar');
-
+  
         let $menuTable = Ember.$('.content table.object-list-view');
         let $menuTableBody = $menuTable.children('tbody');
         let $menuTableRowCount = $menuTableBody.children('tr').length;
-
+  
         let $expandButton = Ember.$('button.hierarchy-expand');
-        $expandButton[0].click();
+        click($expandButton[0]);
+  
+        andThen(() => {
+          assert.equal($menuTableRowCount < $menuTableBody.children('tr').length, true, 'hierarchy expanded');
+    
+          let $nextTableRow = $expandButton.parents('tr').next('tr');
+          let $nextTableRowTd = $nextTableRow.children('td').next();
+    
+          assert.equal($nextTableRowTd.css('padding-left'), '20px', 'expanded text has padding-left: 20px');
 
-        assert.equal($menuTableRowCount < $menuTableBody.children('tr').length, true, 'hierarchy expanded');
-
-        let $nextTableRow = $expandButton.parents('tr').next('tr');
-        let $nextTableRowTd = $nextTableRow.children('td').next();
-
-        assert.equal($nextTableRowTd.css('padding-left'), '20px', 'expanded text has padding-left: 20px');
-
-        let $closeButton = Ember.$('i.close.icon');
-        $closeButton.click();
-
-        done();
-      }, 12500);
+          click($lookupChooseButtons[2]);
+        });
+      });
     });
 
     //Third lookup
-    Ember.run(() => {
-      var done = assert.async();
-      setTimeout(function() {
-        $lookupChooseButtons[2].click();
-
-        done();
-      }, 13000);
-    });
-
-    Ember.run(() => {
-      var done = assert.async();
-      setTimeout(function() {
-        assert.equal(elementCheck('button.hierarchical-button'), false, 'no hierarchical-button');
-        assert.equal(elementCheck('button.hierarchy-expand'), false, 'no hierarchy-expand');
-        assert.equal(elementCheck('div.nav-bar'), true, 'has nav-bar');
-
-        let $closeButton = Ember.$('i.close.icon');
-        $closeButton.click();
-
-        done();
-      }, 14000);
-    });
-
-    Ember.run(() => {
-      var done = assert.async();
-      setTimeout(function() {
-        done();
-      }, 15000);
+    andThen(() => {
+      assert.equal(elementCheck('button.hierarchical-button'), false, 'no hierarchical-button');
+      assert.equal(elementCheck('button.hierarchy-expand'), false, 'no hierarchy-expand');
+      assert.equal(elementCheck('div.nav-bar'), true, 'has nav-bar');
     });
   });
 });

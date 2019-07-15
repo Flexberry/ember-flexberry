5f0cf52ed3b01afc654f355b93e2e0360d9510ed
DubrovinPavel <dubrovip@yandex.ru>
2019-07-15 14:00:37 +0500

    Update test-generator.sh

--- scripts/test-generator.sh
+++ scripts/test-generator.sh
@@ -16,15 +16,24 @@ pushd "$TMP_DIR"
 # Initialize new ember app and install addon from the build.
 # EmberCLI asks whether it needs to overwrite existing files,
 # so we need to remove them for non-interactive build.
-ember init
+ember init --skip-npm
 cp app/index.html .
 rm -r app/*
 mv index.html app
+npm install
+
+# With version 1.11.1 addon installing fails on ember-cli@2.4.3
+npm install resolve@1.11.0
+
 ember install "${ADDON_DIR}"
 #npm install dexie@1.3.6
-rm -f ./ember-cli-build.js
-cp "${ADDON_DIR}/vendor/flexberry/ember-cli-build.js" .
 rm -f ./.jscsrc
+
+# We want to run tests under Headless Chrome
+# So we need to replace testem.js
+popd
+cp -f ./testem.js "$TMP_DIR/testem.js"
+pushd "$TMP_DIR"
 
 # Generate components using Dummy metamodel and test them.
 ember generate flexberry-application app --metadata-dir=${META_DIR}
@@ -41,8 +50,19 @@ mkdir -p "$TMP_DIR"
 rm -rf "$TMP_DIR/*"
 pushd "$TMP_DIR"
 
-ember addon new-addon-for-tests
+ember addon new-addon-for-tests --skip-npm
 pushd new-addon-for-tests
+
+popd
+popd
+cp -f ./testem.js "$TMP_DIR/new-addon-for-tests/testem.js"
+pushd "$TMP_DIR"
+pushd new-addon-for-tests
+
+npm install
+
+# With version 1.11.1 addon installing fails on ember-cli@2.4.3
+npm install resolve@1.11.0
 
 ember install "${ADDON_DIR}"
 

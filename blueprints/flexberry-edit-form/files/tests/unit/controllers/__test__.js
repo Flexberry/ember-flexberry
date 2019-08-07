84e605b83cdc98a6687769f6694953ce78775e5a
DubrovinPavel <dubrovip@yandex.ru>
2019-08-07 17:52:28 +0500

    Fix blueprints

--- blueprints/flexberry-edit-form/files/tests/unit/controllers/__test__.js
+++ blueprints/flexberry-edit-form/files/tests/unit/controllers/__test__.js
@@ -3,12 +3,14 @@ import { moduleFor, test } from 'ember-qunit';
 moduleFor('controller:<%= entityName %>', 'Unit | Controller | <%= entityName %>', {
   // Specify the other units that are required for this test.
   needs: [
+    'controller:advlimit-dialog',
     'controller:flexberry-file-view-dialog',
     'controller:lookup-dialog',
     'service:detail-interaction',
     'service:objectlistview-events',
     'service:user-settings',
     'service:app-state',
+    'service:adv-limit',
   ],
 });
 

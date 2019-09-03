5e65f9894cc5c964e8f94658db260f97ee2cb021
Sokolov Andrey <s.andrey-2011@yandex.ru>
2019-09-03 12:25:44 +0500

    Add method create offline schema

--- blueprints/flexberry-core-offline/index.js
+++ blueprints/flexberry-core-offline/index.js
@@ -1,5 +1,8 @@
 "use strict";
 const skipConfirmationFunc = require('../utils/skip-confirmation');
+const fs = require("fs");
+const path = require("path");
+const ModelBlueprint = require('../flexberry-model/ModelBlueprint');
 module.exports = {
     description: 'Generates core-offline entities for flexberry.',
     availableOptions: [
@@ -11,6 +14,33 @@ module.exports = {
         return false;
     },
 
+    createOfflineSchema: function (options) {
+        let modelsDir = path.join(options.metadataDir, "models");
+        var listModels = fs.readdirSync(modelsDir);
+        var models = [];
+        for (var _i = 0, listModels_1 = listModels; _i < listModels_1.length; _i++) {
+            var modelName = listModels_1[_i];
+            var model = ModelBlueprint.default.loadModel(modelsDir, modelName);
+            var attrs = model.attrs.map((a) => a.name).join(",");
+            var belongsTo = model.belongsTo.map((a) => a.name).join(",");
+            var hasMany = model.hasMany.map((a) => { return "*" + a.name }).join(",");
+            models.push("'" + modelName +  "': 'id" 
+                + this.updateOfflineSchemaString(attrs) 
+                + this.updateOfflineSchemaString(belongsTo) 
+                + this.updateOfflineSchemaString(hasMany) + "'");
+        }
+
+        return models.join(",\n");
+    },
+
+    updateOfflineSchemaString: function (valueString) {
+        if (valueString != undefined && valueString !== "") {
+            valueString = "," + valueString;
+        }
+
+        return valueString;
+    },
+
     processFiles(intoDir, templateVariables) {
         let skipConfirmation = this.options.skipConfirmation;
         if (skipConfirmation) {
@@ -18,6 +48,22 @@ module.exports = {
         }
 
         return this._super.processFiles.apply(this, [intoDir, templateVariables]);
+    },
+
+    	    /**
+     * Blueprint Hook locals.
+     * Use locals to add custom template variables. The method receives one argument: options.
+     *
+     * @method locals
+     * @public
+     *
+     * @param {Object} options Options is an object containing general and entity-specific options.
+     * @return {Object} Custom template variables.
+     */
+    locals: function (options) {
+        return {
+            offlineSchema: this.createOfflineSchema(options),
+        };
     }
 };
 //# sourceMappingURL=index.js.map

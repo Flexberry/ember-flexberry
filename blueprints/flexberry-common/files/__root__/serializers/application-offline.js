b5aa9a1eafa610c2a3944b744512c4d32f9b3e18
AntonIV <anton-iv87@yandex.ru>
2019-09-05 10:39:26 +0500

    Delete offline files in non-offline blueprints

--- blueprints/flexberry-common/files/__root__/serializers/application-offline.js
+++
@@ -1,7 +0,0 @@
-import DS from 'ember-data';
-import { Serializer } from 'ember-flexberry-data';
-
-export default Serializer.Offline.extend(DS.EmbeddedRecordsMixin, {
-  attrs: {
-  }
-});
--- blueprints/flexberry-serializer-init/files/__root__/serializers/__name__-offline.js
+++
@@ -1,6 +0,0 @@
-import { OfflineSerializer as <%= className %>Serializer } from
-  '../mixins/regenerated/serializers/<%= name %>-offline';
-import <%if(parentModelName) {%><%=parentClassName %>Serializer from <%= (parentExternal ? "set path to '" : "'./") + parentModelName %>-offline';<%}else {%>__ApplicationSerializer from './application-offline';<%}%>
-
-export default <%if(parentModelName) {%><%=parentClassName%>Serializer<%}else{%>__ApplicationSerializer<%}%>.extend(<%= className %>Serializer, {
-});

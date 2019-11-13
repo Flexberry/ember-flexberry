cea1870fee88247c6c5add9ff9fe337653235b44
Масалкин Павел <pashamasalkin@yandex.ru>
2019-11-13 13:47:58 +0500

    Update numeric-autocomplete.js

--- tests/dummy/app/controllers/components-examples/flexberry-lookup/numeric-autocomplete.js
+++ tests/dummy/app/controllers/components-examples/flexberry-lookup/numeric-autocomplete.js
@@ -1,6 +1,4 @@
 import BaseEditFormController from 'ember-flexberry/controllers/edit-form';
 import EditFormControllerOperationsIndicationMixin from 'ember-flexberry/mixins/edit-form-controller-operations-indication';
 
-export default BaseEditFormController.extend(EditFormControllerOperationsIndicationMixin, {
- 
-});
+export default BaseEditFormController.extend(EditFormControllerOperationsIndicationMixin, {});

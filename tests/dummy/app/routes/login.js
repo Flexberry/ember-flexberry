a57aaff3646df27d35557483d7f76cac477988a4
Pepelyaeva Kseniya <kpepelyaeva@ics.perm.ru>
2020-02-03 13:27:50 +0500

    Add usermenu foto

--- tests/dummy/app/routes/login.js
+++ tests/dummy/app/routes/login.js
@@ -1,4 +1,19 @@
 import Route from '@ember/routing/route';
+const { $, run } = Ember;
+const { next } = run;
 
 export default Route.extend({
+  activate() {
+    this._super();
+    next(this, function(){
+      $('.main.menu.ui.sidebar').css('cssText', `display: none !important;`);
+      $('.full.height').css('margin-left', 0);
+    });
+  },
+
+  deactivate() {
+    this._super();
+    $('.main.menu.ui.sidebar').css('display', '');
+    $('.full.height').css('margin-left', '');
+  }
 });

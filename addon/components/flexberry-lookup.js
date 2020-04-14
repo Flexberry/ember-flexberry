369e4ba328efcca0a2662f2b122e6fbcdd3fb022
INaidanov <INaidanov@ICS-KK12711>
2020-04-14 15:15:03 +0500

    Затирание текста при focusOut лукапа в автокомплите

--- addon/components/flexberry-lookup.js
+++ addon/components/flexberry-lookup.js
@@ -836,6 +836,17 @@ export default FlexberryBaseComponent.extend({
       };
 
       this.sendAction('preview', previewData);
+    },
+
+    /**
+     * Input focused out (during autocomplete)
+     *
+     * @method actions.onInputFocusOut
+     */
+    onInputFocusOut() {
+      if (Ember.isNone(this.get('value'))) {
+        this.set('displayValue', null);
+      }
     }
   },
 

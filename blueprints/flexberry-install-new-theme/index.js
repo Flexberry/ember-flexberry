798e94532e6f200102cfd8c5e225c65792d9c012
Eugene Kolegov <ekolegov@outlook.com>
2020-04-16 15:16:57 +0500

    Fix app generation with blue-sky theme

--- blueprints/flexberry-install-new-theme/index.js
+++ blueprints/flexberry-install-new-theme/index.js
@@ -3,8 +3,6 @@ module.exports = {
 	afterInstall: function() {
 		var _this = this;
 
-		var lessOption = '        \'node_modules/ember-flexberry-themes\','
-
 		var fontsImports = '\n  app.import(\'vendor/fonts.css\');\n' +
 		'\n  // GOSTUI2\n' +
 		'  app.import(\'vendor/fonts/GOSTUI2/GOSTUI2-w170-regular_g_temp.eot\', { destDir: \'assets/fonts\' });\n' +
@@ -30,26 +28,13 @@ module.exports = {
 		'  app.import(\'vendor/fonts/guideline-icons/guideline-icons.svg\', { destDir: \'assets/fonts/guideline-icons\' });\n' +
 		'\n'
 
-		return this.insertIntoFile(
-		  'ember-cli-build.js',
-		  lessOption,
-		  {
-				after: '    lessOptions: {\n' + '      paths: [\n'
-		  }
+		return _this.insertIntoFile(
+			'ember-cli-build.js',
+			fontsImports,
+			{
+				before: '\n  return app.toTree();\n'
+			}
 		).then(function() {
-		  return _this.insertIntoFile(
-				'app/styles/app.less',
-				'@import \'src/flexberry-imports\';\n'
-		  );
-		}).then(function() {
-		  return _this.insertIntoFile(
-				'ember-cli-build.js',
-				fontsImports,
-				{
-					before: '\n  return app.toTree();\n'
-				}
-		  );
-		}).then(function() {
 		  return _this.insertIntoFile(
 				'ember-cli-build.js',
 				stylesImports,
@@ -58,42 +43,13 @@ module.exports = {
 				}
 		  );
 		}).then(function() {
-      return _this.insertIntoFile('ember-cli-build.js', '\nconst autoprefixer = require(\'autoprefixer\');\n\n', { before: 'module.exports = function' });
-    }).then(function() {
-      var afterPostcssOptions = 'semantic-ui\'\n      ]\n    }';
-      var postcssOptions = ',\n    postcssOptions: {\n' +
-        '      compile: {\n' +
-        '        enabled: false,\n' +
-        '        browsers: [\'last 3 versions\'],\n' +
-        '      },\n' +
-        '      filter: {\n' +
-        '        enabled: true,\n' +
-        '        plugins: [\n' +
-        '          {\n' +
-        '            module: autoprefixer,\n' +
-        '            options: {\n' +
-        '              browsers: [\'last 2 versions\']\n' +
-        '            }\n' +
-        '          }\n' +
-        '        ]\n' +
-        '      }\n' +
-        '    }\n';
-
-      return _this.insertIntoFile('ember-cli-build.js', postcssOptions, { after: afterPostcssOptions });
-    }).then(function() {
       return _this.insertIntoFile('app/templates/application.hbs', '-guideline ', { after: '{{flexberry-sitemap' });
     }).then(function() {
       return _this.insertIntoFile('app/templates/mobile/application.hbs', '-guideline ', { after: '{{flexberry-sitemap' });
-    }).then(function() {
-      return _this.addPackagesToProject([
-        { name: 'autoprefixer', target: '^6' },
-        { name: 'ember-flexberry-themes' }
-      ]);
 		}).then(function() {
 			fs.copySync('node_modules/ember-flexberry-themes/src/themes/gos/assets/fonts', 'vendor/fonts');
 			fs.copySync('node_modules/ember-flexberry-themes/src/themes/gos/assets/fonts.css', 'vendor/fonts.css');
 			fs.copySync('node_modules/ember-flexberry-themes/src/themes/gos/assets/guideline-icons.css', 'vendor/guideline-icons.css');
-			fs.copySync('node_modules/ember-flexberry-themes/src/theme.less', 'app/styles/theme.less');
 			fs.copySync('node_modules/ember-flexberry-themes/src/theme.config.example', 'theme.config');
 		});
 	}

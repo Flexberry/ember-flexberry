633c169b777a12c169246279757b45635b3e581f
pepelyaeva <pepelyaevaks@gmail.com>
2020-05-06 20:16:32 +0500

    Fix modal-dialog template

--- tests/dummy/app/routes/application.js
+++ tests/dummy/app/routes/application.js
@@ -2,6 +2,16 @@ import Route from '@ember/routing/route';
 import ModalApplicationRouteMixin from 'ember-flexberry/mixins/modal-application-route';
 
 export default Route.extend(ModalApplicationRouteMixin, {
+  /**
+  Returns model related to current route.
+
+  @method model
+  */
+  /* eslint-disable no-unused-vars */
+  model(params) {
+    return this.get('store').createRecord('components-examples/flexberry-file/settings-example/base', {});
+  },
+
   actions: {
     /**
       Handles onRefresh action.

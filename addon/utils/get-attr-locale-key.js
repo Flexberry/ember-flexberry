e032c602a27b3a754448b6042cc34b56c8e1179a
pmasalkin <pashamasalkin@yandex.ru>
2020-07-22 09:37:39 +0500

    Update get-attr-locale-key.js

--- addon/utils/get-attr-locale-key.js
+++ addon/utils/get-attr-locale-key.js
@@ -1,7 +1,7 @@
 export default function getAttrLocaleKey(mainModelName, projectionName, bindingPath, nameRelationship) {
- if (nameRelationship) {
+  if (nameRelationship) {
     bindingPath = `${nameRelationship}.${bindingPath}`;
- }
+  }
 
- return `models.${mainModelName}.projections.${projectionName}.${bindingPath}.__caption__`
+  return `models.${mainModelName}.projections.${projectionName}.${bindingPath}.__caption__`;
 }

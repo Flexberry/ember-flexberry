54a6bacfba0a19a7146e2e6f26fddc6eb614e467
Eugene Kolegov <KollegOFF@users.noreply.github.com>
2020-09-03 13:22:24 +0500

    Add flexberry-acceptance-test blueprint (#1169)
    * Add config update for dummy app of addon in default blueprint
    * Fix locales generation for dummy app of addon
    * Fix metadata to generate dummy app
    * Skip JSHint tests in generator test

--- blueprints/flexberry-application/index.js
+++ blueprints/flexberry-application/index.js
@@ -85,6 +85,7 @@ var ApplicationBlueprint = (function () {
         this.promise = this.emberGenerateFlexberryGroup("transform-test");
         this.promise = this.emberGenerateFlexberryGroup("controller-test");
         this.promise = this.emberGenerateFlexberryGroup("route-test");
+        this.promise = this.emberGenerateFlexberryGroup("flexberry-acceptance-test");
         this.promise = this.emberGenerateFlexberryGroup("flexberry-model");
         this.promise = this.emberGenerateFlexberryGroup("flexberry-model-init");
         this.promise = this.emberGenerateFlexberryGroup("flexberry-serializer-init");

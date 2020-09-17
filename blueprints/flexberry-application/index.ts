9e640ab22b1edc82cd0bdf64d7ee19c6aad1f013
DubrovinPavel <dubrovip@yandex.ru>
2020-09-17 16:50:52 +0500

    Remove index.js route generation

--- blueprints/flexberry-application/index.ts
+++ blueprints/flexberry-application/index.ts
@@ -116,9 +116,6 @@ class ApplicationBlueprint {
     this.promise = this.emberGenerateFlexberryGroup("flexberry-enum");
     this.promise = this.emberGenerateFlexberryGroup("flexberry-list-form");
     this.promise = this.emberGenerateFlexberryGroup("flexberry-edit-form");
-    if( !(options.project.pkg.keywords && options.project.pkg.keywords["0"] === "ember-addon" || fs.existsSync("app/routes/index.js") )) {
-      this.promise = this.emberGenerate("route", "index");
-    }
     this.promise = this.emberGenerate("flexberry-common", "app");
     this.promise = this.emberGenerate("flexberry-core", "app");
     this.promise = this.promise

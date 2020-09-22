217a8a9c200097b3da4939c4d6cc7205c55d3342
TeterinaSvetlana <40786685+TeterinaSvetlana@users.noreply.github.com>
2020-09-22 20:55:43 +0500

    Add helper for test the opening of edit forms (#1191)

--- addon-test-support/index.js
+++ addon-test-support/index.js
@@ -3,3 +3,4 @@ import './check-olv-config';
 import './check-olv-sort-for-each-column';
 import './check-olv-sort-on-all-columns';
 import './check-delete-record-from-olv';
+import './open-editform';

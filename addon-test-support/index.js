dbf43e1a6d302a661fe292e36e7fef2d17c79a27
s-andrey <s.andrey-2011@yandex.ru>
2020-09-22 20:18:31 +0500

    Add helper for testing deletions on lists (#1190)

--- addon-test-support/index.js
+++ addon-test-support/index.js
@@ -2,3 +2,4 @@ import './go-to-new-form';
 import './check-olv-config';
 import './check-olv-sort-for-each-column';
 import './check-olv-sort-on-all-columns';
+import './check-delete-record-from-olv';

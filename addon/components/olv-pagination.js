d2c7e334442e36d78510dd0ddac901bdf8136e13
pmasalkin <pashamasalkin@yandex.ru>
2020-09-30 14:31:44 +0500

    Компонент работает

---
+++ addon/components/olv-pagination.js
@@ -0,0 +1,8 @@
+
+import Ember from 'ember';
+import FlexberryBaseComponent from './flexberry-base-component';
+import { translationMacro as t } from 'ember-i18n';
+
+export default FlexberryBaseComponent.extend({
+
+});

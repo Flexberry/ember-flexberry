7f30b8af5d2fe7762be1a63a0ccfe8d692a85276
Масалкин Павел <pashamasalkin@yandex.ru>
2020-11-27 17:36:48 +0500

    Update tests/dummy/app/services/device.js
    Co-authored-by: Eugene Kolegov <KollegOFF@users.noreply.github.com>

--- tests/dummy/app/services/device.js
+++ tests/dummy/app/services/device.js
@@ -3,16 +3,6 @@ import Ember from 'ember';
 
 export default Device.extend({
   /**
-    Get current device is desktop.
-
-    @method isDesktop
-    @return {Boolean} Returns true, if device is desktop.
-  */
-  isDesktop() {
-    return this.desktop() || Ember.$(window).width() >= 480;
-  },
-
-  /**
     Get current device is phone.
 
     @method isMobile
@@ -22,4 +12,3 @@ export default Device.extend({
     return this.mobile() && Ember.$(window).width() < 480;
   }
 });
-

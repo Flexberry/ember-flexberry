951051d9bc1ec5ee7c287c8fbf6e63133d805079
Anisimova Tatyana <TOsotova@ics.perm.ru>
2021-11-17 18:01:14 +0500

    Added test on disabled user settings service.

--- tests/acceptance/components/flexberry-objectlistview/folv-user-settings-test.js
+++ tests/acceptance/components/flexberry-objectlistview/folv-user-settings-test.js
@@ -25,16 +25,18 @@ module('Acceptance | flexberry-objectlistview | per page user settings', {
     store = app.__container__.lookup('service:store');
     route = app.__container__.lookup('route:components-acceptance-tests/flexberry-objectlistview/folv-user-settings');
     userService = app.__container__.lookup('service:user-settings')
+    Ember.set(userService, 'isUserSettingsServiceEnabled', true);
   },
 
   afterEach() {
     // Destroy application.
+    Ember.set(userService, 'isUserSettingsServiceEnabled', true);
     Ember.run(app, 'destroy');
   }
 });
 
 test('check saving of user settings', function(assert) {
-  assert.expect(16);
+  assert.expect(21);
   let uuid = generateUniqueId();
   let arr;
 
@@ -54,7 +56,7 @@ test('check saving of user settings', function(assert) {
           try {
             assert.equal(currentPath(), path);
             let currentUrl = currentURL();
-            assert.ok(currentUrl.contains("perPage=28"), "Переадресация выполнена успешно.");
+            assert.ok(currentUrl.contains("perPage=28"), "Переадресация выполнена успешно (настройка взята из developerUserSettings).");
             checkPaging(assert, '28');
             route.set('developerUserSettings', { FOLVPagingObjectListView: { DEFAULT: { colsOrder: [ { propName: 'name' } ], perPage: 17 } } });
 
@@ -72,8 +74,8 @@ test('check saving of user settings', function(assert) {
                     При тестировании в реальном окружении проходила корректная переадресация на perPage=28, которое уже сохранено в БД
                     и более приоритетно, нежели 17, заданное в developerUserSettings. Поэтому переход осуществляется через visit(pathHelp).
                   */
-
-                  assert.ok(currentUrl.contains("perPage=28"), "Переадресация выполнена успешно.");
+                  let currentUrl = currentURL();
+                  assert.ok(currentUrl.contains("perPage=28"), "Переадресация выполнена успешно (настройка взята из БД).");
                   checkPaging(assert, '28');
                   
                   let done2 = assert.async();
@@ -86,14 +88,17 @@ test('check saving of user settings', function(assert) {
                         try {
                           assert.ok(true, 'Произведён сброс настроек пользователя до developerUserSettings.');
                           let currentUrl = currentURL();
-                          assert.ok(currentUrl.contains("perPage=17"), "Переадресация выполнена успешно.");
+                          assert.ok(currentUrl.contains("perPage=17"), "Переадресация выполнена успешно (настройка взята из developerUserSettings).");
                           checkPaging(assert, '17');
+
+                          let done4 = assert.async();
+                          checkWithDisabledUserSettings(assert, done4);
                         } catch (error) {
+                          deleteRecords(store, modelName, uuid, assert);
+                          deleteUserSetting(assert);
                           throw error;
                         }
                         finally {
-                          deleteRecords(store, modelName, uuid, assert);
-                          deleteUserSetting(assert);
                           done3();
                         }
                       });
@@ -130,6 +135,46 @@ test('check saving of user settings', function(assert) {
     });
   });
 });
+
+function checkWithDisabledUserSettings(assert, asyncDone, uuid) {
+  try{
+    let doneHelp = assert.async();
+    visit(pathHelp);
+    andThen(function() {
+      assert.equal(currentPath(), pathHelp);
+      route.set('developerUserSettings', { FOLVPagingObjectListView: { DEFAULT: { colsOrder: [ { propName: 'name' } ], perPage: 11 } } });
+      Ember.set(userService, 'isUserSettingsServiceEnabled', false);
+
+      // Remove current saved not in Database settings.
+      Ember.set(userService, 'currentUserSettings', {});
+
+      let done1 = assert.async();
+      visit(path);    
+      andThen(function() {
+        try {
+          assert.equal(currentPath(), path);
+          let currentUrl = currentURL();
+          assert.ok(currentUrl.contains("perPage=11"), "Переадресация выполнена успешно (настройка взята из developerUserSettings).");
+          checkPaging(assert, '11');
+        } catch(error) {
+          throw error;
+        } finally {
+          Ember.set(userService, 'isUserSettingsServiceEnabled', true);
+          deleteRecords(store, modelName, uuid, assert);
+          deleteUserSetting(assert);
+          done1();
+        }
+      });
+      doneHelp();
+    });
+  } catch(error) {
+    deleteRecords(store, modelName, uuid, assert);
+    deleteUserSetting(assert);
+    throw error;
+  } finally {
+    asyncDone();
+  }
+}
 
 // Function to check current perPage value on page.
 function checkPaging(assert, expectedCount) {

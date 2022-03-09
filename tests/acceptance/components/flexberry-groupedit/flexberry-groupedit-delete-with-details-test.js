45f6e7a6d6ddfbae0018c176b6171e96c04e3505
Anisimova Tatyana <TOsotova@ics.perm.ru>
2022-03-09 14:13:38 +0500

    Added test for FGE on deleting with details

--- tests/acceptance/components/flexberry-groupedit/flexberry-groupedit-delete-with-details-test.js
+++ tests/acceptance/components/flexberry-groupedit/flexberry-groupedit-delete-with-details-test.js
@@ -32,6 +32,7 @@ module('Acceptance | flexberry-groupedit | delete with details', {
   });
 
 test('delete with details', (assert) => {
+  let mainSuggestionRecord; 
   let initTestData = function(createdRecordsPrefix) {
     // Add records for deleting. 
     return Ember.RSVP.Promise.all([
@@ -45,8 +46,9 @@ test('delete with details', (assert) => {
     .then((createdCustomRecords) => 
       Ember.RSVP.Promise.all([
         store.createRecord(modelName, { text: createdRecordsPrefix + "0", type: createdCustomRecords[0], author: createdCustomRecords[1], editor1: createdCustomRecords[1] }).save()])
-      .then((suggestions) => 
-        Ember.RSVP.Promise.all([
+      .then((suggestions) => {
+        mainSuggestionRecord = suggestions[0];
+        return Ember.RSVP.Promise.all([
           store.createRecord(commentModelName, { text: createdRecordsPrefix + "0", suggestion: suggestions[0], author: createdCustomRecords[1] }).save(),
           store.createRecord(commentModelName, { text: createdRecordsPrefix + "1", suggestion: suggestions[0], author: createdCustomRecords[1] }).save(),
           store.createRecord(commentModelName, { text: createdRecordsPrefix + "2", suggestion: suggestions[0], author: createdCustomRecords[1] }).save()])
@@ -55,57 +57,7 @@ test('delete with details', (assert) => {
             store.createRecord(commentVoteModelName, { name: createdRecordsPrefix + "0", comment: comments[0], applicationUser: createdCustomRecords[1] }).save(),
             store.createRecord(commentVoteModelName, { name: createdRecordsPrefix + "1", comment: comments[0], applicationUser: createdCustomRecords[1] }).save(),
             store.createRecord(commentVoteModelName, { name: createdRecordsPrefix + "2", comment: comments[1], applicationUser: createdCustomRecords[1] }).save()])
-        )))
-    };
-
-  Ember.run(() => {
-    let done1 = assert.async();
-    let createdRecordsPrefix = 'fge-delete-with-details-test' + generateUniqueId();
-    initTestData(createdRecordsPrefix).then(() => {
-      visit(path +'?createdRecordsPrefix=' + createdRecordsPrefix);
-      andThen(() => {
-        assert.equal(currentPath(), path, createdRecordsPrefix);
-        done1();
-      });
-    });
-  });
-});
-
-
-/*executeTest('check delete with details', (store, assert, app) => {
-  assert.expect(5);
-  let path = 'components-acceptance-tests/flexberry-objectlistview/delete-with-details';
-  let modelName = 'ember-flexberry-dummy-suggestion';
-  let commentModelName = 'ember-flexberry-dummy-comment';
-  let commentVoteModelName = 'ember-flexberry-dummy-comment-vote';
-
-  let initTestData = function(createdRecordsPrefix) {
-    // Add records for deleting. 
-    return Ember.RSVP.Promise.all([
-      store.createRecord('ember-flexberry-dummy-suggestion-type', { name: createdRecordsPrefix + "0" }).save(),
-      store.createRecord('ember-flexberry-dummy-application-user', { 
-                                                                    name: createdRecordsPrefix + "1",
-                                                                    eMail: "1",
-                                                                    phone1: "1"
-                                                                   }).save()
-    ])
-    .then((createdCustomRecords) => 
-      Ember.RSVP.Promise.all([
-        store.createRecord(modelName, { text: createdRecordsPrefix + "0", type: createdCustomRecords[0], author: createdCustomRecords[1], editor1: createdCustomRecords[1] }).save(),
-        store.createRecord(modelName, { text: createdRecordsPrefix + "1", type: createdCustomRecords[0], author: createdCustomRecords[1], editor1: createdCustomRecords[1] }).save(),
-        store.createRecord(modelName, { text: createdRecordsPrefix + "2", type: createdCustomRecords[0], author: createdCustomRecords[1], editor1: createdCustomRecords[1] }).save()])
-      .then((suggestions) => 
-        Ember.RSVP.Promise.all([
-          store.createRecord(commentModelName, { text: createdRecordsPrefix + "0", suggestion: suggestions[0], author: createdCustomRecords[1] }).save(),
-          store.createRecord(commentModelName, { text: createdRecordsPrefix + "1", suggestion: suggestions[0], author: createdCustomRecords[1] }).save(),
-          store.createRecord(commentModelName, { text: createdRecordsPrefix + "2", suggestion: suggestions[1], author: createdCustomRecords[1] }).save(),
-          store.createRecord(commentModelName, { text: createdRecordsPrefix + "3", suggestion: suggestions[1], author: createdCustomRecords[1] }).save()])
-        .then((comments) => 
-          Ember.RSVP.Promise.all([
-            store.createRecord(commentVoteModelName, { name: createdRecordsPrefix + "0", comment: comments[0], applicationUser: createdCustomRecords[1] }).save(),
-            store.createRecord(commentVoteModelName, { name: createdRecordsPrefix + "1", comment: comments[0], applicationUser: createdCustomRecords[1] }).save(),
-            store.createRecord(commentVoteModelName, { name: createdRecordsPrefix + "2", comment: comments[1], applicationUser: createdCustomRecords[1] }).save()])
-        )))
+        )}))
     };
 
   let getRows = function(){
@@ -122,7 +74,7 @@ test('delete with details', (assert) => {
 
     // Check that the records have been added.
     let recordIsForDeleting = $rows().reduce((sum, element) => {
-      let nameRecord = Ember.$.trim(element.children[2].innerText);
+      let nameRecord = Ember.$.trim(element.children[1].children[0].children[0].value);
       let flag = nameRecord.indexOf(searchedRecord) >= 0;
       return sum + flag;
     }, 0);
@@ -134,7 +86,7 @@ test('delete with details', (assert) => {
     let $rows = getRows();
     let $deleteBtnInRow = undefined;
     $rows().forEach(function(element)  {
-      let nameRecord = Ember.$.trim(element.children[2].innerText);
+      let nameRecord = Ember.$.trim(element.children[1].children[0].children[0].value);
       if (nameRecord.indexOf(searchedRecord) >= 0) {
         $deleteBtnInRow = Ember.$('.object-list-view-row-delete-button', element);
       }
@@ -154,94 +106,64 @@ test('delete with details', (assert) => {
     return false;
   };
 
-  // Add records for deleting.
   Ember.run(() => {
     let done1 = assert.async();
-    let createdRecordsPrefix = 'folv-delete-with-details-test' + generateUniqueId();
+    let createdRecordsPrefix = 'fge-delete-with-details-test' + generateUniqueId();
     initTestData(createdRecordsPrefix).then(() => {
-      let builder = new QueryBuilder(store).from(modelName).count();
-      let done = assert.async();
-      store.query(modelName, builder.build()).then((result) => {
-        visit(path + '?perPage=' + result.meta.count);
-        andThen(() => {
-          assert.equal(currentPath(), path);
+      visit(path +'?createdRecordsPrefix=' + createdRecordsPrefix);
+      andThen(() => {
+        assert.equal(currentPath(), path, createdRecordsPrefix);
 
-          // Check records added.
-          assert.equal(checkRecordsWereAdded(createdRecordsPrefix + "0") > 0, true, 1 + ' record added');
-          assert.equal(checkRecordsWereAdded(createdRecordsPrefix + "1") > 0, true, 2 + ' record added');
-          assert.equal(checkRecordsWereAdded(createdRecordsPrefix + "2") > 0, true, 3 + ' record added');
+        // Check records added.
+        assert.equal(checkRecordsWereAdded(createdRecordsPrefix + "0") > 0, true, 1 + ' record added');
+        assert.equal(checkRecordsWereAdded(createdRecordsPrefix + "1") > 0, true, 2 + ' record added');
+        assert.equal(checkRecordsWereAdded(createdRecordsPrefix + "2") > 0, true, 3 + ' record added');
 
-          let $deleteButton1 = getDeleteButton(createdRecordsPrefix + "0");
-          let done2 = assert.async();
-          Ember.run(() => {
-            // An exception can be thrown to console due to observer on detail's count.
-            $deleteButton1.click();
-          });
-          wait().then(() => {
-            assert.equal(checkRecordsWereAdded(createdRecordsPrefix + "0"), 0, 1 + ' record deleted');
-            assert.equal(checkRecordsWereAdded(createdRecordsPrefix + "1") > 0, true, 2 + ' still on OLV');
-            assert.equal(checkRecordsWereAdded(createdRecordsPrefix + "2") > 0, true, 3 + ' still on OLV');
-            
-            // Check local storage.
-            assert.notOk(lookAtLocalStore(modelName, 'text', createdRecordsPrefix + "0"), "1 suggestion deleted on store");
-            assert.ok(lookAtLocalStore(modelName, 'text', createdRecordsPrefix + "1"), "2 suggestion still on store");
-            assert.ok(lookAtLocalStore(modelName, 'text', createdRecordsPrefix + "2"), "3 suggestion still on store");
-
-            assert.notOk(lookAtLocalStore(commentModelName, 'text', createdRecordsPrefix + "0"), "1 comment deleted");
-            assert.notOk(lookAtLocalStore(commentModelName, 'text', createdRecordsPrefix + "1"), "2 comment deleted");
-            assert.ok(lookAtLocalStore(commentModelName, 'text', createdRecordsPrefix + "2"), "3 comment still on store");
-            assert.ok(lookAtLocalStore(commentModelName, 'text', createdRecordsPrefix + "3"), "4 comment still on store");
-
-            assert.notOk(lookAtLocalStore(commentVoteModelName, 'comment.text', createdRecordsPrefix + "0"), "Comment vote deleted");
-            assert.notOk(lookAtLocalStore(commentVoteModelName, 'comment.text', createdRecordsPrefix + "1"), "Comment vote deleted");
-
-            let builder = new QueryBuilder(store, modelName)
-              .where(new SimplePredicate('text', "==", createdRecordsPrefix + "0"));
-            let done3 = assert.async();
-            store.query(modelName, builder.build())
-            .then((data) => {
-              assert.equal(data.get('length'), 0, '1 suggestion deleted on backend');
-
-              let $deleteButton2 = getDeleteButton(createdRecordsPrefix + "1");
-              let done4 = assert.async();
-              Ember.run(() => {
-                // An exception can be thrown to console due to observer on detail's count.
-                $deleteButton2.click();
-              });
-              wait().then(() => {
-                assert.equal(checkRecordsWereAdded(createdRecordsPrefix + "1"), 0, 2 + ' record deleted');
-                assert.equal(checkRecordsWereAdded(createdRecordsPrefix + "2") > 0, true, 3 + ' still on OLV');
-                
-                // Check local storage.
-                assert.notOk(lookAtLocalStore(modelName, 'text', createdRecordsPrefix + "1"), "2 suggestion deleted on store");
-                assert.ok(lookAtLocalStore(modelName, 'text', createdRecordsPrefix + "2"), "3 suggestion still on store");
-
-                assert.notOk(lookAtLocalStore(commentModelName, 'text', createdRecordsPrefix + "2"), "3 comment deleted");
-                assert.notOk(lookAtLocalStore(commentModelName, 'text', createdRecordsPrefix + "3"), "4 comment deleted");
-
-                let $deleteButton3 = getDeleteButton(createdRecordsPrefix + "2");
-                let done5 = assert.async();
-                Ember.run(() => {
-                  // An exception can be thrown to console due to observer on detail's count.
-                  $deleteButton3.click();
-                });
-                wait().then(() => {
-                  assert.equal(checkRecordsWereAdded(createdRecordsPrefix + "2"), 0, 3 + ' record deleted');
-                  
-                  // Check local storage.
-                  assert.notOk(lookAtLocalStore(modelName, 'text', createdRecordsPrefix + "2"), "3 suggestion deleted on store");
-                  done5();
-                });
-                done4();
-              });
-              done3();
-            });
-            done2();
-          });
+        let $deleteButton1 = getDeleteButton(createdRecordsPrefix + "0");
+        let done2 = assert.async();
+        Ember.run(() => {
+          // An exception can be thrown to console due to observer on detail's count.
+          $deleteButton1.click();
         });
-        done();
+
+        wait().then(() => {
+          assert.equal(checkRecordsWereAdded(createdRecordsPrefix + "0"), 0, 1 + ' record deleted');
+          assert.equal(checkRecordsWereAdded(createdRecordsPrefix + "1") > 0, true, 2 + ' still on OLV');
+          assert.equal(checkRecordsWereAdded(createdRecordsPrefix + "2") > 0, true, 3 + ' still on OLV');
+          
+          assert.notOk(lookAtLocalStore(commentModelName, 'text', createdRecordsPrefix + "0"), "1 comment deleted");
+          assert.ok(lookAtLocalStore(commentModelName, 'text', createdRecordsPrefix + "1"), "2 comment still on store");
+          assert.ok(lookAtLocalStore(commentModelName, 'text', createdRecordsPrefix + "2"), "3 comment still on store");
+
+          assert.notOk(lookAtLocalStore(commentVoteModelName, 'comment.text', createdRecordsPrefix + "0"), "Comment votes for 1 deleted");
+          assert.ok(lookAtLocalStore(commentVoteModelName, 'comment.text', createdRecordsPrefix + "1"), "Comment votes for 2 still on store");
+
+          let builder = new QueryBuilder(store, commentModelName)
+            .where(new SimplePredicate('text', "==", createdRecordsPrefix + "0"));
+          let done3 = assert.async();
+          store.query(commentModelName, builder.build())
+          .then((data) => {
+            assert.equal(data.get('length'), 1, '1 comment is not deleted on backend');
+
+            let builder = new QueryBuilder(store, commentVoteModelName)
+              .where(new SimplePredicate('comment.text', "==", createdRecordsPrefix + "0"));
+            let done4 = assert.async();
+            store.query(commentVoteModelName, builder.build())
+            .then((data) => {
+              assert.equal(data.get('length'), 2, 'Comment votes for comment 1 not deleted on backend');
+              let done5 = assert.async();
+
+              // An exception can be thrown to console due to observer on detail's count.
+              mainSuggestionRecord.destroyRecord().then(() => done5());
+              done4();
+            });
+            done3();
+          });
+          done2();
+        });
+        done1();
       });
-      done1();
     });
   });
-});*/
+});
+

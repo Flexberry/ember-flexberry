bdda3b648c190bdc44b24b37d212a4d1a8e940ad
Churekov Valentin <VChurekov@ics.perm.ru>
2022-12-29 13:00:50 +0500

    Controllers.

--- tests/unit/controllers/detail-edit-form-test.js
+++ tests/unit/controllers/detail-edit-form-test.js
@@ -1,28 +1,16 @@
-import { moduleFor, test } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
 import { run } from '@ember/runloop';
 
-moduleFor('controller:detail-edit-form', 'Unit | Controller | detail edit form', {
-  needs: [
-    'controller:advlimit-dialog',
-    'controller:colsconfig-dialog',
-    'controller:flexberry-file-view-dialog',
-    'controller:lookup-dialog',
-    'controller:filters-dialog',
-    'service:detail-interaction',
-    'service:objectlistview-events',
-    'service:user-settings',
-    'service:app-state',
-    'service:adv-limit',
-    'service:lookup-events',
-  ],
-});
+module('Unit | Controller | detail edit form', function (hooks) {
+  setupTest(hooks);
 
-// Replace this with your real tests.
-test('it exists', function(assert) {
-  let controller;
-  run(() => {
-    controller = this.subject();
+  test('it exists', function(assert) {
+    let controller;
+    run(() => {
+      controller = this.owner.lookup('controller:detail-edit-form');
+    });
+  
+    assert.ok(controller);
   });
-
-  assert.ok(controller);
 });
--- tests/unit/controllers/list-form-test.js
+++ tests/unit/controllers/list-form-test.js
@@ -1,18 +1,11 @@
-import { moduleFor, test } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
 
-moduleFor('controller:list-form', 'Unit | Controller | list form', {
-  needs: [
-    'controller:advlimit-dialog',
-    'controller:colsconfig-dialog',
-    'controller:filters-dialog',
-    'service:objectlistview-events',
-    'service:user-settings',
-    'service:adv-limit',
-  ],
-});
+module('Unit | Controller | list form', function (hooks) {
+  setupTest(hooks);
 
-// Replace this with your real tests.
-test('it exists', function(assert) {
-  let controller = this.subject();
-  assert.ok(controller);
+  test('it exists', function(assert) {
+    let controller = this.owner.lookup('controller:list-form');
+    assert.ok(controller);
+  });
 });

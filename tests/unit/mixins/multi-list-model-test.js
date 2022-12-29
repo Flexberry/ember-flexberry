dca266496981c1715a3620a4a7f9f3554dd1d35c
Churekov Valentin <VChurekov@ics.perm.ru>
2022-12-29 14:11:23 +0500

    Mixins

--- tests/unit/mixins/multi-list-model-test.js
+++ tests/unit/mixins/multi-list-model-test.js
@@ -2,10 +2,10 @@ import EmberObject from '@ember/object';
 import MultiListModelMixin from 'ember-flexberry/mixins/multi-list-model';
 import { module, test } from 'qunit';
 
-module('Unit | Mixin | multi list model');
-
-test('it works', function(assert) {
-  let MultiListModelObject = EmberObject.extend(MultiListModelMixin);
-  let subject = MultiListModelObject.create();
-  assert.ok(subject);
+module('Unit | Mixin | multi list model', function() {
+  test('it works', function(assert) {
+    let MultiListModelObject = EmberObject.extend(MultiListModelMixin);
+    let subject = MultiListModelObject.create();
+    assert.ok(subject);
+  });
 });

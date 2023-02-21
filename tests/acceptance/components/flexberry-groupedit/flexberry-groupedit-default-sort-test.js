ec65efa869f5b2fa0bbeed0fc167cf63080d4e91
TeterinaSvetlana <40786685+TeterinaSvetlana@users.noreply.github.com>
2023-02-21 09:44:36 +0500

    Flexberry groupedit default sort test (#1072)
    Co-authored-by: Sokolov Andrey <s.andrey-2011@yandex.ru>

---
+++ tests/acceptance/components/flexberry-groupedit/flexberry-groupedit-default-sort-test.js
@@ -0,0 +1,102 @@
+import Ember from 'ember';
+import { module, test } from 'qunit';
+import startApp from '../../../helpers/start-app';
+
+let app;
+const testName = 'default sort test';
+
+module('Acceptance | flexberry-groupedit | ' + testName, {
+    beforeEach() {
+
+      // Start application.
+      app = startApp();
+
+      // Enable acceptance test mode in application controller (to hide unnecessary markup from application.hbs).
+      let applicationController = app.__container__.lookup('controller:application');
+      applicationController.set('isInAcceptanceTestMode', true);
+    },
+
+    afterEach() {
+      Ember.run(app, 'destroy');
+    }
+  });
+
+test(testName, (assert) => {
+  assert.expect(9);
+  let path = 'ember-flexberry-dummy-suggestion-edit/2e98a54d-7146-4e61-bb2d-a278796c861e';
+  visit(path);
+  andThen(() => {
+    let controller = app.__container__.lookup('controller:' + currentRouteName());
+    assert.equal(controller.model.id, '2e98a54d-7146-4e61-bb2d-a278796c861e');
+
+    let currentSorting = controller.get('sorting')[0];
+    let defaultSorting = controller.developerUserSettings.suggestionUserVotesGroupEdit.DEFAULT.sorting;
+
+    let $usersVotesTable = Ember.$('.object-list-view')[1];
+
+    click($usersVotesTable.tHead.rows[0].children[1]);
+    andThen(() => {
+      currentSorting = controller.get('sorting')[0];
+      assert.ok(currentSorting.propName === 'voteType', currentSorting.direction === 'asc', 'sorting changed');
+
+      let $defaultSortingButton = Ember.$('.object-list-view .clear-sorting-button')[0];
+
+      click($defaultSortingButton);
+      andThen(() => {
+        currentSorting = controller.get('sorting')[0];
+        assert.ok(currentSorting.propName === defaultSorting[0].propName, currentSorting.direction === defaultSorting[0].direction, 'default sorting');
+        click($usersVotesTable.tHead.rows[0].children[1]);
+
+        andThen(() => {
+          currentSorting = controller.get('sorting')[0];
+          assert.ok(currentSorting.propName === 'voteType', currentSorting.direction === 'asc', 'sorting changed');
+          let $clearSettingsButton = Ember.$('.ui-clear-settings')[0];
+
+          click($clearSettingsButton);
+          andThen(() => {
+            currentSorting = controller.get('sorting')[0];
+            assert.ok(currentSorting.propName === defaultSorting[0].propName, currentSorting.direction === defaultSorting[0].direction);
+
+            var press = $.Event('click');
+            press.ctrlKey = true;
+            press.which = 17;
+            $('body').trigger(press);
+
+            andThen(() => {
+              $($usersVotesTable.tHead.rows[0].children[1]).trigger(press);
+              Ember.run.later((() => {
+                currentSorting = controller.get('sorting');
+                assert.ok(currentSorting[0].propName === 'author', currentSorting.direction === 'asc',
+                          currentSorting[1].propName === 'voteType', currentSorting.direction === 'asc');
+
+                click($defaultSortingButton);
+                andThen(() => {
+                  currentSorting = controller.get('sorting')[0];
+                  assert.ok(currentSorting.propName === defaultSorting[0].propName,
+                            currentSorting.direction === defaultSorting[0].direction);
+
+                  $('body').trigger(press);
+                  andThen(() => {
+                    $($usersVotesTable.tHead.rows[0].children[1]).trigger(press);
+                    Ember.run.later((() => {
+                      currentSorting = controller.get('sorting');
+                      assert.ok(currentSorting[0].propName === 'author', currentSorting.direction === 'asc',
+                                currentSorting[1].propName === 'voteType', currentSorting.direction === 'asc');
+
+                      click($clearSettingsButton);
+                      andThen(() => {
+                        currentSorting = controller.get('sorting')[0];
+                        assert.ok(currentSorting.propName === defaultSorting[0].propName,
+                                  currentSorting.direction === defaultSorting[0].direction);
+                      });
+                    }));
+                  });
+                });
+              }));
+            });
+          });
+        });
+      });
+    });
+  });
+});

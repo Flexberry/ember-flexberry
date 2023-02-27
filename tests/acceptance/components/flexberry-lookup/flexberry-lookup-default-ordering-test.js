c3e927ccf728c745765d9d55e4f78b51bbb22f53
Pepelyaeva Kseniya <kpepelyaeva@neoplatform.ru>
2023-02-27 13:44:34 +0500

    Fix test

--- tests/acceptance/components/flexberry-lookup/flexberry-lookup-default-ordering-test.js
+++ tests/acceptance/components/flexberry-lookup/flexberry-lookup-default-ordering-test.js
@@ -1,77 +1,56 @@
 import Ember from 'ember';
 import { executeTest } from './execute-flexberry-lookup-test';
 
-function $menuTableGetHeaders() {
-  let $menuTable = Ember.$('.content table.object-list-view');
-  let $menuTableHead = $menuTable.children('thead');
-  let $menuTableRow = $menuTableHead.children('tr');
-  return $menuTableRow.children('th');
-}
-
 executeTest('flexberry-lookup default ordering test', (store, assert, app) => {
   assert.expect(9);
-  let path = 'components-examples/flexberry-lookup/default-ordering-example';
+  const path = 'components-examples/flexberry-lookup/default-ordering-example';
   visit(path);
+
+  function checkHeaderOrder(defaultSorting = false) {
+    const $menuTableHeaders = Ember.$('.content table.object-list-view thead tr th');
+    $menuTableHeaders.each(function() {
+      const $header = $(this).children('div');
+      const $headerName = $header.attr('data-olv-header-property-name');
+      const $headerOrder = $header.children('.object-list-view-order-icon');
+      const $headerOrderTitle = $headerOrder.children('div').attr('title');
+
+      const columnIsName = $headerName === 'name';
+
+      if (columnIsName && !defaultSorting) {
+        assert.equal($headerOrder.length === 0, true, $headerName + ' has no sorting');
+        return;
+      };
+
+      assert.equal($headerOrder.length !== 0, true, $headerName + ' has sorting ' + $headerOrderTitle);
+
+      if (!columnIsName && defaultSorting) {
+        click($header);
+      }
+    });
+  }
+
   andThen(() => {
     assert.equal(currentPath(), path);
 
-    let $lookup = Ember.$('.flexberry-lookup');
-    let $lookupChooseButton = Ember.$('.ui-change', $lookup);
+    const $lookupChooseButton = Ember.$('.flexberry-lookup .ui-change');
 
     //Default sorting
     click($lookupChooseButton);
     andThen(() => {
-      $menuTableGetHeaders().each(function() {
-        let $header = $(this).children('div');
-        let $headerName = $header.attr('data-olv-header-property-name');
-        let $headerOrder = $header.children('.object-list-view-order-icon');
-        let $headerOrderTitle = $headerOrder.children('div').attr('title');
-
-        if ($headerName === 'name') {
-          assert.equal($headerOrder.length !== 0, true, $headerName + ' has sorting ' + $headerOrderTitle);
-        }else {
-          assert.equal($headerOrder.length !== 0, true, $headerName + ' has sorting ' + $headerOrderTitle);
-          click($header);
-        }
-      });
+      checkHeaderOrder(true);
     });
 
     //Changed sorting check
     andThen(() => {
-      $menuTableGetHeaders().each(function() {
-        let $header = $(this).children('div');
-        let $headerName = $header.attr('data-olv-header-property-name');
-        let $headerOrder = $header.children('.object-list-view-order-icon');
-        let $headerOrderTitle = $headerOrder.children('div').attr('title');
-
-        if ($headerName === 'name') {
-          assert.equal($headerOrder.length === 0, true, $headerName + ' has no sorting');
-        }else {
-          assert.equal($headerOrder.length !== 0, true, $headerName + ' has sorting ' + $headerOrderTitle);
-        }
-      });
-
-      let $menuPageButtons = Ember.$('.ui.basic.buttons');
-      let $menuNextPageButton = $menuPageButtons.children('.next-page-button');
+      checkHeaderOrder();
+      const $menuNextPageButton = Ember.$('.ui.basic.buttons').children('.next-page-button');
       click($menuNextPageButton);
     });
 
     //Switch page check
     andThen(() => {
-      $menuTableGetHeaders().each(function() {
-        let $header = $(this).children('div');
-        let $headerName = $header.attr('data-olv-header-property-name');
-        let $headerOrder = $header.children('.object-list-view-order-icon');
-        let $headerOrderTitle = $headerOrder.children('div').attr('title');
-
-        if ($headerName === 'name') {
-          assert.equal($headerOrder.length === 0, true, $headerName + ' has no sorting');
-        }else {
-          assert.equal($headerOrder.length !== 0, true, $headerName + ' has sorting ' + $headerOrderTitle);
-        }
-      });
-
-      let $menuCloseButton = Ember.$('.close.icon');
+      checkHeaderOrder();
+      const $menuCloseButton = Ember.$('.close.icon');
       click($menuCloseButton);
     });
 
@@ -81,21 +60,8 @@ executeTest('flexberry-lookup default ordering test', (store, assert, app) => {
 
     //Close&open page check
     andThen(() => {
-      $menuTableGetHeaders().each(function() {
-        let $header = $(this).children('div');
-        let $headerName = $header.attr('data-olv-header-property-name');
-        let $headerOrder = $header.children('.object-list-view-order-icon');
-        let $headerOrderTitle = $headerOrder.children('div').attr('title');
-
-        if ($headerName === 'name') {
-          assert.equal($headerOrder.length === 0, true, $headerName + ' has no sorting');
-        }else {
-          assert.equal($headerOrder.length !== 0, true, $headerName + ' has sorting ' + $headerOrderTitle);
-        }
-      });
-
-      let $secondaryMenu = Ember.$('.ui.secondary.menu.no-margin.ember-view');
-      let $defaultSortingButton = $secondaryMenu.children('.ui.button');
+      checkHeaderOrder();
+      const $defaultSortingButton = Ember.$('.ui.button.default-sort');
       click($defaultSortingButton);
     });
   });

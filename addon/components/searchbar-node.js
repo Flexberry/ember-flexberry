77b63c41aba885da015ab7bb8b9701219f594d69
TeterinaSvetlana <40786685+TeterinaSvetlana@users.noreply.github.com>
2023-07-13 16:05:50 +0500

    Fix destroyed object error

--- addon/components/searchbar-node.js
+++ addon/components/searchbar-node.js
@@ -1,5 +1,9 @@
 import Component from "@ember/component";
 
-export default Component.extend({
-
+export default Component.extend({  
+  click(e) {
+    if (!e.target.href) {
+      e.stopPropagation();
+    }
+  }
 });

73c1387c27f43ad174a945ed4f685a45672daa2c
Valentin Churekov <VChurekov@neoplatform.ru>
2023-07-14 12:21:20 +0500

    fix: ember application import.

--- tests/dummy/app/initializers/odata-adapter-custom.js
+++ tests/dummy/app/initializers/odata-adapter-custom.js
@@ -1,10 +1,11 @@
 import { isNone } from '@ember/utils';
+import Application from '@ember/application';
 import ODataAdapter from 'ember-flexberry-data/query/odata-adapter';
 import getSerializedDateValue from 'ember-flexberry-data/utils/get-serialized-date-value';
 
 export function initialize() {
   ODataAdapter.prototype._processConstForODataSimplePredicateByType = function (predicate, predicateValue, valueType) {
-    let appContainer = Ember.Application.NAMESPACES.find(x => x.backendUrl && x.__container__).__container__;
+    let appContainer = Application.NAMESPACES.find(x => x.backendUrl && x.__container__).__container__;
     let transformInstance = appContainer.lookup('transform:' + valueType);
     let transformClass = !isNone(transformInstance) ? transformInstance.constructor : null;
 

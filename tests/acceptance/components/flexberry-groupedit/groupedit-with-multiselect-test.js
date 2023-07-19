447c90b10ac89c185eb4d2ed2b65df352a486e71
TeterinaSvetlana <40786685+TeterinaSvetlana@users.noreply.github.com>
2023-07-19 10:03:15 +0500

    fix

--- tests/acceptance/components/flexberry-groupedit/groupedit-with-multiselect-test.js
+++ tests/acceptance/components/flexberry-groupedit/groupedit-with-multiselect-test.js
@@ -30,52 +30,50 @@ test(testName, (assert) => {
 
     let $olv = $('.object-list-view ');
     let $tbody = $('td.field', $olv)[0];
-    run(() => {
-      let done = assert.async();
-      click($tbody);
+    let done = assert.async();
+    run(() => click($tbody));
+    wait().then(() => {
+      const children = $('tbody', $('div.groupedit-container')[1])[0];
+      let startChildrenCount;
+
+      if (children.children[0].cells.length == 1) {
+        startChildrenCount = 0;
+      } else {            
+        startChildrenCount = children.childElementCount;
+      }
+      let $addButton = $('button.ui.ui-add.button')[1];
+
+      if (startChildrenCount == 0) {
+        run(() => {
+          click($addButton);
+        });
+      }
+
       wait().then(() => {
-        const children = $('tbody', $('div.groupedit-container')[1])[0];
-        let startChildrenCount;
-
-        if (children.children[0].cells.length == 1) {
-          startChildrenCount = 0;
-        } else {            
-          startChildrenCount = children.childElementCount;
-        }
-        let $addButton = $('button.ui.ui-add.button')[1];
-
-        if (startChildrenCount == 0) {
-          run(() => {
-            click($addButton);
-          });
-        }
-
+        let $lookupButton = $('button.ui.ui-change.button')[2];
+        run(() => {
+          click($lookupButton);
+        });
         wait().then(() => {
-          let $lookupButton = $('button.ui.ui-change.button')[2];
-          run(() => {
-            click($lookupButton);
-          });
-          wait().then(() => {
-            let $modal = $('div.flexberry-modal')[0];
-            let $checkbox1 = $('div.flexberry-checkbox.ui', $modal)[0];
-            let $checkbox2 = $('div.flexberry-checkbox.ui', $modal)[1];
+          let $modal = $('div.flexberry-modal')[0];
+          let $checkbox1 = $('div.flexberry-checkbox.ui', $modal)[0];
+          let $checkbox2 = $('div.flexberry-checkbox.ui', $modal)[1];
 
-            click($checkbox1);
-            click($checkbox2);
+          click($checkbox1);
+          click($checkbox2);
+          wait().then(() => {
+            $modal = $('div.flexberry-modal')[0];
+            let $addRecords = $('button.ui.button', $modal)[0];
+            run(() => {
+              click($addRecords);
+            });
             wait().then(() => {
-              $modal = $('div.flexberry-modal')[0];
-              let $addRecords = $('button.ui.button', $modal)[0];
-              run(() => {
-                click($addRecords);
-              });
+              assert.equal(children.childElementCount, startChildrenCount + 2, 'All records are added');
+              
               wait().then(() => {
-                assert.equal(children.childElementCount, startChildrenCount + 2, 'All records are added');
-                
-                wait().then(() => {
-                  run(app, 'destroy');
-                });
-                done();
+                run(app, 'destroy');
               });
+              done();
             });
           });
         });

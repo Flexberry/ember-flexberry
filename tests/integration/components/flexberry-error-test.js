f03cdff062da9c78b0c1b41faf3ea0f6ab5927d4
Bobrikov Mikhail <mihapir4t@yandex.ru>
2024-07-18 10:23:22 +0500

    fix

--- tests/integration/components/flexberry-error-test.js
+++ tests/integration/components/flexberry-error-test.js
@@ -5,14 +5,9 @@ import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | flexberry-error', function(hooks) {
   setupRenderingTest(hooks);
-
   test('Component renders properly', async function(assert) {
     this.set('error', new Error('Error, error, error...'));
-    
-    await render(hbs`
-      {{flexberry-error error=error modalContext='body'}}
-    `);
+    await render(hbs`{{flexberry-error error=error modalContext='body'}}`);
     assert.ok(/Error, error, error.../.test(this.element.textContent), 'Error message is displayed correctly');
-    ""
   });
 });

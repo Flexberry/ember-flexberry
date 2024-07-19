036b2333484101448aea733af7cd42aee3b654f9
Bobrikov Mikhail <mihapir4t@yandex.ru>
2024-07-19 14:57:55 +0500

    fix

--- tests/integration/components/groupedit-toolbar-test.js
+++ tests/integration/components/groupedit-toolbar-test.js
@@ -3,30 +3,28 @@ import hbs from 'htmlbars-inline-precompile';
 import { render } from '@ember/test-helpers';
 import { setupRenderingTest } from 'ember-qunit';
 
-
 module('Integration | Component | groupedit toolbar', function(hooks) {
   setupRenderingTest(hooks);
 
+  test('it renders', async function(assert) {
+    assert.expect(2);
 
-test('it renders',async function(assert) {
-  assert.expect(2);
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.on('myAction', function(val) { ... });
 
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+    await render(hbs`{{groupedit-toolbar componentName = "someName"}}`);
 
-  await render(hbs`{{groupedit-toolbar componentName = "someName"}}`);
+    assert.equal(this.$().text().trim(), '');
 
-  assert.equal(this.$().text().trim(), '');
+    // Template block usage:
+    await render(hbs`
+     {{#groupedit-toolbar componentName = "someName"}}
+     template block text
+     {{/groupedit-toolbar}}
+   `);
 
-  // Template block usage:
-  await render(hbs`
-    {{#groupedit-toolbar componentName = "someName"}}
-      template block text
-    {{/groupedit-toolbar}}
-  `);
-
-  //Component does not support template block usage.
-  assert.equal(this.$().text().trim(), '');
-});
+    //Component does not support template block usage.
+    assert.equal(this.$().text().trim(), '');
+  });
 });
 

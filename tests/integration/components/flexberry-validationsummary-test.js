da76c134512d6ad1add245b5e8a70d9f5d4f3b2c
Bobrikov Mikhail <mihapir4t@yandex.ru>
2024-07-19 15:04:15 +0500

    fix

--- tests/integration/components/flexberry-validationsummary-test.js
+++ tests/integration/components/flexberry-validationsummary-test.js
@@ -8,29 +8,28 @@ import { render } from '@ember/test-helpers';
 module( 'Integration | Component | flexberry-validationsummary', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders and works',async function(assert) {
-    
+  test('it renders and works', async function(assert) {
     await render(hbs`{{flexberry-validationsummary errors=errors color=color header=header}}`);
 
-  let errors = this.set('errors', A());
-  assert.ok(this.$('.ui.message').is(':hidden'), 'Component is hidden if no errors.');
+    let errors = this.set('errors', A());
+    assert.ok(this.$('.ui.message').is(':hidden'), 'Component is hidden if no errors.');
 
-  run(() => {
-    errors.pushObject('Validation error.');
-  });
-  assert.ok(this.$('.ui.message').is(':visible'), 'Component is visible if there errors.');
-  assert.ok(this.$().text().trim(), 'Validation error.', 'Component shows errors at added.');
+    run(() => {
+      errors.pushObject('Validation error.');
+    });
+    assert.ok(this.$('.ui.message').is(':visible'), 'Component is visible if there errors.');
+    assert.ok(this.$().text().trim(), 'Validation error.', 'Component shows errors at added.');
 
-  this.set('header', 'Validation errors');
-  assert.ok(/Validation errors\s*/.test(this.$().text().trim()), 'Component has a header.');
+    this.set('header', 'Validation errors');
+    assert.ok(/Validation errors\s*/.test(this.$().text().trim()), 'Component has a header.');
 
-  assert.notOk(this.$('.ui.label').hasClass('red'), 'Override default color with undefined value.');
+    assert.notOk(this.$('.ui.label').hasClass('red'), 'Override default color with undefined value.');
 
-  this.set('color', 'blue');
-  assert.ok(this.$('.ui.message').hasClass('blue'), 'Color works through CSS class.');
+    this.set('color', 'blue');
+    assert.ok(this.$('.ui.message').hasClass('blue'), 'Color works through CSS class.');
 
-  await render(hbs`{{flexberry-validationsummary}}`);
-  assert.ok(this.$('.ui.message').hasClass('red'), `Default color 'red'.`);
+    await render(hbs`{{flexberry-validationsummary}}`);
+    assert.ok(this.$('.ui.message').hasClass('red'), `Default color 'red'.`);
   });
 });
 

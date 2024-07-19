f866934aae0253d72441704fc568ed84daf0aaa3
Bobrikov Mikhail <mihapir4t@yandex.ru>
2024-07-19 10:42:34 +0500

    modal-dialog test

--- tests/integration/components/modal-dialog-test.js
+++ tests/integration/components/modal-dialog-test.js
@@ -1,50 +1,48 @@
-import { moduleForComponent, test } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
-import wait from 'ember-test-helpers/wait';
 import { registerWaiter } from '@ember/test';
 
-moduleForComponent('modal-dialog', 'Integration | Component | modal dialog', {
-  integration: true,
+module('Integration | Component | modal-dialog', function(hooks) {
+  setupRenderingTest(hooks);
 
-  beforeEach() {
-    // detachable need for jquery can do select child components
-    this.set('settings', {
-      detachable: false
-    });
-
+  hooks.beforeEach(function() {
+    this.set('settings', { detachable: false });
     this.set('created', false);
     this.set('createdConsumer', () => {
       this.set('created', true);
     });
-
     registerWaiter(this, () => this.get('created'));
-  },
+  });
 
-  afterEach() {
-    this.$('.flexberry-modal').modal('hide dimmer');
-  }
-});
+  hooks.afterEach(function() {
+    // This might cause an error if the modal is not present
+    try {
+      this.element.querySelector('.flexberry-modal').classList.remove('dimmer'); // Assuming you have such class
+    } catch (e) {
+      // Handle the error or ignore if it is expected
+    }
+  });
 
-test('it renders', function (assert) {
-  this.render(hbs`
-    {{#modal-dialog settings=settings created=createdConsumer}}
-      template block text
-    {{/modal-dialog}}
-  `);
+  test('it renders', async function(assert) {
+    await render(hbs`
+      {{#modal-dialog settings=settings created=createdConsumer}}
+        template block text
+      {{/modal-dialog}}
+    `);
 
-  return wait().then(() => {
-    assert.equal(this.$('.content').text().trim(), 'template block text');
+    assert.equal(this.element.querySelector('.content').textContent.trim(), 'template block text');
+  });
+
+  test('it should not show actions div if no buttons visible', async function(assert) {
+    await render(hbs`
+      {{#modal-dialog settings=settings created=createdConsumer useOkButton=false useCloseButton=false}}
+        template block text
+      {{/modal-dialog}}
+    `);
+
+    assert.equal(this.element.querySelectorAll('.actions').length, 0);
   });
 });
 
-test('it should not show actions div if no buttons visible', function(assert) {
-  this.render(hbs`
-    {{#modal-dialog settings=settings created=createdConsumer useOkButton=false useCloseButton=false}}
-      template block text
-    {{/modal-dialog}}
-  `);
-
-  return wait().then(() => {
-    assert.equal(this.$('.actions').length, 0);
-  });
-});

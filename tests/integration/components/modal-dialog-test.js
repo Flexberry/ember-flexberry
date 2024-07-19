e873596128e1faf8eb6b1e0fb742cc04cb120c41
Bobrikov Mikhail <mihapir4t@yandex.ru>
2024-07-19 13:55:23 +0500

    fix

--- tests/integration/components/modal-dialog-test.js
+++ tests/integration/components/modal-dialog-test.js
@@ -3,6 +3,7 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 import { registerWaiter } from '@ember/test';
+import wait from 'ember-test-helpers/wait';
 
 module('Integration | Component | modal-dialog', function(hooks) {
   setupRenderingTest(hooks);
@@ -17,38 +18,30 @@ module('Integration | Component | modal-dialog', function(hooks) {
   });
 
   hooks.afterEach(function() {
-    if (this.element.querySelector('.flexberry-modal')) {
-      this.element.querySelector('.flexberry-modal').classList.remove('modal', 'hide', 'dimmer');
-    }
+    // This might cause an error if the modal is not present
+    this.$('.flexberry-modal').modal('hide dimmer');
   });
 
-  test('it renders', function(assert) {
-    this.render(hbs`
+  test('it renders', async function(assert) {
+    await render(hbs`
       {{#modal-dialog settings=settings created=createdConsumer}}
         template block text
       {{/modal-dialog}}
     `);
 
-    return new Promise(resolve => {
-      setTimeout(() => {
-        assert.equal(this.element.querySelector('.content').textContent.trim(), 'template block text');
-        resolve();
-      }, 0);
-    });
+    assert.equal(this.element.querySelector('.content').textContent.trim(), 'template block text');
   });
 
-  test('it should not show actions div if no buttons visible', function(assert) {
-    this.render(hbs`
+  test('it should not show actions div if no buttons visible', async function(assert) {
+    await render(hbs`
       {{#modal-dialog settings=settings created=createdConsumer useOkButton=false useCloseButton=false}}
         template block text
       {{/modal-dialog}}
     `);
 
-    return new Promise(resolve => {
-      setTimeout(() => {
-        assert.equal(this.element.querySelectorAll('.actions').length, 0);
-        resolve();
-      }, 0);
+    return wait().then(() => {
+      assert.equal(this.$('.actions').length, 0);
     });
   });
 });
+

ad5037a5135c60e197d2a49e86360bc7ac752938
Ruslan Bannikov <bannikov.ru@inbox.ru>
2024-07-22 12:56:44 +0500

    2 of 6 flexberry-groupedit acceptance tests

--- tests/acceptance/components/flexberry-groupedit/flexberry-groupedit-check-all-at-page-test.js
+++ tests/acceptance/components/flexberry-groupedit/flexberry-groupedit-check-all-at-page-test.js
@@ -2,61 +2,57 @@ import $ from 'jquery';
 import { run } from '@ember/runloop';
 import { module, test } from 'qunit';
 import { loadingList } from '../flexberry-objectlistview/folv-tests-functions';
+import { setupApplicationTest } from 'ember-qunit';
+import { set } from '@ember/object';
+import { visit, currentURL } from '@ember/test-helpers';
 import startApp from '../../../helpers/start-app';
 
 let app;
 const path = 'components-examples/flexberry-groupedit/configurate-row-example';
 const testName = 'check all at page';
-var olvContainerClass = '.object-list-view-container';
-var trTableClass = 'table.object-list-view tbody tr';
+const olvContainerClass = '.object-list-view-container';
+const trTableClass = 'table.object-list-view tbody tr';
 
-module('Acceptance | flexberry-groupedit | ' + testName, {
-  beforeEach() {
+module('Acceptance | flexberry-groupedit | ' + testName, function(hooks) {
+  setupApplicationTest(hooks);
 
+  hooks.beforeEach(function() {
     // Start application.
     app = startApp();
 
     // Enable acceptance test mode in application controller (to hide unnecessary markup from application.hbs).
     let applicationController = app.__container__.lookup('controller:application');
-    applicationController.set('isInAcceptanceTestMode', true);
-  },
+    set(applicationController, 'isInAcceptanceTestMode', true);
+  });
 
-  afterEach() {
+  hooks.afterEach(function() {
     run(app, 'destroy');
-  }
-});
+  });
 
-test(testName, (assert) => {
-  assert.expect(4);
+  test(testName, async function(assert) {
+    assert.expect(4);
 
-  visit(path);
+    await visit(path);
+    assert.equal(currentURL(), path, 'Path is correctly');
 
-  andThen(() => {
-    assert.equal(currentPath(), path);
+    const $olv = $('.object-list-view');
+    const $thead = $('th.dt-head-left', $olv).first();
 
-    let $olv = $('.object-list-view ');
-    let $thead = $('th.dt-head-left', $olv)[0];
+    await run(async () => {
+      const $list = await loadingList($thead, olvContainerClass, trTableClass);
 
-    run(() => {
-      let done = assert.async();
-      loadingList($thead, olvContainerClass, trTableClass).then(($list) => {
-        assert.ok($list);
-        let $rows = $('.object-list-view-helper-column', $list);
+      assert.ok($list);
+      const $rows = $('.object-list-view-helper-column', $list);
 
-        click('.ui.check-all-at-page-button');
-        andThen(() => {
-          let $checkCheckBox = $('.flexberry-checkbox.checked', $rows);
-          assert.equal($checkCheckBox.length, $rows.length, 'All checkBox in row are select');
+      await click('.ui.check-all-at-page-button');
+      let $checkCheckBox = $('.flexberry-checkbox.checked', $rows);
+      assert.equal($checkCheckBox.length, $rows.length, 'All checkBox in row are select');
 
-          click('.ui.check-all-at-page-button');
-          andThen(() => {
-            $checkCheckBox = $('.flexberry-checkbox.checked', $rows);
-            assert.equal($checkCheckBox.length, 0, 'All checkBox in row are unselect');
-          });
-        });
-
-        done();
-      });
+      await click('.ui.check-all-at-page-button');
+      $checkCheckBox = $('.flexberry-checkbox.checked', $rows);
+      assert.equal($checkCheckBox.length, 0, 'All checkBox in row are unselect');
     });
   });
 });
+
+

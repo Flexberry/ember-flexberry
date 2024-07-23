43b4aa06b831c8b47b7d7900915c7cfd10deff3d
Kseniya Pepelyaeva <C:\Users\kpepelyaeva\AppData\Roaming\The Bat!>
2024-07-23 13:02:39 +0500

    Update modal dialog integration test

--- tests/integration/components/modal-dialog-test.js
+++ tests/integration/components/modal-dialog-test.js
@@ -1,50 +1,47 @@
-import { moduleForComponent, test } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 import wait from 'ember-test-helpers/wait';
 import { registerWaiter } from '@ember/test';
 
-moduleForComponent('modal-dialog', 'Integration | Component | modal dialog', {
-  integration: true,
+module('Integration | Component | modal-dialog', function(hooks) {
+  setupRenderingTest(hooks);
 
-  beforeEach() {
-    // detachable need for jquery can do select child components
-    this.set('settings', {
-      detachable: false
-    });
-
+  hooks.beforeEach(function() {
+    this.set('settings', { detachable: false });
     this.set('created', false);
     this.set('createdConsumer', () => {
       this.set('created', true);
     });
-
     registerWaiter(this, () => this.get('created'));
-  },
-
-  afterEach() {
-    this.$('.flexberry-modal').modal('hide dimmer');
-  }
-});
-
-test('it renders', function (assert) {
-  this.render(hbs`
-    {{#modal-dialog settings=settings created=createdConsumer}}
-      template block text
-    {{/modal-dialog}}
-  `);
-
-  return wait().then(() => {
-    assert.equal(this.$('.content').text().trim(), 'template block text');
   });
-});
 
-test('it should not show actions div if no buttons visible', function(assert) {
-  this.render(hbs`
-    {{#modal-dialog settings=settings created=createdConsumer useOkButton=false useCloseButton=false}}
-      template block text
-    {{/modal-dialog}}
-  `);
+  hooks.afterEach(function() {
+    this.$('.flexberry-modal').modal('hide dimmer');
+  });
 
-  return wait().then(() => {
-    assert.equal(this.$('.actions').length, 0);
+  test('it renders', async function(assert) {
+    await render(hbs`
+      {{#modal-dialog settings=settings created=createdConsumer}}
+        template block text
+      {{/modal-dialog}}
+    `);
+
+    return wait().then(() => {
+      assert.equal(this.$('.content').text().trim(), 'template block text');
+    });
+  });
+
+  test('it should not show actions div if no buttons visible', async function(assert) {
+    await render(hbs`
+      {{#modal-dialog settings=settings created=createdConsumer useOkButton=false useCloseButton=false}}
+        template block text
+      {{/modal-dialog}}
+    `);
+
+    return wait().then(() => {
+      assert.equal(this.$('.actions').length, 0);
+    });
   });
 });

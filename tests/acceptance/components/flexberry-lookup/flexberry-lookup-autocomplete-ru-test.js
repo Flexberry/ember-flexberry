98accac24f3d7636a9f8da886fde98fc4929f89c
Svetlana <steterina@neoplatform.ru>
2024-07-24 12:32:54 +0500

    autocomplete tests

--- tests/acceptance/components/flexberry-lookup/flexberry-lookup-autocomplete-ru-test.js
+++ tests/acceptance/components/flexberry-lookup/flexberry-lookup-autocomplete-ru-test.js
@@ -1,35 +1,31 @@
-import $ from 'jquery';
-import { run } from '@ember/runloop';
+import { fillIn, settled } from '@ember/test-helpers';
 import { executeTest } from './execute-flexberry-lookup-test';
 import { loadingLocales } from './lookup-test-functions';
-import { fillIn } from '@ember/test-helpers';
+import $ from 'jquery';
 
-executeTest('flexberry-lookup autocomplete message ru', (store, assert, app) => {
+executeTest('flexberry-lookup autocomplete message ru', async (store, assert, app) => {
   assert.expect(4);
-  let path = 'components-acceptance-tests/flexberry-lookup/settings-example-autocomplete';
-  visit(path);
+  const path = 'components-acceptance-tests/flexberry-lookup/settings-example-autocomplete';
+  
+  await visit(path);
+  assert.equal(currentPath(), path, 'The current URL is correct');
 
-  andThen(function() {
-    assert.equal(currentPath(), path);
+  await loadingLocales('ru', app);
+  
+  const textbox = $('.ember-text-field')[0];
+  await fillIn(textbox, 'gfhfkjglkhlh');
 
-    loadingLocales('ru', app).then(() => {
-      let textbox = $('.ember-text-field')[0];
-      fillIn(textbox, 'gfhfkjglkhlh');
-    });
+  // Wait for the async operations to complete
+  await settled();
 
-    let asyncOperationsCompleted = assert.async();
-    run.later(function() {
-      asyncOperationsCompleted();
+  await new Promise(resolve => setTimeout(resolve, 6000));
 
-      let $message = $('.message');
-      assert.strictEqual($message.hasClass('empty'), true, 'Component\'s wrapper has message');
+  const $message = $('.message');
+  assert.ok($message.hasClass('empty'), 'Component\'s wrapper has message');
 
-      let $messageHeader = $message.children('.header');
-      assert.equal($messageHeader.text(), 'Нет данных', 'Message\'s header is properly');
+  const $messageHeader = $message.children('.header');
+  assert.equal($messageHeader.text(), 'Нет данных', 'Message\'s header is properly');
 
-      let $messageDescription = $message.children('.description');
-      assert.equal($messageDescription.text(), 'Значения не найдены', 'Message\'s description is properly');
-
-    }, 5000);
-  });
+  const $messageDescription = $message.children('.description');
+  assert.equal($messageDescription.text(), 'Значения не найдены', 'Message\'s description is properly');
 });

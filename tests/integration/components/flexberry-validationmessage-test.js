d59f81bd965a944a65dd94069262f777c95227ce
Bobrikov Mikhail <mihapir4t@yandex.ru>
2024-07-30 09:24:20 +0500

    fix integration validationmessage  test

--- tests/integration/components/flexberry-validationmessage-test.js
+++ tests/integration/components/flexberry-validationmessage-test.js
@@ -11,26 +11,26 @@ module('flexberry-validationmessage', 'Integration | Component | flexberry-valid
 
     [undefined, null, '', []].forEach((error) => {
       this.set('error', error);
-      assert.ok(this.$('.ui.label').is(':hidden'), 'Component is hidden if no error.');
+      assert.ok($('.ui.label', this.element).is(':hidden'), 'Component is hidden if no error.');
     });
 
     this.set('error', 'This is error.');
-    assert.ok(this.$('.ui.label').is(':visible'), 'Component is visible if there errors.');
-    assert.equal(this.$().text().trim(), 'This is error.', 'Component shows error.');
+    assert.ok($('.ui.label', this.element).is(':visible'), 'Component is visible if there errors.');
+    assert.equal($(this.element).text().trim(), 'This is error.', 'Component shows error.');
 
     this.set('error', ['First error.', 'Second error.']);
-    assert.equal(this.$().text().trim(), 'First error.,Second error.', 'Component shows all errors.');
+    assert.equal($(this.element).text().trim(), 'First error.,Second error.', 'Component shows all errors.');
 
-    assert.notOk(this.$('.ui.label').hasClass('red'), 'Override default color with undefined value.');
-    assert.notOk(this.$('.ui.label').hasClass('pointing'), 'Override default pointing with undefined value.');
+    assert.notOk($(this.element, '.ui.label').hasClass('red'), 'Override default color with undefined value.');
+    assert.notOk($(this.element, '.ui.label').hasClass('pointing'), 'Override default pointing with undefined value.');
 
     this.set('color', 'pink');
     this.set('pointing', 'left pointing');
-    assert.ok(this.$('.ui.label').hasClass('pink'), 'Color works through CSS class.');
-    assert.ok(this.$('.ui.label').hasClass('left'), 'Pointing works through CSS class.');
+    assert.ok($('.ui.label', this.element).hasClass('pink'), 'Color works through CSS class.');
+    assert.ok($('.ui.label', this.element).hasClass('left'), 'Pointing works through CSS class.');
 
     await  render(hbs`{{flexberry-validationmessage}}`);
-    assert.ok(this.$('.ui.label').hasClass('red'), `Default color 'red'.`);
-    assert.ok(this.$('.ui.label').hasClass('pointing'), `Default pointing 'pointing'.`);
+    assert.ok($('.ui.label', this.element).hasClass('red'), `Default color 'red'.`);
+    assert.ok($('.ui.label', this.element).hasClass('pointing'), `Default pointing 'pointing'.`);
   });
 });

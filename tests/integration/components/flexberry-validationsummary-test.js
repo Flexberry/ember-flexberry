4a06172c427b52487a08ae2efd19f8cc715bd123
Bobrikov Mikhail <mihapir4t@yandex.ru>
2024-07-30 09:30:24 +0500

    fix integration validationsummary  test

--- tests/integration/components/flexberry-validationsummary-test.js
+++ tests/integration/components/flexberry-validationsummary-test.js
@@ -12,21 +12,21 @@ module( 'Integration | Component | flexberry-validationsummary', function(hooks)
     await render(hbs`{{flexberry-validationsummary errors=errors color=color header=header}}`);
 
     let errors = this.set('errors', A());
-    assert.ok(this.$('.ui.message').is(':hidden'), 'Component is hidden if no errors.');
+    assert.ok($('.ui.message', this.element).is(':hidden'), 'Component is hidden if no errors.');
 
     run(() => {
       errors.pushObject('Validation error.');
     });
-    assert.ok(this.$('.ui.message').is(':visible'), 'Component is visible if there errors.');
-    assert.ok(this.$().text().trim(), 'Validation error.', 'Component shows errors at added.');
+    assert.ok($('.ui.message',  this.element).is(':visible'), 'Component is visible if there errors.');
+    assert.ok($(this.element).text().trim(), 'Validation error.', 'Component shows errors at added.');
 
     this.set('header', 'Validation errors');
-    assert.ok(/Validation errors\s*/.test(this.$().text().trim()), 'Component has a header.');
+    assert.ok(/Validation errors\s*/.test($(this.element).text().trim()), 'Component has a header.');
 
-    assert.notOk(this.$('.ui.label').hasClass('red'), 'Override default color with undefined value.');
+    assert.notOk($(this.element, '.ui.label').hasClass('red'), 'Override default color with undefined value.');
 
     this.set('color', 'blue');
-    assert.ok(this.$('.ui.message').hasClass('blue'), 'Color works through CSS class.');
+    assert.ok($('.ui.message', this.element).hasClass('blue'), 'Color works through CSS class.');
 
     await render(hbs`{{flexberry-validationsummary}}`);
     assert.ok(this.$('.ui.message').hasClass('red'), `Default color 'red'.`);

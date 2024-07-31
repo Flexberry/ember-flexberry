e4cc3682cde2508cbeef4971a1d9b46cc9052577
Bobrikov Mikhail <mihapir4t@yandex.ru>
2024-07-31 15:25:23 +0500

    update toolbar-custom-components-test

--- tests/acceptance/components/flexberry-objectlistview/folv-toolbar-custom-components-test.js
+++ tests/acceptance/components/flexberry-objectlistview/folv-toolbar-custom-components-test.js
@@ -1,50 +1,46 @@
 import $ from 'jquery';
 import { executeTest } from './execute-folv-test';
 import { run } from '@ember/runloop';
-import wait from 'ember-test-helpers/wait';
+import {settled, waitFor } from '@ember/test-helpers';
 
-executeTest('check toolbar custom components', (store, assert) => {
+executeTest('check toolbar custom components', async (store, assert) => {
   assert.expect(8);
   const path = 'components-examples/flexberry-objectlistview/toolbar-custom-components-example';
 
-  visit(path);
+    await visit(path);
 
-  andThen(() => {
-    assert.equal(currentPath(), path, 'Path is correct');
+  assert.equal(currentPath(), path, 'Path is correct');
 
-    let $toolbar = $('.flexberry-olv-toolbar');
-    let $dropdown = $toolbar.children('.flexberry-dropdown');
-    let $text = $dropdown.children('.text');
-    let $menu = $dropdown.children('div.menu');
-    let $items = $menu.children();
+  let $toolbar = $('.flexberry-olv-toolbar');
+  let $dropdown = $toolbar.children('.flexberry-dropdown');
+ 
+  let $text = $dropdown.children('.text');
+  let $menu = $dropdown.children('div.menu');
+  let $items = $menu.children();
 
-    assert.equal($dropdown.length, 1, 'Dropdown is render');
-    assert.equal($text[0].innerText, 'Enum value №1', 'Dropdown is render');
+  assert.equal($dropdown.length, 1, 'Dropdown is render');
+  assert.equal($text[0].innerText, 'Enum value №1', 'Dropdown is render');
 
-    andThen(() => {
-      run(() => $dropdown.click());
+  await click($dropdown);
+  await settled();
 
-      assert.equal($items[0].innerText, 'Enum value №1', 'Dropdown list menu is rendered');
-      assert.equal($($items[0]).hasClass('active selected'), true, 'Selected dropdown list item has the css-class \'active\'');
+  assert.equal($items[0].innerText, 'Enum value №1', 'Dropdown list menu is rendered');
+  assert.equal($($items[0]).hasClass('active selected'), true, 'Selected dropdown list item has the css-class \'active\'');
 
-      run(() => $($items[5]).click());
+  await click($($items[5]));
 
-      wait().then(() => {
-        $dropdown = $toolbar.children('.flexberry-dropdown');
-        $text = $dropdown.children('.text');
+  $dropdown = $toolbar.children('.flexberry-dropdown');
+  $text = $dropdown.children('.text');
 
-        assert.equal($text[0].innerText, 'Enum value №6', 'Text in the dropdown list has changed');
+  assert.equal($text[0].innerText, 'Enum value №6', 'Text in the dropdown list has changed');
       
-        run(() => $dropdown.click());
+  await click($dropdown);
+  await settled();
 
-        wait().then(() => {
-          $menu = $dropdown.children('div.menu');
-          $items = $menu.children();
+  $menu = $dropdown.children('div.menu');
+  $items = $menu.children();
 
-          assert.equal($items[5].innerText, 'Enum value №6', 'Dropdown list menu is rendered');
-          assert.equal($($items[5]).hasClass('active selected'), true, 'Selected dropdown list item has the css-class \'active\'');
-        });
-      });
-    });
-  });
+  assert.equal($items[5].innerText, 'Enum value №6', 'Dropdown list menu is rendered');
+  assert.equal($($items[5]).hasClass('active selected'), true, 'Selected dropdown list item has the css-class \'active\'');
 });
+

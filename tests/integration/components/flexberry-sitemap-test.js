79d5306e1d9946222ebfcd8a9f6654b1b9701fda
Bobrikov Mikhail <mihapir4t@yandex.ru>
2024-08-04 22:21:46 +0500

    fix

--- tests/integration/components/flexberry-sitemap-test.js
+++ tests/integration/components/flexberry-sitemap-test.js
@@ -2,6 +2,7 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+import $ from 'jquery';
 
 module('flexberry-sitemap', 'Integration | Component | flexberry-sitemap', function(hooks) {
  setupRenderingTest(hooks);

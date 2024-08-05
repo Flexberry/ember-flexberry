aed0d6df4e3a3d80d2cf84d74831dfb2759a435f
BobrikovMikhail <115791173+BobrikovMikhail@users.noreply.github.com>
2024-08-05 15:18:35 +0500

    Update validationmessage test/Integration (#1604)

--- tests/integration/components/flexberry-validationmessage-test.js
+++ tests/integration/components/flexberry-validationmessage-test.js
@@ -2,8 +2,9 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+import $ from 'jquery';
 
-module('flexberry-validationmessage', 'Integration | Component | flexberry-validationmessage', function(hooks) {
+module('Integration | Component | flexberry-validationmessage', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders and works', async function (assert) {
@@ -11,26 +12,26 @@ module('flexberry-validationmessage', 'Integration | Component | flexberry-valid
 
     [undefined, null, '', []].forEach((error) => {
       this.set('error', error);
-      assert.ok(this.$('.ui.label').is(':hidden'), 'Component is hidden if no error.');
+      assert.ok($('.ui.label', this.element).is(':hidden'), 'Component is hidden if no error.');
     });
 
     this.set('error', 'This is error.');
-    assert.ok(this.$('.ui.label').is(':visible'), 'Component is visible if there errors.');
-    assert.equal(this.$().text().trim(), 'This is error.', 'Component shows error.');
+    assert.ok($('.ui.label', this.element).is(':visible'), 'Component is visible if there errors.');
+    assert.equal($(this.element).text().trim(), 'This is error.', 'Component shows error.');
 
     this.set('error', ['First error.', 'Second error.']);
-    assert.equal(this.$().text().trim(), 'First error.,Second error.', 'Component shows all errors.');
+    assert.equal($(this.element).text().trim(), 'First error.,Second error.', 'Component shows all errors.');
 
-    assert.notOk(this.$('.ui.label').hasClass('red'), 'Override default color with undefined value.');
-    assert.notOk(this.$('.ui.label').hasClass('pointing'), 'Override default pointing with undefined value.');
+    assert.notOk($('.ui.label', this.element).hasClass('red'), 'Override default color with undefined value.');
+    assert.notOk($('.ui.label', this.element).hasClass('pointing'), 'Override default pointing with undefined value.');
 
     this.set('color', 'pink');
     this.set('pointing', 'left pointing');
-    assert.ok(this.$('.ui.label').hasClass('pink'), 'Color works through CSS class.');
-    assert.ok(this.$('.ui.label').hasClass('left'), 'Pointing works through CSS class.');
+    assert.ok($('.ui.label', this.element).hasClass('pink'), 'Color works through CSS class.');
+    assert.ok($('.ui.label', this.element).hasClass('left'), 'Pointing works through CSS class.');
 
     await  render(hbs`{{flexberry-validationmessage}}`);
-    assert.ok(this.$('.ui.label').hasClass('red'), `Default color 'red'.`);
-    assert.ok(this.$('.ui.label').hasClass('pointing'), `Default pointing 'pointing'.`);
+    assert.ok($('.ui.label', this.element).hasClass('red'), `Default color 'red'.`);
+    assert.ok($('.ui.label', this.element).hasClass('pointing'), `Default pointing 'pointing'.`);
   });
 });

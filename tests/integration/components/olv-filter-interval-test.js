f705b5b2d617e0ff71d793f57133bffd5fd13d4a
Bobrikov Mikhail <mihapir4t@yandex.ru>
2024-08-05 09:48:19 +0500

    fix

--- tests/integration/components/olv-filter-interval-test.js
+++ tests/integration/components/olv-filter-interval-test.js
@@ -2,6 +2,7 @@ import { module, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 import { render } from '@ember/test-helpers';
 import { setupRenderingTest } from 'ember-qunit';
+import $ from 'jquery';
 
 module('Integration | Component | olv-filter-interval', function(hooks){
   setupRenderingTest(hooks);

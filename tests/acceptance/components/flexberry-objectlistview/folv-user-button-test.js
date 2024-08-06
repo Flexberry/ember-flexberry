3755b623326ee436ed276a31dfa6aeb7deeacd5d
Bobrikov Mikhail <mihapir4t@yandex.ru>
2024-08-06 10:22:03 +0500

    Update folv tests acceptance

--- tests/acceptance/components/flexberry-objectlistview/folv-user-button-test.js
+++ tests/acceptance/components/flexberry-objectlistview/folv-user-button-test.js
@@ -1,30 +1,28 @@
 import { executeTest } from './execute-folv-test';
+import {settled} from '@ember/test-helpers';
 
-executeTest('user button test', (store, assert, app) => {
+executeTest('user button test', async (store, assert, app) => {
   assert.expect(5);
   let path = 'components-examples/flexberry-objectlistview/toolbar-custom-buttons-example';
 
-  visit(path);
-  andThen(() => {
-    assert.equal(currentPath(), path);
+  await visit(path);
+  assert.equal(currentPath(), path);
 
-    let controller = app.__container__.lookup('controller:' + currentRouteName());
+  let controller = app.__container__.lookup('controller:' + currentRouteName());
 
     // Enable the hi button.
-    click('.toggle-hi-button');
+  await click('.toggle-hi-button');
 
     // First click.
-    click('.test-click-button');
-    andThen(() => assert.equal(controller.clickCounter, 2, 'Test button was pressed'));
+  await click('.test-click-button');
+  assert.equal(controller.clickCounter, 2, 'Test button was pressed');
 
-    // Second click.
-    click('.test-click-button');
-    andThen(() => assert.equal(controller.clickCounter, 3, 'Test button was pressed'));
+  // Second click.
+  click('.test-click-button');
+  andThen(() => assert.equal(controller.clickCounter, 3, 'Test button was pressed'));
 
-    assert.notOk(controller.get('modelFromClickedRow'));
-    click('.ui.button > .bug.icon:first');
-    andThen(() => {
-      assert.equal(controller.get('modelFromClickedRow.id'), controller.get('model.firstObject.id'));
-    });
-  });
+  assert.notOk(controller.get('modelFromClickedRow'));
+  await click('.ui.button > .bug.icon:first');
+  await settled;
+  assert.equal(controller.get('modelFromClickedRow.id'), controller.get('model.firstObject.id'))
 });

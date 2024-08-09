ba3061fe322149900fd0a40f77461d634705ba0e
Bobrikov Mikhail <mihapir4t@yandex.ru>
2024-08-09 18:42:54 +0500

    Update-Acceptance-highload-edit-form-menu-test Acceptance

--- tests/acceptance/components/highload-edit-form-menu-test.js
+++ tests/acceptance/components/highload-edit-form-menu-test.js
@@ -1,62 +1,56 @@
 import $ from 'jquery';
-import { run } from '@ember/runloop';
 import { module, test } from 'qunit';
 import startApp from '../../helpers/start-app';
-import wait from 'ember-test-helpers/wait';
+import { settled } from '@ember/test-helpers';
+import { visit, currentURL } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+import { run } from '@ember/runloop';
+
 let app;
+module('Acceptance | high-edit-form-menu', function (hooks) {
+  setupApplicationTest(hooks);
 
-module('Acceptance | high-edit-form-menu', {
-  beforeEach() {
+  hooks.beforeEach(function () {
     app = startApp();
-  },
+  });
 
-  afterEach() {
+  hooks.afterEach(function () {
     // Destroy application.
     run(app, 'destroy');
-  },
-});
+  });
 
-test('it properly renders', function(assert) {
-  assert.expect(7);
-  let done = assert.async();
+  test('it properly renders', async function (assert) {
+    assert.expect(7);
 
-  let path = 'components-examples/highload-edit-form-menu/index';
-  visit(path);
-  andThen(function() {
+    let path = 'components-examples/highload-edit-form-menu/index';
+    await visit(path);
     assert.equal(currentURL(), path);
     $('.object-list-view').find('tr')[1].children[1].click();
 
-    andThen(function() {
-      wait().then(() => {
-        assert.equal($('.gruppaPolejVvoda').length, 4, 'all tabs are here');
-        assert.equal($('.gruppaPolejVvoda.active').length, 1, 'only one tab is active');
-        assert.equal($('.gruppaPolejVvoda')[0].classList.contains('active'), true, 'first tab is active')
+    await settled();
+    assert.equal($('.gruppaPolejVvoda').length, 4, 'all tabs are here');
+    assert.equal($('.gruppaPolejVvoda.active').length, 1, 'only one tab is active');
+    assert.equal($('.gruppaPolejVvoda')[0].classList.contains('active'), true, 'first tab is active');
 
-        run(() => {
-          $('.tabsNavigation')[0].click();
-        });
-
-        wait().then(() => {
-          assert.equal($('.gruppaPolejVvoda')[1].classList.contains('active'), true, 'next tab is active')
-
-          run(() => {
-            $('.tabsNavigation')[1].click();
-          });
-
-          wait().then(() => {
-            assert.equal($('.gruppaPolejVvoda')[0].classList.contains('active'), true, 'previous tab is active')
-
-            run(() => {
-              $('.showAllFormsButton').click();
-            });
-
-            wait().then(() => {
-              assert.equal($('.gruppaPolejVvoda.active').length, 4, 'all tabs are active');
-              done();
-            });
-          });
-        });
-      });
+    run(() => {
+      $('.tabsNavigation')[0].click();
     });
+
+    await settled();
+    assert.equal($('.gruppaPolejVvoda')[1].classList.contains('active'), true, 'next tab is active');
+
+    run(() => {
+      $('.tabsNavigation')[1].click();
+    });
+
+    await settled();
+    assert.equal($('.gruppaPolejVvoda')[0].classList.contains('active'), true, 'previous tab is active');
+
+    run(() => {
+      $('.showAllFormsButton').click();
+    });
+
+    await settled();
+    assert.equal($('.gruppaPolejVvoda.active').length, 4, 'all tabs are active');
   });
 });
